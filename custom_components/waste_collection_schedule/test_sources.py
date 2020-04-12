87e4c90f1734ceb2eebaec6d7049b84f1d0d5f21
mampfes <mampfes@users.noreply.github.com>
2020-04-12 09:51:29 +0200

    add arguments to select source and show list

--- custom_components/waste_collection_schedule/test_sources.py
+++ custom_components/waste_collection_schedule/test_sources.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
 
+import argparse
 import os
 import pathlib
 import importlib
 
 
 def main():
-    for f in pathlib.Path("package/source").glob("*.py"):
+    parser = argparse.ArgumentParser(description='Test sources.')
+    parser.add_argument("-s", "--source", action='append', help="Test given source file")
+    parser.add_argument("-l", "--list", action="store_true", help="List retrieved entries")
+    args = parser.parse_args()
+
+    if args.source is not None:
+        files = args.source
+    else:
+        files = filter(lambda x: x != "__init__", map(lambda x: x.stem, pathlib.Path("package/source").glob("*.py")))
+
+    for f in files:
         # iterate through all *.py files in package/source
-        if f.stem != "__init__":
-            # ignore __init__.py
-            print(f"Testing source {f.stem} ...")
-            module = importlib.import_module(f"package.source.{f.stem}")
-            # create source
-            for name, tc in module.TEST_CASES.items():
-                # run through all test-cases
-                source = module.Source(**tc)
-                result = source.fetch()
-                print(f"  found {len(result)} entries for {name}")
+        print(f"Testing source {f} ...")
+        module = importlib.import_module(f"package.source.{f}")
+        # create source
+        for name, tc in module.TEST_CASES.items():
+            # run through all test-cases
+            source = module.Source(**tc)
+            result = source.fetch()
+            print(f"  found {len(result)} entries for {name}")
+            if args.list:
+                for x in result:
+                    print(f"    {x.date.isoformat()}: {x.type}")
 
 
 if __name__ == "__main__":
