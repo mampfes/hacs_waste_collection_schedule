118566b77c8ab66bdb7b30e08c8ac67f25ccf9cd
mampfes <mampfes@users.noreply.github.com>
2020-05-07 07:57:56 +0200

    add local file support for ics source

--- custom_components/waste_collection_schedule/package/source/ics.py
+++ custom_components/waste_collection_schedule/package/source/ics.py
@@ -2,6 +2,8 @@ import requests
 import datetime
 import icalendar
 from collections import OrderedDict
+from pathlib import Path
+
 
 from ..helpers import CollectionAppointment
 
@@ -22,6 +24,17 @@ TEST_CASES = OrderedDict(
                 "url": "https://api.abfall.io/?kh=DaA02103019b46345f1998698563DaAd&t=ics&s=1a862df26f6943997cef90233877a4fe"
             },
         ),
+        (
+            "Test File",
+            {
+                # Path is used here to allow to call the Source from any location.
+                # This is not required in a yaml configuration!
+                "file": Path(__file__)
+                .resolve()
+                .parents[1]
+                .joinpath("test/test.ics")
+            },
+        ),
     ]
 )
 
@@ -30,37 +43,50 @@ HEADERS = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 
 
 class Source:
-    def __init__(self, url, offset=None):
+    def __init__(self, url=None, file=None, offset=None):
         self._url = url
+        self._file = file
         self._offset = offset
+        if bool(self._url is not None) == bool(self._file is not None):
+            raise RuntimeError("Specify either url or file")
 
     def fetch(self):
-        if "{%Y}" in self._url:
-            # url contains wildcard
-            now = datetime.datetime.now()
-            url = self._url.replace("{%Y}", str(now.year))
-            entries = self.fetch_year(url)
-            if now.month == 12:
-                # also get data for next year if we are already in december
-                url = self._url.replace("{%Y}", str(now.year + 1))
-                try:
-                    entries.extend(self.fetch_year(url))
-                except Exception:
-                    # ignore if fetch for next year fails
-                    pass
-            return entries
-        else:
-            return self.fetch_year(self._url)
+        if self._url is not None:
+            if "{%Y}" in self._url:
+                # url contains wildcard
+                now = datetime.datetime.now()
+                url = self._url.replace("{%Y}", str(now.year))
+                entries = self.fetch_url(url)
+                if now.month == 12:
+                    # also get data for next year if we are already in december
+                    url = self._url.replace("{%Y}", str(now.year + 1))
+                    try:
+                        entries.extend(self.fetch_url(url))
+                    except Exception:
+                        # ignore if fetch for next year fails
+                        pass
+                return entries
+            else:
+                return self.fetch_url(self._url)
+        elif self._file is not None:
+            return self.fetch_file(self._file)
 
-    def fetch_year(self, url):
+    def fetch_url(self, url):
         # get ics file
         r = requests.get(url, headers=HEADERS)
         r.encoding = "utf-8"  # requests doesn't guess the encoding correctly
 
+        return self._convert(r.text)
+
+    def fetch_file(self, file):
+        f = open(file, "r")
+        return self._convert(f.read())
+
+    def _convert(self, data):
         entries = []
 
         # parse ics file
-        calender = icalendar.Calendar.from_ical(r.text)
+        calender = icalendar.Calendar.from_ical(data)
 
         entries = []
         for e in calender.walk():
--- doc/source/ics.md
+++ doc/source/ics.md
@@ -10,22 +10,34 @@ waste_collection_schedule:
     - name: ics
       args:
         url: URL
+        file: FILE
         offset: OFFSET
 ```
 
 ### Configuration Variables
 
 **url**<br>
-*(string) (required)*
+*(string) (optional)*
+
+URL to ICS file. File will be downloaded using a HTTP GET command.
 
 If the original url contains the current year (4 digits including century), this can be replaced by the wildcard `{%Y}` (see example below).
+
+You have to specify either `url` or `file`!
+
+**file**<br>
+*(string) (optional)*
+
+Local ICS file name. Can be used instead of `url` for local files.
+
+You have to specify either `url` or `file`!
 
 **offset**<br>
 *(int) (optional, default: `0`)*
 
 Offset in days which will be added to every start time. Can be used if the start time of the events in the ICS file are ahead of the actual date.
 
-## Example
+## Examples
 
 ```yaml
 waste_collection_schedule:
@@ -34,5 +46,13 @@ waste_collection_schedule:
       args:
         url: "https://www.avl-ludwigsburg.de/fileadmin/Files/Abfallkalender/ICS/Privat/Privat_{%Y}_Ossweil.ics"
         offset: 1
+```
+
+```yaml
+waste_collection_schedule:
+  sources:
+    - name: ics
+      args:
+        file: "test.ics"
 ```
 
