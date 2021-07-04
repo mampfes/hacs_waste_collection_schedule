b6bc84acac6206d6148d0eb78c9c5d79b3debcd3
mampfes <mampfes@users.noreply.github.com>
2021-07-04 15:06:35 +0200

    fix hvcgroup_nl test case
    and add error log output if 1st fetch fails

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/hvcgroep_nl.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/hvcgroep_nl.py
@@ -1,30 +1,28 @@
+import json
+import logging
 from datetime import datetime
 
 import requests
-import json
-
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = "HVCGroep"
 DESCRIPTION = "Source for the Dutch HVCGroep waste management."
 URL = "https://www.hvcgroep.nl/zelf-regelen/afvalkalender"
-TEST_CASES = {
-    "Obdam": {
-        "postal_code": "1713VM",
-        "house_number": "1",
-    },
-    "Tollebeek": {
-        "postal_code": "8309AV",
-        "house_number": "1",
-    }
-}
+TEST_CASES = {"Tollebeek": {"postal_code": "8309AV", "house_number": "1"}}
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class Source:
     def __init__(self, postal_code, house_number):
         self.postal_code = postal_code
         self.house_number = house_number
-        self.icons = {"plastic-blik-drinkpak": "mdi:recycle", "gft": "mdi:leaf", "papier-en-karton": "mdi:archive", "restafval": "mdi:trash-can"}
+        self.icons = {
+            "plastic-blik-drinkpak": "mdi:recycle",
+            "gft": "mdi:leaf",
+            "papier-en-karton": "mdi:archive",
+            "restafval": "mdi:trash-can",
+        }
 
     def fetch(self):
         bag_id = 0
@@ -35,11 +33,12 @@ class Source:
         )
         data = json.loads(r.text)
 
-        bag_id = data[0]["bagid"]
-
         # Something must be wrong, maybe the address isn't valid? No need to do the extra requests so just return here.
-        if bag_id == 0:
+        if len(data) == 0:
+            _LOGGER.error("no data found for this address")
             return []
+
+        bag_id = data[0]["bagid"]
 
         # Retrieve the details about different waste management flows (for example, paper, plastic etc.)
         r = requests.get(
@@ -56,12 +55,14 @@ class Source:
         entries = []
 
         for item in data:
-            waste_details = [x for x in waste_flows if x["id"] == item["afvalstroom_id"]]
+            waste_details = [
+                x for x in waste_flows if x["id"] == item["afvalstroom_id"]
+            ]
             entries.append(
                 Collection(
                     date=datetime.strptime(item["ophaaldatum"], "%Y-%m-%d").date(),
                     t=waste_details[0]["title"],
-                    icon=self.icons.get(waste_details[0]["icon"], 'mdi:trash-can')
+                    icon=self.icons.get(waste_details[0]["icon"], "mdi:trash-can"),
                 )
             )
 
