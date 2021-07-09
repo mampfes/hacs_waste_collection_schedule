1e2b1c917759efab85d9d282319c80a4e3e5051a
autinerd <27780930+autinerd@users.noreply.github.com>
2021-07-09 19:01:25 +0200

    add test-cases and documentation for awido

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/awido_cubefour_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/awido_cubefour_de.py
@@ -3,16 +3,41 @@ import requests
 import json
 
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
-from waste_collection_schedule.service.ICS import ICS
 
 TITLE = "Cubefour AWIDO"
 DESCRIPTION = "Source for waste collections hosted by Cubefour."
 URL = "https://www.awido-online.de/"
+TEST_CASES = {
+    "Schorndorf, Raingrund 21": {
+        "customer": "rmk",
+        "city": "Schorndorf",
+        "street": "Raingrund",
+        "housenumber": "21",
+        "fraktionen": "1"
+    },
+    "Altomünster, Maisbrunn": {
+        "customer": "lra-dah",
+        "city": "Altomünster",
+        "street": "Maisbrunn",
+        "fraktionen": "1,3"
+    },
+    "SOK-Alsmannsdorf": {
+        "customer": "zaso",
+        "city": "SOK-Alsmannsdorf",
+        "fraktionen": "1,2,3,10"
+    },
+    "Kaufbeuren, Rehgrund": {
+        "customer": "kaufbeuren",
+        "city": "Kaufbeuren",
+        "street": "Rehgrund",
+        "fraktionen": "1,3,4"
+    }
+}
 
 class Source:
-    def __init__(self, customer, fraktionen, city, street, housenumber):
+    def __init__(self, customer, fraktionen=None, city, street=None, housenumber=None):
         self._customer = customer
-        self._fractions = fractions
+        self._fraktionen = fraktionen
         self._city = city
         self._street = street
         self._housenumber = housenumber
@@ -25,24 +50,27 @@ class Source:
         found_places = [(place['key'], place['value']) for place in places if place['value'] == self._city]
         if len(found_places) != 1:
             return []
-        place_oid = found_places[0][0]
-
-        streets = json.loads(requests.get(f'https://awido.cubefour.de//WebServices/Awido.Service.svc/secure/getGroupedStreets/{place_oid}?client={self._customer}').text)
-        found_streets = [(street['key'], street['value']) for street in streets if self._street in street['value']]
-        if len(found_streets) != 1:
-            return []
-        # If Housenumber is already in found street, oid is already found, if not, check for housenumber
-        if self._street != found_streets[0][1]:
-            oid = found_streets[0][0]
+        if self._street is None:
+            oid = found_places[0][0]
         else:
-            street_oid = found_streets[0][0]
-            housenumbers = json.loads(requests.get(f'https://awido.cubefour.de//WebServices/Awido.Service.svc/secure/getStreetAddons/{street_oid}?client={self._customer}').text)
-            found_housenumbers = [(hn['key'], hn['value']) for hn in housenumbers if self._housenumber == hn['value']]
-            if len(found_housenumbers) == 0:
-                return []
-            oid = found_housenumbers[0][0]
+            place_oid = found_places[0][0]
 
-        cal_json = json.loads(requests.get(f'https://awido.cubefour.de//WebServices/Awido.Service.svc/secure/getData/{oid}?fractions={fraktionen}&client={self._customer}'))
+            streets = json.loads(requests.get(f'https://awido.cubefour.de//WebServices/Awido.Service.svc/secure/getGroupedStreets/{place_oid}?client={self._customer}').text)
+            found_streets = [(street['key'], street['value']) for street in streets if self._street in street['value']]
+            if len(found_streets) != 1:
+                return []
+            # If Housenumber is already in found street, oid is already found, if not, check for housenumber
+            if self._street != found_streets[0][1] or self._housenumber is None:
+                oid = found_streets[0][0]
+            else:
+                street_oid = found_streets[0][0]
+                housenumbers = json.loads(requests.get(f'https://awido.cubefour.de//WebServices/Awido.Service.svc/secure/getStreetAddons/{street_oid}?client={self._customer}').text)
+                found_housenumbers = [(hn['key'], hn['value']) for hn in housenumbers if str(self._housenumber) == hn['value']]
+                if len(found_housenumbers) == 0:
+                    return []
+                oid = found_housenumbers[0][0]
+
+        cal_json = json.loads(requests.get(f'https://awido.cubefour.de//WebServices/Awido.Service.svc/secure/getData/{oid}?fractions={str(self._fraktionen) if self._fraktionen is not None else ""}&client={self._customer}').text)
         calendar = [item for item in cal_json['calendar'] if item['ad'] is not None]
         fracts = cal_json['fracts']
         fractions = {}
@@ -52,7 +80,7 @@ class Source:
         entries = []
         for calitem in calendar:
             date = datetime.date(int(calitem['dt'][0:4]), int(calitem['dt'][4:6]), int(calitem['dt'][6:8]))
-            for i, fracitem in calitem['fr']:
+            for fracitem in calitem['fr']:
                 typ = fractions[fracitem]
                 entries.append(Collection(date, typ))
         
