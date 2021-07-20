1e4bef0074d3d6697a1e859dbf695c729e78a407
mampfes <mampfes@users.noreply.github.com>
2021-07-20 07:39:18 +0200

    improve error logging in case of config option mismatch

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/awido_cubefour_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/awido_cubefour_de.py
@@ -1,6 +1,7 @@
 import datetime
 import requests
 import json
+import logging
 
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
@@ -8,27 +9,30 @@ TITLE = "Cubefour AWIDO"
 DESCRIPTION = "Source for waste collections hosted by Cubefour."
 URL = "https://www.awido-online.de/"
 TEST_CASES = {
-    "Schorndorf, Raingrund 21": {
+    "Schorndorf, Miedelsbacher Straße 30 /1": {
         "customer": "rmk",
         "city": "Schorndorf",
-        "street": "Raingrund",
-        "housenumber": "21"
+        "street": "Miedelsbacher Straße",
+        "housenumber": "30 /1"
     },
-#    "Altomünster, Maisbrunn": {
-#        "customer": "lra-dah",
-#        "city": "Altomünster",
-#        "street": "Maisbrunn"
-#    },
-#    "SOK-Alsmannsdorf": {
-#        "customer": "zaso",
-#        "city": "SOK-Alsmannsdorf"
-#    },
-#    "Kaufbeuren, Rehgrund": {
-#        "customer": "kaufbeuren",
-#        "city": "Kaufbeuren",
-#        "street": "Rehgrund"
-#    }
+    "Altomünster, Maisbrunn": {
+        "customer": "lra-dah",
+        "city": "Altomünster",
+        "street": "Maisbrunn"
+    },
+    "SOK-Alsmannsdorf": {
+        "customer": "zaso",
+        "city": "SOK-Alsmannsdorf"
+    },
+    "Kaufbeuren, Rehgrund": {
+        "customer": "kaufbeuren",
+        "city": "Kaufbeuren",
+        "street": "Rehgrund"
+    }
 }
+
+_LOGGER = logging.getLogger(__name__)
+
 
 class Source:
     def __init__(self, customer, city, street=None, housenumber=None):
@@ -42,48 +46,60 @@ class Source:
         r = requests.get(f'https://awido.cubefour.de/WebServices/Awido.Service.svc/secure/getPlaces/client={self._customer}')
         places = json.loads(r.text)
 
-        # Check if given place is in the list
-        found_places = [(place['key'], place['value']) for place in places if place['value'] == self._city]
-        if len(found_places) != 1:
-            return []
-        if self._street is None:
-            oid = found_places[0][0]
-        else:
-            place_oid = found_places[0][0]
+        # create city to key map from retrieved places
+        city_to_oid = {place["value"].strip():place["key"] for (place) in places}
 
-            r = requests.get(f'https://awido.cubefour.de/WebServices/Awido.Service.svc/secure/getGroupedStreets/{place_oid}', params={"client":self._customer})
+        if self._city not in city_to_oid:
+            _LOGGER.error(f"city not found: {self._city}")
+            return []
+
+        oid = city_to_oid[self._city]
+
+        if self._street is not None:
+            r = requests.get(f'https://awido.cubefour.de/WebServices/Awido.Service.svc/secure/getGroupedStreets/{oid}', params={"client":self._customer})
             streets = json.loads(r.text)
 
-            found_streets = [(street['key'], street['value']) for street in streets if self._street in street['value']]
-            if len(found_streets) != 1:
-                return []
-            # If Housenumber is already in found street, oid is already found, if not, check for housenumber
-            if self._street != found_streets[0][1] or self._housenumber is None:
-                oid = found_streets[0][0]
-            else:
-                street_oid = found_streets[0][0]
-                r = requests.get(f'https://awido.cubefour.de/WebServices/Awido.Service.svc/secure/getStreetAddons/{street_oid}', params={"client":self._customer})
-                housenumbers = json.loads(r.text)
-                found_housenumbers = [(hn['key'], hn['value']) for hn in housenumbers if str(self._housenumber) == hn['value']]
-                if len(found_housenumbers) == 0:
-                    return []
-                oid = found_housenumbers[0][0]
+            # create street to key map from retrieved places
+            street_to_oid = {street["value"].strip():street["key"] for (street) in streets}
 
+            if self._street not in street_to_oid:
+                _LOGGER.error(f"street not found: {self._street}")
+                return []
+
+            oid = street_to_oid[self._street]
+
+            if self._housenumber is not None:
+                r = requests.get(f'https://awido.cubefour.de/WebServices/Awido.Service.svc/secure/getStreetAddons/{oid}', params={"client":self._customer})
+                hsnbrs = json.loads(r.text)
+
+                # create housenumber to key map from retrieved places
+                hsnbr_to_oid = {hsnbr["value"].strip():hsnbr["key"] for (hsnbr) in hsnbrs}
+
+                if self._housenumber not in hsnbr_to_oid:
+                    _LOGGER.error(f"housenumber not found: {self._housenumber}")
+                    return []
+
+                oid = hsnbr_to_oid[self._housenumber]
+                
+        # get calendar data
         r = requests.get(f'https://awido.cubefour.de/WebServices/Awido.Service.svc/secure/getData/{oid}', params={
             "fractions": "",
-            "client":self._customer})
+            "client": self._customer})
         cal_json = json.loads(r.text)
+
+        # map fraction code to fraction name
+        fractions = {fract["snm"]:fract["nm"] for (fract) in cal_json["fracts"]}
+
+        # calendar also contains public holidays. In this case, 'ad' is None
         calendar = [item for item in cal_json['calendar'] if item['ad'] is not None]
-        fracts = cal_json['fracts']
-        fractions = {}
-        for fract in fracts:
-            fractions[fract['snm']] = fract['nm']
         
         entries = []
         for calitem in calendar:
-            date = datetime.date(int(calitem['dt'][0:4]), int(calitem['dt'][4:6]), int(calitem['dt'][6:8]))
+            date = datetime.datetime.strptime(calitem["dt"], "%Y%m%d").date()
+
+            # add all fractions for this date
             for fracitem in calitem['fr']:
-                typ = fractions[fracitem]
-                entries.append(Collection(date, typ))
+                waste_type = fractions[fracitem]
+                entries.append(Collection(date, waste_type))
         
         return entries
