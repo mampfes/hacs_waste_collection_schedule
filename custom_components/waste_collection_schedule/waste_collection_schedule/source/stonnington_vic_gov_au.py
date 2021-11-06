fcd623ce1bfb21ce6550b1c6960d9f779bc47f5c
mampfes <mampfes@users.noreply.github.com>
2021-11-06 10:31:09 +0100

    fix static code analyzer warnings

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/stonnington_vic_gov_au.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/stonnington_vic_gov_au.py
@@ -1,8 +1,7 @@
-# coding: utf-8
-from datetime import datetime
 import logging
-import requests
+from datetime import datetime
 
+import requests
 from bs4 import BeautifulSoup
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
@@ -16,56 +15,65 @@ TEST_CASES = {
 
 _LOGGER = logging.getLogger(__name__)
 
+ICON_MAP = {
+    "Food and Green Waste": "mdi:leaf",
+    "Hard Waste": "mdi:sofa",
+    "Recycling": "mdi:recycle",
+}
+
 
 class Source:
     def __init__(self, street_address):
         self._street_address = street_address
 
     def fetch(self):
-        session = requests.Session() 
+        session = requests.Session()
 
-        response = session.get("https://www.stonnington.vic.gov.au/Services/Waste-and-recycling")
+        response = session.get(
+            "https://www.stonnington.vic.gov.au/Services/Waste-and-recycling"
+        )
         response.raise_for_status()
 
         response = session.get(
-            f"https://www.stonnington.vic.gov.au/api/v1/myarea/search",
-            params={'keywords': self._street_address}
+            "https://www.stonnington.vic.gov.au/api/v1/myarea/search",
+            params={"keywords": self._street_address},
         )
         response.raise_for_status()
         addressSearchApiResults = response.json()
-        if addressSearchApiResults['Items'] == None or len(addressSearchApiResults['Items']) < 1:
-            _LOGGER.error(f"Address search for '{self._street_address}' returned no results. Check your address on https://www.stonnington.vic.gov.au/Services/Waste-and-recycling")
+        if (
+            addressSearchApiResults["Items"] is None
+            or len(addressSearchApiResults["Items"]) < 1
+        ):
+            _LOGGER.error(
+                f"Address search for '{self._street_address}' returned no results. Check your address on https://www.stonnington.vic.gov.au/Services/Waste-and-recycling"
+            )
             return []
 
-        addressSearchTopHit = addressSearchApiResults['Items'][0]
+        addressSearchTopHit = addressSearchApiResults["Items"][0]
         _LOGGER.debug("Address search top hit: %s", addressSearchTopHit)
 
-        geolocationid = addressSearchTopHit['Id']
+        geolocationid = addressSearchTopHit["Id"]
         _LOGGER.debug("Geolocationid: %s", geolocationid)
 
         response = session.get(
             "https://www.stonnington.vic.gov.au/ocapi/Public/myarea/wasteservices?ocsvclang=en-AU",
-            params={'geolocationid': geolocationid}
+            params={"geolocationid": geolocationid},
         )
         response.raise_for_status()
 
         wasteApiResult = response.json()
         _LOGGER.debug("Waste API result: %s", wasteApiResult)
 
-        soup = BeautifulSoup(wasteApiResult['responseContent'], 'html.parser')
+        soup = BeautifulSoup(wasteApiResult["responseContent"], "html.parser")
 
         entries = []
-        for article in soup.find_all('article'):
+        for article in soup.find_all("article"):
             waste_type = article.h3.string
-            icon = \
-                "mdi:leaf" if waste_type == "Food and Green Waste" else \
-                "mdi:sofa" if waste_type == "Hard Waste" else \
-                "mdi:recycle" if waste_type == "Recycling" else \
-                "mdi:trash-can"
+            icon = ICON_MAP.get(waste_type, "mdi:trash-can")
             next_pickup = article.find(class_="next-service").string.strip()
-            next_pickup_date = datetime.strptime(next_pickup.split(sep=' ')[1], '%d/%m/%Y').date()
-            entries.append(
-                Collection(date=next_pickup_date, t=waste_type, icon=icon)
-            )
+            next_pickup_date = datetime.strptime(
+                next_pickup.split(sep=" ")[1], "%d/%m/%Y"
+            ).date()
+            entries.append(Collection(date=next_pickup_date, t=waste_type, icon=icon))
 
         return entries
