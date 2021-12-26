a19b6102b2df673e2d0697db1460ad4c3d2cb932
mampfes <mampfes@users.noreply.github.com>
2021-12-26 08:59:55 +0100

    pylint lindau_ch

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/lindau_ch.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/lindau_ch.py
@@ -1,8 +1,8 @@
 import json
 from datetime import datetime
-from bs4 import BeautifulSoup
 
 import requests
+from bs4 import BeautifulSoup
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = "Abfall Lindau"
@@ -14,38 +14,45 @@ TEST_CASES = {
 }
 
 
+IconMap = {
+    "kehricht": "mdi:trash-can",
+    "grungut": "mdi:leaf",
+    "hackseldienst": "mdi:leaf",
+    "papier und karton": "mdi:package-variant",
+    "altmetalle": "mdi:nail",
+}
+
 
 class Source:
     def __init__(self, city):
         self._city = city
-        self._iconMap  = {
-            "kehricht": "mdi:trash-can",
-            "grungut" : "mdi:leaf",
-            "hackseldienst" : "mdi:leaf",
-            "papier und karton" : "mdi:package-variant",
-            "altmetalle" : "mdi:nail",
-        }     
 
     def fetch(self):
-
-    
         response = requests.get("https://www.lindau.ch/abfalldaten")
-        
-        html = BeautifulSoup(response.text, 'html.parser')
 
-        table = html.find('table', attrs={'id':'icmsTable-abfallsammlung'})
-        data = json.loads(table.attrs['data-entities'])
+        html = BeautifulSoup(response.text, "html.parser")
+
+        table = html.find("table", attrs={"id": "icmsTable-abfallsammlung"})
+        data = json.loads(table.attrs["data-entities"])
 
         entries = []
-        for item in data['data']:
-
-            if self._city in item['abfallkreisIds'] or self._city in item['abfallkreisNameList']:
-                next_pickup = item['_anlassDate-sort'].split()[0]
+        for item in data["data"]:
+            if (
+                self._city in item["abfallkreisIds"]
+                or self._city in item["abfallkreisNameList"]
+            ):
+                next_pickup = item["_anlassDate-sort"].split()[0]
                 next_pickup_date = datetime.fromisoformat(next_pickup).date()
 
-                waste_type = BeautifulSoup(item['name'],'html.parser').text
-                waste_type_sorted = BeautifulSoup(item['name-sort'],'html.parser').text
-                
-                entries.append(Collection(date=next_pickup_date, t=waste_type, icon=self._iconMap.get(waste_type_sorted,"mdi:trash-can")))               
-                
+                waste_type = BeautifulSoup(item["name"], "html.parser").text
+                waste_type_sorted = BeautifulSoup(item["name-sort"], "html.parser").text
+
+                entries.append(
+                    Collection(
+                        date=next_pickup_date,
+                        t=waste_type,
+                        icon=IconMap.get(waste_type_sorted, "mdi:trash-can"),
+                    )
+                )
+
         return entries
