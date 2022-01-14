12055b44463e03d760f8994ab25ed5d8490271fa
Sam Jordan <sam@netsells.co.uk>
2022-01-14 09:52:07 -0800

    Switches york to calendar endpoint instead of next collection

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/york_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/york_gov_uk.py
@@ -1,6 +1,5 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 import json
-from urllib.parse import quote
 
 import requests
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
@@ -8,7 +7,6 @@ from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 TITLE = "York.gov.uk"
 DESCRIPTION = "Source for York.gov.uk services for the city of York, UK."
 URL = "https://york.gov.uk"
-TEST_CASES = {}
 TEST_CASES = {
     "Reighton Avenue, York": {
         "uprn": "100050580641",
@@ -16,6 +14,12 @@ TEST_CASES = {
     "Granary Walk, York": {
         "uprn": "010093236548",
     },
+}
+
+ICONS = {
+    "REFUSE": "mdi:trash-can",
+    "RECYCLING": "mdi:recycle",
+    "GARDEN": "mdi:leaf",
 }
 
 
@@ -26,65 +30,26 @@ class Source:
     def fetch(self):
         # get json file
         r = requests.get(
-            f"https://waste-api.york.gov.uk/api/GetBinCollectionDataForUprn/{self._uprn}"
+            f"https://waste-api.york.gov.uk/api/GetBinCalendarDataForUprn/{self._uprn}"
         )
 
         # extract data from json
         data = json.loads(r.text)
 
-        # Map to better format to work with
-        serviceData = {}
-
-        for service in data["services"]:
-            serviceData[service["service"]] = service["nextCollection"]
-
-            lastCollected = datetime.fromisoformat(service["lastCollected"])
-
-            # York data will never show "today" unless we check for last collected
-            if lastCollected.date() == datetime.today().date():
-                serviceData[service["service"]] = service["lastCollected"]
-
-        # create entries for trash, recycling, and yard waste
         entries = []
 
-        try:
-            entries.append(
-                Collection(
-                    date=datetime.strptime(
-                        serviceData["REFUSE"], "%Y-%m-%dT%H:%M:%S"
-                    ).date(),
-                    t="Trash",
-                    icon="mdi:trash-can",
+        for collection in data["collections"]:
+            try:
+                entries.append(
+                    Collection(
+                        date=datetime.strptime(
+                            collection["date"], "%Y-%m-%dT%H:%M:%S"
+                        ).date(),
+                        t=collection["roundType"].title(),
+                        icon=ICONS[collection["roundType"]],
+                    )
                 )
-            )
-        except ValueError:
-            pass  # ignore date conversion failure for not scheduled collections
-
-        try:
-            entries.append(
-                Collection(
-                    date=datetime.strptime(
-                        serviceData["RECYCLING"], "%Y-%m-%dT%H:%M:%S"
-                    ).date(),
-                    t="Recycling",
-                    icon="mdi:recycle",
-                )
-            )
-        except ValueError:
-            pass  # ignore date conversion failure for not scheduled collections
-
-        try:
-            entries.append(
-                Collection(
-                    date=datetime.strptime(
-                        serviceData["GARDEN"], "%Y-%m-%dT%H:%M:%S"
-                    ).date(),
-                    t="Yard Waste",
-                    icon="mdi:leaf",
-                )
-            )
-        except ValueError:
-            pass  # ignore date conversion failure for not scheduled collections
+            except ValueError:
+                pass  # ignore date conversion failure for not scheduled collections
 
         return entries
-
