5b270615fbfa89c32583d07595738a19aa2af95f
percidae <percidae@users.noreply.github.com>
2022-03-27 22:25:41 +0200

    Removed not necessary header data

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/avl_ludwigsburg.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/avl_ludwigsburg.py
@@ -58,24 +58,8 @@ class Source:
             data.pop('tx_avlcollections_pi5[wasteCalendarStreetItem]', None)
 
         # Creat the headers and cookies
-        headers = {
-            'Connection': 'keep-alive',
-            'Cache-Control': 'max-age=0',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Opera";v="84"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'Upgrade-Insecure-Requests': '1',
-            'Origin': 'https://www.avl-ludwigsburg.de',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 OPR/84.0.4316.21',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-User': '?1',
-            'Sec-Fetch-Dest': 'document',
-            'Referer': 'https://www.avl-ludwigsburg.de/privatkunden/termine/abfallkalender/suche/',
-            'Accept-Language': 'de-DE,de;q=0.9',
-        }
+        headers = {}
+
         cookies = {
             'agree': 'false',
             'track_ga': 'false',
