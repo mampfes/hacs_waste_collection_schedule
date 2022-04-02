9b90fd5a57996f01eb10c74962216969ac375bc1
mampfes <mampfes@users.noreply.github.com>
2022-04-02 12:49:47 +0200

    refactor aw_harburg_de

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/aw_harburg_de.py
@@ -0,0 +1,134 @@
+import requests
+from bs4 import BeautifulSoup
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
+from waste_collection_schedule.service.ICS import ICS
+
+TITLE = "AW Harburg"
+DESCRIPTION = "Abfallwirtschaft Landkreis Harburg"
+URL = "https://www.landkreis-harburg.de/bauen-umwelt/abfallwirtschaft/abfallkalender/"
+
+TEST_CASES = {
+    "CityWithTwoLevels": {"level_1": "Hanstedt", "level_2": "Evendorf"},
+    "CityWithThreeLevels": {
+        "level_1": "Buchholz",
+        "level_2": "Buchholz mit Steinbeck (ohne Reindorf)",
+        "level_3": "Seppenser Mühlenweg Haus-Nr. 1 / 2",
+    },
+}
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64)",
+}
+
+
+class Source:
+    def __init__(self, level_1, level_2, level_3=None):
+        self._districts = [level_1, level_2, level_3]
+        self._ics = ICS()
+
+    def fetch(self):
+        # Use a session to keep cookies and stuff
+        session = requests.Session()
+
+        # Get the IDs of the districts on the first level
+        # Double loading is on purpose because sometimes the webpage has an overlay
+        # which is gone on the second try in a session
+        r = session.get(URL, headers=HEADERS)
+        if "Zur aufgerufenen Seite" in r.text:
+            r = session.get(URL, headers=HEADERS)
+        if r.status_code != 200:
+            raise Exception(f"Error: failed to fetch first url: {URL}")
+
+        # Get the IDs of the districts on the first level
+        id = self.parse_level(r.text, 1)
+
+        # Get the IDs of the districts on the second level
+        url = (
+            "https://www.landkreis-harburg.de/ajax/abfall_gebiete_struktur_select.html"
+        )
+        params = {
+            "parent": id,
+            "ebene": 1,
+            "portal": 1,
+            "selected_ebene": 0,
+        }
+        r = session.get(url, params=params, headers=HEADERS)
+        if r.status_code != 200:
+            raise Exception(f"Error: failed to fetch second url: {url}")
+
+        # Get the IDs of the districts on the second level
+        id = self.parse_level(r.text, 2)
+
+        # Get the IDs of the third level - if applicable
+        if self._districts[3 - 1] is not None:
+            # Get the IDs of the districts on the third level
+            params = {
+                "parent": id,
+                "ebene": 2,
+                "portal": 1,
+                "selected_ebene": 0,
+            }
+            r = session.get(url, params=params, headers=HEADERS)
+            if r.status_code != 200:
+                raise Exception(f"Error: failed to fetch third url: {url}")
+
+            # Get the IDs of the districts on the third level
+            id = self.parse_level(r.text, 3)
+
+        # Prepare data for the real web request
+        url = "https://www.landkreis-harburg.de/abfallkalender/abfallkalender_struktur_daten_suche.html"
+        params = {
+            "selected_ebene": id,
+            "owner": 20100,
+        }
+        r = session.get(url, params=params, headers=HEADERS)
+
+        # Sometimes there is no garbage calendar available
+        if "Es sind keine Abfuhrbezirke hinterlegt." in r.text:
+            raise Exception(
+                f'Error: "Es sind keine Abfuhrbezirke hinterlegt." for "{self._districts[3-1]}". Please use different input data.'
+            )
+
+        soup = BeautifulSoup(r.text, features="html.parser")
+        links = soup.find_all("a")
+        ical_url = ""
+        for any_link in links:
+            if " als iCal" in any_link.text:
+                ical_url = any_link.get("href")
+
+        if "ical.html" not in ical_url:
+            raise Exception("No ical Link in the result: " + str(links))
+
+        # Get the final data
+        r = requests.get(ical_url, headers=HEADERS)
+        if not r.ok:
+            raise Exception(f"Error: failed to fetch url: {ical_url}")
+
+        # Parse ics file
+        dates = self._ics.convert(r.text)
+
+        entries = []
+        for d in dates:
+            entries.append(Collection(d[0], d[1]))
+        return entries
+
+    def parse_level(self, response, level):
+        soup = BeautifulSoup(response, features="html.parser")
+        select_content = soup.find_all("select", id=f"strukturEbene{level}")
+        soup = BeautifulSoup(str(select_content), features="html.parser")
+        options_content = soup.find_all("option")
+        level_ids = {}
+        for option in options_content:
+            # Ignore the "Bitte wählen..."
+            if option.get("value") != "0":
+                level_ids[option.text] = option.get("value")
+
+        if level_ids == {}:
+            raise Exception(f"Error: Level {level} Dictionary empty")
+
+        if self._districts[level - 1] not in level_ids:
+            raise Exception(
+                f"Error: District {self._districts[level]} is not in the dictionary: {level_ids}"
+            )
+
+        return level_ids[self._districts[level - 1]]
--- doc/source/aw_harburg_de.md
+++ doc/source/aw_harburg_de.md
@@ -9,118 +9,44 @@ waste_collection_schedule:
   sources:
     - name: aw_harburg
       args:
-          district_level_1: "Hanstedt"
-          district_level_2: "Evendorf"
+          level_1: LEVEL_1
+          level_2: LEVEL_2
+          level_3: LEVEL_3
 ```
 
 ### Configuration Variables
 
-**district_level_1**<br>
+**level_1**<br>
 *(string) (required)*
 
-**district_level_2**<br>
+**level_2**<br>
 *(string) (required)*
 
-**district_level_3**<br>
-*(string) (optional - depending on district_level_2)*
+**level_3**<br>
+*(string) (optional - depending on level_2)*
 
 ## Example
 
 ```yaml
 waste_collection_schedule:
   sources:
-    - name: aw_harburg
+    - name: aw_harburg_de
       args:
-          district_level_1: "Buchholz"
-          district_level_2: "Buchholz mit Steinbeck (ohne Reindorf)"
-          district_level_3: "Seppenser Mühlenweg Haus-Nr. 1 / 2"
-      customize:
-        - type: Biotonne
-          alias: Biomüll
-          show: true
-        - type: Grünabfall
-          alias: Grünabfall
-          show: true
-        - type: Gelber Sack
-          alias: Gelber Sack
-          show: true
-        - type: Hausmüll 14-täglich
-          alias: Hausmüll 2wö
-          show: true
-        - type: Hausmüll 4-wöchentlich
-          alias: Hausmüll 4wö
-          show: true
-        - type: Altpapier
-          alias: Papier
-          show: true
+          level_1: "Hanstedt"
+          level_2: "Evendorf"
 ```
 
-Use `sources.customize` to filter or rename the waste types:
 
 ```yaml
 waste_collection_schedule:
   sources:
-    - name: aw_harburg
+    - name: aw_harburg_de
       args:
-          district_level_1: "Buchholz"
-          district_level_2: "Buchholz mit Steinbeck (ohne Reindorf)"
-          district_level_3: "Seppenser Mühlenweg Haus-Nr. 1 / 2"
-      customize:
-        - type: Biotonne
-          alias: Biomüll
-          show: true
-        - type: Grünabfall
-          alias: Grünabfall
-          show: true
-        - type: Gelber Sack
-          alias: Gelber Sack
-          show: true
-        - type: Hausmüll 14-täglich
-          alias: Hausmüll 2wö
-          show: true
-        - type: Hausmüll 4-wöchentlich
-          alias: Hausmüll 4wö
-          show: true
-        - type: Altpapier
-          alias: Papier
-          show: true
-
-sensor:
-  # Nächste Müllabholung
-  - platform: waste_collection_schedule
-    name: Nächste Leerung
-
-  # Nächste Biomüll Leerung
-  - platform: waste_collection_schedule
-    name: Nächste Biomüll Leerung
-    types: Biomüll
-
-  # Nächste Grünabfall Abholung
-  - platform: waste_collection_schedule
-    name: Nächste Grünabfall Abholung
-    types: Grünabfall
-
-  # Nächste Gelber Sack Abholung
-  - platform: waste_collection_schedule
-    name: Nächste Gelber Sack Abholung
-    types: Gelber Sack
-
-  # Nächste Hausmüll 14-täglich Leerung
-  - platform: waste_collection_schedule
-    name: Nächste Hausmüll 2wö Leerung
-    types: Hausmüll 2wö
-
-  # Nächste Hausmüll 4-wöchentlich Leerung
-  - platform: waste_collection_schedule
-    name: Nächste Hausmüll 4wö Leerung
-    types: Hausmüll 4wö
-
-  # Nächste Papier Leerung
-  - platform: waste_collection_schedule
-    name: Nächste Papier Leerung
-    types: Papier
+          level_1: "Buchholz"
+          level_2: "Buchholz mit Steinbeck (ohne Reindorf)"
+          level_3: "Seppenser Mühlenweg Haus-Nr. 1 / 2"
 ```
 
 ## How to get the source arguments
 
-Check [AW Harburg Abfallkalender](https://www.landkreis-harburg.de/bauen-umwelt/abfallwirtschaft/abfallkalender/) if you need two or three levels of entries in the config. The strings need to be written in the exact same way as in the webinterface e.g. "Bremer Straße Haus-Nr. 93 - 197 / 78 - 158"
+Check [AW Harburg Abfallkalender](https://www.landkreis-harburg.de/bauen-umwelt/abfallwirtschaft/abfallkalender/) if you need two or three levels of entries in the config. The strings need to be written in the exact same way as in the webinterface e.g. "Bremer Straße Haus-Nr. 93 - 197 / 78 - 158".
