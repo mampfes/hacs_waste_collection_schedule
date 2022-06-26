5783c65cc4c506813db4d98ffad97f835cc0503d
mampfes <mampfes@users.noreply.github.com>
2022-06-26 10:22:26 +0200

    refactor alw_wf_de

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/alw_wf_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/alw_wf_de.py
@@ -1,8 +1,7 @@
 import datetime
 import json
-import pytz
-import logging
 
+import pytz
 import requests
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
@@ -11,7 +10,10 @@ DESCRIPTION = "Source for ALW Wolfenbüttel."
 URL = "https://abfallapp.alw-wf.de"
 TEST_CASES = {
     "Linden alte Straße": {"ort": "Linden mit Okertalsiedlung", "strasse": "Siedlung"},
-    "Linden neuere Straße": {"ort": "Linden mit Okertalsiedlung", "strasse": "Kleingartenweg"},
+    "Linden neuere Straße": {
+        "ort": "Linden mit Okertalsiedlung",
+        "strasse": "Kleingartenweg",
+    },
     "Dettum": {"ort": "Dettum", "strasse": "Egal!"},
 }
 
@@ -24,10 +26,11 @@ AUTH_DATA = {
         "Username": "ALW",
         "Password": "ALW",
     },
-    "request": {}
+    "request": {},
 }
 ALL_STREETS = "Alle Straßen"
 BIN_TYPE_NORMAL = "0"
+
 
 class Source:
     def __init__(self, ort, strasse):
@@ -41,21 +44,22 @@ class Source:
         r = requests.post(f"{URL}/GetOrte.php", data=auth_params, verify=False)
         orte = r.json()
         if orte["result"][0]["StatusCode"] != 200:
-            _LOGGER.error(f"Error getting Orte: {orte['result'][0]['StatusMsg']}")
-            return []
+            raise Exception(f"Error getting Orte: {orte['result'][0]['StatusMsg']}")
+
         orte = orte["result"][0]["result"]
         orte = {i["Name"]: i["ID"] for i in orte}
         ort_id = orte.get(self._ort, None)
 
-        if ort_id == None:
-            _LOGGER.error(f"Error finding Ort {self._ort}")
-            return []
+        if ort_id is None:
+            raise Exception(f"Error finding Ort {self._ort}")
 
         r = requests.post(f"{URL}/GetStrassen.php", data=auth_params, verify=False)
         strassen = r.json()
         if strassen["result"][0]["StatusCode"] != 200:
-            _LOGGER.error(f"Error getting Straßen: {strassen['result'][0]['StatusMsg']}")
-            return []
+            raise Exception(
+                f"Error getting Straßen: {strassen['result'][0]['StatusMsg']}"
+            )
+
         strassen = strassen["result"][0]["result"]
         strasse_id = None
         for strasse in strassen:
@@ -66,33 +70,38 @@ class Source:
                 break
             continue
 
-        if strasse_id == None:
-            _LOGGER.error(f"Error finding Straße {self._strasse}")
-            return []
+        if strasse_id is None:
+            raise Exception(f"Error finding Straße {self._strasse}")
 
         r = requests.post(f"{URL}/GetArten.php", data=auth_params, verify=False)
         arten = r.json()
         if arten["result"][0]["StatusCode"] != 200:
-            _LOGGER.error(f"Error getting Arten: {arten['result'][0]['StatusMsg']}")
-            return []
+            raise Exception(f"Error getting Arten: {arten['result'][0]['StatusMsg']}")
+
         arten = arten["result"][0]["result"]
         arten = [i for i in arten if i["Art"] == BIN_TYPE_NORMAL]
         arten = {int(i["Wertigkeit"]): i["Name"] for i in arten}
 
         entries = []
-        r = requests.post(f"{URL}/GetTermine.php/{strasse_id}", data=auth_params, verify=False)
+        r = requests.post(
+            f"{URL}/GetTermine.php/{strasse_id}", data=auth_params, verify=False
+        )
         termine = r.json()
         if termine["result"][0]["StatusCode"] != 200:
-            _LOGGER.error(f"Error getting Termine: {termine['result'][0]['StatusMsg']}")
-            return []
+            raise Exception(
+                f"Error getting Termine: {termine['result'][0]['StatusMsg']}"
+            )
+
         termine = termine["result"][0]["result"]
         for termin in termine:
-            ts = int(termin["DatumLong"])/1000
+            ts = int(termin["DatumLong"]) / 1000
             # Timestamps are unix with milliseconds but not UTC...
-            date = datetime.datetime.fromtimestamp(ts, tz=pytz.timezone("Europe/Berlin")).date()
+            date = datetime.datetime.fromtimestamp(
+                ts, tz=pytz.timezone("Europe/Berlin")
+            ).date()
             types = int(termin["Abfallwert"])
             for art in arten:
                 if types & art:
-                    entries.append(Collection(date, arten[art], date))
+                    entries.append(Collection(date, arten[art]))
 
         return entries
