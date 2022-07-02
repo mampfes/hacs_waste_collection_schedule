21770f7ce1798642acf56e51d731e47b985dcdec
Johnnie Blows <johnnie.blows@gmail.com>
2022-07-02 22:32:33 +0100

    Add Huntingdonshire District Council
    Signed-off-by: Johnnie Blows <johnnie.blows@gmail.com>

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/huntingdonshire_gov_uk.py
@@ -0,0 +1,53 @@
+import json
+from datetime import datetime
+
+import requests
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
+
+TITLE = "York.gov.uk"
+DESCRIPTION = "Source for Huntingdonshire.gov.uk services for Huntingdonshire District Council."
+URL = "https://york.gov.uk"
+TEST_CASES = {
+    "Wells Close, Brampton": {"uprn": "100090123510"},
+    "Inkerman Rise, St. Neots": {"uprn": "10000144271"},
+}
+
+ICONS = {
+    "Refuse": "mdi:trash-can",
+    "Recycling": "mdi:recycle",
+    "Garden": "mdi:leaf",
+}
+
+
+class Source:
+    def __init__(self, uprn):
+        self._uprn = uprn
+
+    def fetch(self):
+        # get json file
+        r = requests.get(
+            f"https://servicelayer3c.azure-api.net/wastecalendar/collection/search/{self._uprn}?authority=HDC&take=20"
+        )
+
+        # extract data from json
+        data = json.loads(r.text)
+
+        entries = []
+
+        collections = r.json()["collections"]
+        entries = []
+
+        for collection in collections:
+            for round_type in collection["roundTypes"]:
+                entries.append(
+                    Collection(
+                        date=datetime.strptime(
+                            collection["date"], "%Y-%m-%dT%H:%M:%SZ"
+                        ).date(),
+                        t=round_type.title(),
+                        icon=ICONS.get(round_type),
+                    )
+                )
+
+
+        return entries
