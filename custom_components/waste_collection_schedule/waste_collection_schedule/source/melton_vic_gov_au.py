339fe807de6b24e8bd87aa6dcdf2b8b24372c5ce
ankycooper <ankycooper@gmail.com>
2022-07-14 09:19:34 +1000

    Added source for melton.vic.gov.au

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/melton_vic_gov_au.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/melton_vic_gov_au.py
@@ -1,12 +1,10 @@
 import logging
-import re
-import typing
 from datetime import datetime
+import re
 
 import requests
 from bs4 import BeautifulSoup
-from waste_collection_schedule import Collection
-
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = 'Melton City Council'
 DESCRIPTION = 'Source for Melton City Council rubbish collection.'
@@ -15,120 +13,72 @@ TEST_CASES = {
     'Tuesday A': {'street_address': '23 PILBARA AVENUE BURNSIDE 3023'},
     'Tuesday A Geolocation ID': {'geolocation_id': '00faf1f7-9aa0-4b2c-a9b9-54c29401e68c'},
     'Tuesday B': {'street_address': '29 COROWA CRESCENT BURNSIDE 3023'},
-    'Thursday A': {'street_address': '2 ASPIRE BOULEVARD FRASER RISE 3336'},
-    'Thursday B': {'street_address': '17 KEYNES CIRCUIT FRASER RISE 3336'}
+    'Wednesday A': {'street_address': '2 ASPIRE BOULEVARD FRASER RISE 3336'},
+    'Wednesday B': {'street_address': '17 KEYNES CIRCUIT FRASER RISE 3336'}
 }
 
 _LOGGER = logging.getLogger(__name__)
 
 ICON_MAP = {
-    'green waste': 'mdi:leaf',
-    'recycling': 'mdi:recycle'
+    "Food and Green Waste": "mdi:leaf",
+    "Hard Waste": "mdi:sofa",
+    "Recycling": "mdi:recycle",
 }
 
 
-class SourceConfigurationError(ValueError):
-    pass
-
-
-class SourceParseError(ValueError):
-    pass
-
-
 class Source:
-    OC_GEOLOCATION_SEARCH_URL = 'https://www.melton.vic.gov.au/api/v1/myarea/search'
-
-    OC_SESSION_URL = 'https://www.melton.vic.gov.au/My-Area'
-    OC_CALENDAR_URL = 'https://www.melton.vic.gov.au/ocapi/Public/myarea/wasteservices'
-
-    OC_RE_DATE_STR = re.compile(r'[^\s]+\s(\d{1,2}/\d{1,2}/\d{4})')
-
-    def __init__(self, street_address: typing.Optional[str] = None, geolocation_id: typing.Optional[str] = None):
-        if street_address is None and geolocation_id is None:
-            raise SourceConfigurationError('Either street_address or geolocation_id must have a value')
-
+    def __init__(self, street_address):
         self._street_address = street_address
-        self._geolocation_id = geolocation_id
 
-    @property
-    def geolocation_id(self) -> str:
-        if self._geolocation_id is None:
-            # Search for geolocation ID
-            geolocation_response = requests.get(
-                self.OC_GEOLOCATION_SEARCH_URL,
-                params={
-                    'keywords': self._street_address,
-                    'maxresults': 1
-                }
-            )
-            geolocation_response.raise_for_status()
+    def fetch(self):
+        session = requests.Session()
 
-            # Pull ID from results
-            geolocation_result = geolocation_response.json()
-            _LOGGER.debug(f"Search response: {geolocation_response!r}")
-
-            if 'success' in geolocation_result and not geolocation_result['success']:
-                raise SourceParseError('Unspecified server-side error when searching address')
-
-            if 'Items' not in geolocation_result or \
-                    geolocation_result['Items'] is None or \
-                    len(geolocation_result['Items']) < 1:
-                raise SourceParseError('Expected list of locations from address search, got empty or missing list')
-
-            geolocation_data = geolocation_result['Items'][0]
-
-            if 'Id' not in geolocation_data:
-                raise SourceParseError('Location in address search result but missing geolocation ID')
-
-            self._geolocation_id = geolocation_data['Id']
-            _LOGGER.info(f"Address {self._street_address} mapped to geolocation ID {self._geolocation_id}")
-
-        return self._geolocation_id
-
-    def fetch(self) -> typing.List[Collection]:
-        # Calendar lookup cares about a cookie, so a Session must be used
-        calendar_session = requests.Session()
-
-        calendar_request = calendar_session.get(self.OC_SESSION_URL)
-        calendar_request.raise_for_status()
-
-        calendar_request = calendar_session.get(
-            self.OC_CALENDAR_URL,
-            params={
-                'geolocationid': self.geolocation_id,
-                'ocsvclang': 'en-AU'
-            }
+        response = session.get(
+            "https://www.melton.vic.gov.au/My-Area"
         )
-        calendar_request.raise_for_status()
+        response.raise_for_status()
 
-        calendar_result = calendar_request.json()
-        _LOGGER.debug(f"Calendar response: {calendar_result!r}")
+        response = session.get(
+            "https://www.melton.vic.gov.au/api/v1/myarea/search",
+            params={"keywords": self._street_address},
+        )
+        response.raise_for_status()
+        addressSearchApiResults = response.json()
+        if (
+            addressSearchApiResults["Items"] is None
+            or len(addressSearchApiResults["Items"]) < 1
+        ):
+            _LOGGER.error(
+                f"Address search for '{self._street_address}' returned no results. Check your address on https://www.melton.vic.gov.au/My-Area"
+            )
+            return []
 
-        if 'success' in calendar_result and not calendar_result['success']:
-            raise SourceParseError('Unspecified server-side error when getting calendar')
+        addressSearchTopHit = addressSearchApiResults["Items"][0]
+        _LOGGER.debug("Address search top hit: %s", addressSearchTopHit)
 
-        # Extract entries from bundled HTML
-        calendar_parser = BeautifulSoup(calendar_result['responseContent'], 'html.parser')
+        geolocationid = addressSearchTopHit["Id"]
+        _LOGGER.debug("Geolocationid: %s", geolocationid)
 
-        pickup_entries = []
+        response = session.get(
+            "https://www.melton.vic.gov.au/ocapi/Public/myarea/wasteservices?ocsvclang=en-AU",
+            params={"geolocationid": geolocationid},
+        )
+        response.raise_for_status()
 
-        for element in calendar_parser.find_all('article'):
-            _LOGGER.debug(f"Parsing collection: {element!r}")
+        wasteApiResult = response.json()
+        _LOGGER.debug("Waste API result: %s", wasteApiResult)
 
-            waste_type = element.h3.string
+        soup = BeautifulSoup(wasteApiResult["responseContent"], "html.parser")
 
-            # Extract and parse collection date
-            waste_date_match = self.OC_RE_DATE_STR.match(element.find(class_='next-service').string.strip())
+        entries = []
+        for article in soup.find_all("article"):
+            waste_type = article.h3.string
+            icon = ICON_MAP.get(waste_type, "mdi:trash-can")
+            next_pickup = article.find(class_="next-service").string.strip()
+            if re.match("[^\s]* \d{1,2}\/\d{1,2}\/\d{4}", next_pickup):
+                next_pickup_date = datetime.strptime(
+                    next_pickup.split(sep=" ")[1], "%d/%m/%Y"
+                ).date()
+                entries.append(Collection(date=next_pickup_date, t=waste_type, icon=icon))
 
-            if waste_date_match is None:
-                continue
-
-            waste_date = datetime.strptime(waste_date_match[1], '%d/%m/%Y').date()
-
-            # Base icon on type
-            waste_icon = ICON_MAP.get(waste_type.lower(), 'mdi:trash-can')
-
-            pickup_entries.append(Collection(waste_date, waste_type, waste_icon))
-            _LOGGER.info(f"Collection for {waste_type} (icon: {waste_icon}) on {waste_date}")
-
-        return pickup_entries
+        return entries
