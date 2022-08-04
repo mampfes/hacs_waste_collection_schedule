8df2dd5a92d72b7d3690e32301d5b73abb1338fe
steffenrapp <88974099+steffenrapp@users.noreply.github.com>
2022-08-04 21:59:03 +0200

    Update lrasha_de.py

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/lrasha_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/lrasha_de.py
@@ -5,8 +5,8 @@ from waste_collection_schedule.service.ICS import ICS
 
 
 TITLE = "Landkreis Schwäbisch Hall"
-DESCRIPTION = "Source for lrasha.de - Landkreis Schwäbisch Hall"  # Describe your source
-URL = "https://exchange.cmcitymedia.de/landkreis-schwaebisch-hallt3/wasteCalendarExport.php"    # Insert url to service homepage
+DESCRIPTION = "Source for lrasha.de - Landkreis Schwäbisch Hall"
+URL = "https://exchange.cmcitymedia.de/landkreis-schwaebisch-hallt3/wasteCalendarExport.php"
 TEST_CASES = { # Insert arguments for test cases using test_sources.py script
     "TestName": {"arg1": 100, "arg2": "street"}
 }
@@ -15,15 +15,26 @@ TEST_CASES = { # Insert arguments for test cases using test_sources.py script
 class Source:
     def __init__(self, location):
         self._location = location
+        self._ics = ICS()
 
     def fetch(self):
+        # get ics file
+        full_url = {URL} + "?location=" + self._location
+        r = requests.get(full_url)
+        
+        # parse ics file
+        r.encoding = "utf-8"
+        dates = self._ics.convert(r.text)
+        
         entries = []
 
-        entries.append(
-            Collection(
-                datetime.datetime(2020, 4, 11),
-                "Waste Type",
-            )
-        )
+#        entries.append(
+#            Collection(
+#                datetime.datetime(2020, 4, 11),
+#                "Waste Type",
+#            )
+#        )
 
+        for d in dates:
+            entries.append(Collection(d[0], d[1]))
         return entries
