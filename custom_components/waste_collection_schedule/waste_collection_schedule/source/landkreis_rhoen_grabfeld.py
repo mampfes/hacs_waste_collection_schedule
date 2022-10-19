69cfbd18ba05079eba7a726c9ddb68eb04064e85
Random-Cow <jonathan.kroener@gmail.com>
2022-10-19 16:38:26 +0200

    fix errors and implement review improvements

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/landkreis_rhoen_grabfeld.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/landkreis_rhoen_grabfeld.py
@@ -6,6 +6,15 @@ from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 TITLE = "Source for Rhön Grabfeld"
 DESCRIPTION = "Source for Rhönn Grabfeld uses service by offizium."
 URL = 'https://fs-api-rg.offizium.com/abfalltermine'
+ICON_MAP = {
+    "Restmüll/Gelber Sack/Biotonne": "mdi:trash-can",
+    "Papiersammlung": "mdi:package-variant",
+    "Problemmüllsammlung": "mdi:biohazard"
+}
+EVENT_BLACKLIST = ['Wertstoffhof Mellrichstadt',
+                   'Wertstoffhof Bad Königshofen', 'Wertstoffzentrum Bad Neustadt',
+                   'Wertstoffsammelstelle Ostheim',
+                   'Wertstoffsammelstelle Bischofsheim']
 TEST_CASES = {
     "City only": {"city": "Ostheim"},
     "City + District": {"city": "Ostheim", "district": "Oberwaldbehrungen"},
@@ -18,11 +27,6 @@ class Source:
     def __init__(self, city: str = None, district: str = None):
         self._city = city
         self._district = district
-        self._iconMap = {
-            "Restmüll/Gelber Sack/Biotonne": "mdi:trash-can",
-            "Papiersammlung": "mdi:package-variant",
-            "Problemmüllsammlung": "mdi:biohazard"
-        }
 
     def fetch(self):
         now = datetime.datetime.now().date()
@@ -37,13 +41,13 @@ class Source:
         entries = []
         for event in r.json():
             # filter out Sammelstellen, Wertstoffhof and Wertstoffzentrum
-            if event["muellart"] in self._iconMap:
+            if event["muellart"] not in EVENT_BLACKLIST:
                 entries.append(
                     Collection(
                         date=datetime.datetime.fromisoformat(
                             event["termin"]).date(),
                         t=event["muellart"],
-                        icon=self._iconMap.get(
+                        icon=ICON_MAP.get(
                             event["muellart"], "mdi:trash-can")
                     )
                 )
