e3363b67b3c2fedc5a320d2128835b5c0043643b
Finn Freitag <dev@freitag.fi>
2022-11-26 13:41:59 +0100

    Initial commit for Nordwestmecklenburg source

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/geoport_nwm_de.py
@@ -0,0 +1,53 @@
+import datetime
+import requests
+import urllib
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
+from waste_collection_schedule.service.ICS import ICS
+
+TITLE = "Landkreis Nordwestmecklenburg"
+DESCRIPTION = "Source for Landkreis Nordwestmecklenburg"
+URL = "https://www.geoport-nwm.de/de/abfuhrtermine-geoportal.html"
+TEST_CASES = {
+    "Rüting": {"district": "Rüting"},
+    # Ortsteil_Rueting
+    "Grevenstein u.": {"district": "Grevenstein u. Ausbau"},
+    # Ortsteil_Grevenstein_u_Ausbau
+    "Seefeld": {"district": "Seefeld/ Testorf- Steinfort"},
+    # Ortsteil_Seefeld_Testorf_Steinfort
+    "1100l": {"district": "Groß Stieten (1.100 l Behälter)"}
+    # Ortsteil_Gross_Stieten_1100_l
+}
+
+
+class Source:
+    def __init__(self, district):
+        self._district = district
+        self._ics = ICS()
+
+    def fetch(self):
+        arg = convert_to_arg(self._district)
+        today = datetime.date.today()
+        year = today.year
+        r = requests.get(
+            f"https://www.geoport-nwm.de/nwm-download/Abfuhrtermine/ICS/{year}/{arg}.ics")
+
+        dates = self._ics.convert(r.text)
+
+        entries = []
+        for d in dates:
+            entries.append(Collection(d[0], d[1]))
+        return entries
+
+
+def convert_to_arg(district):
+    district = district.replace("(1.100 l Behälter)", "1100_l")
+    district = district.replace("ü", "ue")
+    district = district.replace("ö", "oe")
+    district = district.replace("ä", "ae")
+    district = district.replace("ß", "ss")
+    district = district.replace("/", "")
+    district = district.replace("-", "")
+    district = district.replace(".", "")
+    district = district.replace(" ", "_")
+    arg = urllib.parse.quote("Ortsteil_" + district)
+    return arg
