4febf5b393c57b79917931999b1600f13d098e78
mampfes <mampfes@users.noreply.github.com>
2022-12-17 14:37:41 +0100

    use params instead of encoded url

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/wermelskirchen_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/wermelskirchen_de.py
@@ -1,6 +1,6 @@
-import requests
 from datetime import datetime
-import urllib.parse
+
+import requests
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 from waste_collection_schedule.service.ICS import ICS
 
@@ -16,38 +16,36 @@ TEST_CASES = {
 
 INFOS = {
     "Restabfall 2-woechentlich": {
-        "icon": "mdi:trash-can", 
-        "image": "https://abfallkalender.citkomm.de/fileadmin/_processed_/1/b/csm_Restmuell_6b2b32c774.png" 
+        "icon": "mdi:trash-can",
+        "image": "https://abfallkalender.citkomm.de/fileadmin/_processed_/1/b/csm_Restmuell_6b2b32c774.png",
     },
     "Restabfall 4-woechentlich": {
         "icon": "mdi:trash-can",
-        "image": "https://abfallkalender.citkomm.de/fileadmin/_processed_/1/b/csm_Restmuell_6b2b32c774.png"
+        "image": "https://abfallkalender.citkomm.de/fileadmin/_processed_/1/b/csm_Restmuell_6b2b32c774.png",
     },
     "Restabfall 6-woechentlich": {
         "icon": "mdi:trash-can",
-        "image": "https://abfallkalender.citkomm.de/fileadmin/_processed_/1/b/csm_Restmuell_6b2b32c774.png"
+        "image": "https://abfallkalender.citkomm.de/fileadmin/_processed_/1/b/csm_Restmuell_6b2b32c774.png",
     },
     "Gelber Sack": {
-        "icon": "mdi:recycle-variant", 
-        "image": "https://abfallkalender.citkomm.de/fileadmin/_processed_/f/4/csm_GelbeTonne_24ffc276b2.png"
+        "icon": "mdi:recycle-variant",
+        "image": "https://abfallkalender.citkomm.de/fileadmin/_processed_/f/4/csm_GelbeTonne_24ffc276b2.png",
     },
     "Papier": {
         "icon": "mdi:package-variant",
-        "image": "https://abfallkalender.citkomm.de/fileadmin/_processed_/2/3/csm_Papiertonne_919ed3b5da.png"
+        "image": "https://abfallkalender.citkomm.de/fileadmin/_processed_/2/3/csm_Papiertonne_919ed3b5da.png",
     },
     "Biotonne": {
         "icon": "mdi:leaf",
-        "image": "https://abfallkalender.citkomm.de/fileadmin/_processed_/6/f/csm_Biotonne_wk_ae1b0e61aa.png"    
+        "image": "https://abfallkalender.citkomm.de/fileadmin/_processed_/6/f/csm_Biotonne_wk_ae1b0e61aa.png",
     },
     "Schadstoffsammlung": {
         "icon": "mdi:bottle-tonic-skull",
-        "image": "https://abfallkalender.citkomm.de/fileadmin/_processed_/4/2/csm_sondermuell_62f5701a7b.png"
+        "image": "https://abfallkalender.citkomm.de/fileadmin/_processed_/4/2/csm_sondermuell_62f5701a7b.png",
     },
-    "Weihnachtsbaum": {
-        "icon": "mdi:pine-tree",
-        "image": ""
-    },
+    "Weihnachtsbaum": {"icon": "mdi:pine-tree", "image": ""},
 }
+
 
 class Source:
     def __init__(self, street, house_number):
@@ -59,16 +57,22 @@ class Source:
         # the url contains the current year, but this doesn't really seems to matter at least for the ical, since the result is always the same
         # still replace it for compatibility sake
         now = datetime.now()
-        url = (
-            "https://abfallkalender.citkomm.de/wermelskirchen/abfallkalender-"+
-            str(now.year)+
-            "/ics/FrontendIcs.html?tx_citkoabfall_abfallkalender%5Bstrasse%5D="+
-            urllib.parse.quote_plus(self._street)+
-            "&tx_citkoabfall_abfallkalender%5Bhausnummer%5D="+
-            urllib.parse.quote_plus(self._house_number)+
-            "&tx_citkoabfall_abfallkalender%5Babfallarten%5D%5B0%5D=86&tx_citkoabfall_abfallkalender%5Babfallarten%5D%5B1%5D=85&tx_citkoabfall_abfallkalender%5Babfallarten%5D%5B2%5D=84&tx_citkoabfall_abfallkalender%5Babfallarten%5D%5B3%5D=82&tx_citkoabfall_abfallkalender%5Babfallarten%5D%5B4%5D=81&tx_citkoabfall_abfallkalender%5Babfallarten%5D%5B5%5D=80&tx_citkoabfall_abfallkalender%5Babfallarten%5D%5B6%5D=79&tx_citkoabfall_abfallkalender%5Babfallarten%5D%5B7%5D=76&tx_citkoabfall_abfallkalender%5Babfallarten%5D%5B8%5D=75&tx_citkoabfall_abfallkalender%5Babfallarten%5D%5B9%5D=74"
-        )
-        r = requests.get(url)
+        url = f"https://abfallkalender.citkomm.de/wermelskirchen/abfallkalender-{str(now.year)}/ics/FrontendIcs.html"
+        params = {
+            "tx_citkoabfall_abfallkalender[strasse]": self._street,
+            "tx_citkoabfall_abfallkalender[hausnummer]": self._house_number,
+            "tx_citkoabfall_abfallkalender[abfallarten][0]": 86,
+            "tx_citkoabfall_abfallkalender[abfallarten][1]": 85,
+            "tx_citkoabfall_abfallkalender[abfallarten][2]": 84,
+            "tx_citkoabfall_abfallkalender[abfallarten][3]": 82,
+            "tx_citkoabfall_abfallkalender[abfallarten][4]": 81,
+            "tx_citkoabfall_abfallkalender[abfallarten][5]": 80,
+            "tx_citkoabfall_abfallkalender[abfallarten][6]": 79,
+            "tx_citkoabfall_abfallkalender[abfallarten][7]": 76,
+            "tx_citkoabfall_abfallkalender[abfallarten][8]": 75,
+            "tx_citkoabfall_abfallkalender[abfallarten][9]": 74,
+        }
+        r = requests.get(url, params=params)
         r.raise_for_status()
 
         r.encoding = "utf-8"
@@ -76,11 +80,8 @@ class Source:
 
         entries = []
         for d in dates:
-            info = INFOS.get(d[1], {"icon": "mdi:trash-can", "image": ""})    
-            entries.append(Collection(
-                d[0], 
-                d[1],
-                picture = info['image'],
-                icon = info['icon']
-            ))
+            info = INFOS.get(d[1], {"icon": "mdi:trash-can", "image": ""})
+            entries.append(
+                Collection(d[0], d[1], picture=info["image"], icon=info["icon"])
+            )
         return entries
