d80e74a2412145df5818dd0ea01605cb75f15479
mampfes <mampfes@users.noreply.github.com>
2022-12-24 11:01:33 +0100

    fix source erlangen_hoechstadt_de during year transition

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/erlangen_hoechstadt_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/erlangen_hoechstadt_de.py
@@ -1,9 +1,8 @@
-import requests
 import datetime
+
+import requests
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 from waste_collection_schedule.service.ICS import ICS
-
-import urllib
 
 TITLE = "Landkreis Erlangen-Höchstadt"
 DESCRIPTION = "Source for Landkreis Erlangen-Höchstadt"
@@ -11,7 +10,7 @@ URL = "https://www.erlangen-hoechstadt.de/"
 TEST_CASES = {
     "Höchstadt": {"city": "Höchstadt", "street": "Böhmerwaldstraße"},
     "Brand": {"city": "Eckental", "street": "Eckenhaid, Amselweg"},
-    "Ortsteile": {"city": "Wachenroth", "street": "Wachenroth Ort ink. aller Ortsteile"}
+    "Ortsteile": {"city": "Wachenroth", "street": "Ort inkl. aller Ortsteile"},
 }
 
 
@@ -22,20 +21,25 @@ class Source:
         self._ics = ICS(split_at=" / ")
 
     def fetch(self):
-        city = self._city.upper()
-        street = self._street
         today = datetime.date.today()
-        year = today.year
-
-        payload = {"ort": city, "strasse": street,
-                   "abfallart": "Alle", "jahr": year}
-        r = requests.get(
-            "https://www.erlangen-hoechstadt.de/komx/surface/dfxabfallics/GetAbfallIcs", params=payload
-        )
-        r.encoding = r.apparent_encoding
-        dates = self._ics.convert(r.text)
+        dates = self.fetch_year(today.year)
+        if today.month == 12:
+            dates.extend(self.fetch_year(today.year + 1))
 
         entries = []
         for d in dates:
             entries.append(Collection(d[0], d[1]))
         return entries
+
+    def fetch_year(self, year):
+        city = self._city.upper()
+        street = self._street
+
+        payload = {"ort": city, "strasse": street, "abfallart": "Alle", "jahr": year}
+        r = requests.get(
+            "https://www.erlangen-hoechstadt.de/komx/surface/dfxabfallics/GetAbfallIcs",
+            params=payload,
+        )
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return self._ics.convert(r.text)
