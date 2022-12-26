a961b686f31a2e73916eb67d5c8cc11df8960ba9
bbr111 <braeuer.ben@gmail.com>
2022-12-26 14:35:20 +0100

    Add SRV återvinning - changes
    change filenames
    change sourcename in doc
    remove old files
    remove dates variable
    use params in requests

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/srvatervinning_se.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/srvatervinning_se.py
@@ -3,8 +3,8 @@ import requests
 from datetime import datetime
 from waste_collection_schedule import Collection
 
-TITLE = "SRV, Sweden"
-DESCRIPTION = "Source for SRV, Sweden"
+TITLE = "SRV återvinning AB Sweden"
+DESCRIPTION = "Source for SRV återvinning AB, Sweden"
 URL = "https://www.srvatervinning.se/sophamtning/privat/hamtinformation-och-driftstorningar"
 TEST_CASES = {
   "Skansvägen" : {"address":"Skansvägen" },
@@ -19,7 +19,12 @@ class Source:
 
     def fetch(self):
 
-        r = requests.get('https://www.srvatervinning.se/rest-api/srv-slamsok-rest-new/search?query='+self._address+'&city=')
+        params = {
+            "query" : self._address,
+            "city" : "",
+        }
+        url = 'https://www.srvatervinning.se/rest-api/srv-slamsok-rest-new/search'
+        r = requests.get(url, params)
 
         if r.status_code != 200:
             _LOGGER.error("Error querying calender data")
@@ -32,7 +37,6 @@ class Source:
         for container in data["results"][0]["containers"]:
             type=container["contentType"]
             for calentry in container["calendars"]:
-                dates =  {calentry["startDate"],type}
                 date_obj = datetime.strptime(calentry["startDate"], '%Y-%m-%d').date()
                 entries.append(Collection(date_obj,type))
 
