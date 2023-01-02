caeafb135bb9126427a8a96abda92c294a5c5f4b
Andre Basche <andre.basche@gmail.com>
2023-01-02 02:42:14 +0100

    Refactor

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/meinawb_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/meinawb_de.py
@@ -46,6 +46,7 @@ class Source:
         self._street = street
         self._house_number = house_number
         self._address_suffix = address_suffix
+        self._boundary = "WebKitFormBoundary" + "".join(random.sample(string.ascii_letters + string.digits, 16))
 
     def __str__(self):
         return f"{self._city} {self._street} {self._house_number} {self._address_suffix}"
@@ -63,33 +64,38 @@ class Source:
         parsed = re.findall("<INPUT\\sNAME=\"([^\"]+?)\"\\sID=\"[^\"]+?\"(?:\\sVALUE=\"([^\"]*?)\")?", text)
         return {k: v for k, v in parsed}
 
-    def _get_dates(self, session, payload, calendar=""):
-        boundary = "WebKitFormBoundary" + "".join(random.sample(string.ascii_letters + string.digits, 16))
-        headers = {'Content-Type': f'multipart/form-data; boundary=----{boundary}'}
-        payload.update({"SubmitAction": "CITYCHANGED", "Ort": self._city, "Strasse": ""})
-        if calendar:
-            payload.update({"Zeitraum": html.unescape(calendar)})
-        response = session.post(API_URL, headers=headers, data=self._parse_data(payload, boundary))
-        payload = self._parse_response_input(response.text)
-        payload.update(
-            {"SubmitAction": "forward", "Ort": self._city, "Strasse": self._street, "Hausnummer": self._house_number,
-             "Hausnummerzusatz": self._address_suffix})
-        if calendar:
-            payload.update({"Zeitraum": html.unescape(calendar)})
-        response = session.post(API_URL, headers=headers, data=self._parse_data(payload, boundary))
-        if error := re.findall("informationItemsText_1\">([^<]+?)<", response.text):
+    def _address(self):
+        return {"Ort": self._city, "Strasse": self._street, "Hausnummer": self._house_number,
+                "Hausnummerzusatz": self._address_suffix}
+
+    def _headers(self):
+        return {'Content-Type': f'multipart/form-data; boundary=----{self._boundary}'}
+
+    def _payload(self, last_request, action="", period="", **kwargs):
+        payload = self._parse_response_input(last_request)
+        payload.update({"SubmitAction": action, **kwargs})
+        if period:
+            payload.update({"Zeitraum": html.unescape(period)})
+        return self._parse_data(payload, self._boundary)
+
+    def _get_dates(self, session, init_request, calendar=""):
+        kwargs = {"Ort": self._city, "Strasse": ""}
+        payload = self._payload(init_request, action="CITYCHANGED", period=calendar, **kwargs)
+        city_response = session.post(API_URL, headers=self._headers(), data=payload)
+        payload = self._payload(city_response.text, action="forward", period=calendar, **self._address())
+        final_response = session.post(API_URL, headers=self._headers(), data=payload)
+        if error := re.findall("informationItemsText_1\">([^<]+?)<", final_response.text):
             _LOGGER.warning(f"{self} - {html.unescape(error[0])}")
             return []
-        return re.findall('<P ID="TermineDatum([0-9A-Z]+)_\\d+">[A-Z][a-z]. ([0-9.]{10}) </P>', response.text)
+        return re.findall('<P ID="TermineDatum([0-9A-Z]+)_\\d+">[A-Z][a-z]. ([0-9.]{10}) </P>', final_response.text)
 
     def fetch(self):
         session = requests.Session()
-        response = session.get(f"{API_URL}?SubmitAction=wasteDisposalServices&InFrameMode=true")
-        payload = self._parse_response_input(response.text)
-        if calendars := re.findall('NAME="Zeitraum" VALUE=\"([^\"]+?)\"', response.text):
-            dates = [date for calendar in calendars for date in self._get_dates(session, payload, calendar)]
+        init_request = session.get(f"{API_URL}?SubmitAction=wasteDisposalServices&InFrameMode=true").text
+        if calendars := re.findall('NAME="Zeitraum" VALUE=\"([^\"]+?)\"', init_request):
+            dates = [date for calendar in calendars for date in self._get_dates(session, init_request, calendar)]
         else:
-            dates = self._get_dates(session, payload)
+            dates = self._get_dates(session, init_request)
         entries = []
         for bin_type, date in dates:
             name = TYPES[next(x for x in list(TYPES) if x in bin_type)]
