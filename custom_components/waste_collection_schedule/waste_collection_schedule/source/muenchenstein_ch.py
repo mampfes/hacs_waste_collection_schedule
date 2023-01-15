fbade3dbb296044281e1043a5c51531c9c22262b
mampfes <mampfes@users.noreply.github.com>
2023-01-15 10:55:49 +0100

    refactor muenchenstein_ch

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/muenchenstein_ch.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/muenchenstein_ch.py
@@ -5,16 +5,18 @@ import requests
 from bs4 import BeautifulSoup
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
-TITLE = "Abfallsammlung Münchenstein"
+TITLE = "Münchenstein"
 DESCRIPTION = "Source for Muenchenstein waste collection."
-URL = "https://www.muenchenstein.ch/abfuhrdaten"
+URL = "https://www.muenchenstein.ch"
 TEST_CASES = {
     "Abfuhrkreis Ost": {"waste_district": "Abfuhrkreis Ost"},
     "Abfuhrkreis West": {"waste_district": "492"},
 }
 
+API_URL = "https://www.muenchenstein.ch/abfuhrdaten"
 
-IconMap = {
+
+ICON_MAP = {
     "kehricht-und-kleinsperrgut-brennbar": "mdi:trash-can-outline",
     "hackseldienst": "mdi:leaf",
     "papierabfuhr": "mdi:newspaper-variant-multiple-outline",
@@ -28,7 +30,7 @@ class Source:
         self._waste_district = waste_district
 
     def fetch(self):
-        response = requests.get(URL)
+        response = requests.get(API_URL)
 
         html = BeautifulSoup(response.text, "html.parser")
 
@@ -51,26 +53,30 @@ class Source:
                     Collection(
                         date=next_pickup_date,
                         t=waste_type,
-                        icon=IconMap.get(waste_type_sorted, "mdi:trash-can"),
+                        icon=ICON_MAP.get(waste_type_sorted, "mdi:trash-can"),
                     )
                 )
 
         # Collection of "Kehricht und Kleinsperrgut brennbar" are not listed with dates as events on website.
         # Instead it states the day of the week for each waste district: tuesday for east and friday for west
         # So we're going to set those collections programmatically for the next 4 occurrences
-        weekday_collection = 2 if self._waste_district == 'Abfuhrkreis Ost' or self._waste_district == 491 else 5
+        weekday_collection = (
+            2
+            if self._waste_district == "Abfuhrkreis Ost" or self._waste_district == 491
+            else 5
+        )
         weekday_today = datetime.now().isoweekday()
         for x in range(4):
             days_to_pickup = (x * 7) + ((weekday_collection - weekday_today) % 7)
             next_pickup_date = (datetime.now() + timedelta(days=days_to_pickup)).date()
             waste_type = "Kehricht und Kleinsperrgut brennbar"
-            waste_type_sorted = waste_type.lower().replace(' ', '-')
+            waste_type_sorted = waste_type.lower().replace(" ", "-")
 
             entries.append(
                 Collection(
                     date=next_pickup_date,
                     t=waste_type,
-                    icon=IconMap.get(waste_type_sorted, "mdi:trash-can"),
+                    icon=ICON_MAP.get(waste_type_sorted),
                 )
             )
 
