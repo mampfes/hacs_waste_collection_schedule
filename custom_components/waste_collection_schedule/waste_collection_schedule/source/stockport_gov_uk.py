b1602eeca79c86279c2b8786208028e1b67f4172
mampfes <mampfes@users.noreply.github.com>
2023-01-18 21:04:13 +0100

    refactor stockport_gov_uk

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/stockport_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/stockport_gov_uk.py
@@ -1,21 +1,19 @@
+import logging
+import re
 from datetime import datetime
 
 import requests
-import re
+from bs4 import BeautifulSoup
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
-from bs4 import BeautifulSoup
-
-import logging
-
-TITLE = "stockport.gov.uk"
+TITLE = "Stockport Council"
 DESCRIPTION = "Source for bin collection services for Stockport Council, UK.\n Refactored with thanks from the Manchester equivalent"
-URL =  "https://myaccount.stockport.gov.uk/bin-collections/show/"
+URL = "https://stockport.gov.uk"
 TEST_CASES = {
-    "domestic": {'uprn': '100011460157'},
+    "domestic": {"uprn": "100011460157"},
 }
 
-ICONS = {
+ICON_MAP = {
     "Black bin": "mdi:trash-can",
     "Blue bin": "mdi:recycle",
     "Brown bin": "mdi:glass-fragile",
@@ -26,49 +24,42 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class Source:
-    def __init__(
-        self, uprn: int = None
-    ):
+    def __init__(self, uprn):
         self._uprn = uprn
-        if not self._uprn:
-            _LOGGER.error(
-                "uprn must be provided in config"
-            )
-        self._session = requests.Session()
 
     def fetch(self):
-        entries = []
-
-        bin_URL = URL + self._uprn
 
         r = requests.get(
-            bin_URL,
-            )
+            f"https://myaccount.stockport.gov.uk/bin-collections/show/{self._uprn}"
+        )
 
         soup = BeautifulSoup(r.text, features="html.parser")
 
-        results = soup.find_all("div", {"class": "collection"})
-        bins = soup.find_all("div" ,{"class": re.compile("service-item service-item-.*")})
+        bins = soup.find_all(
+            "div", {"class": re.compile("service-item service-item-.*")}
+        )
 
         bins = str(bins)
         header_string = "<h3>"
         bin_name_start_position = bins.find(header_string)
+
+        entries = []
         while bin_name_start_position != -1:
             bin_name_start_position += len(header_string)
-            bin_name_end_position = bins.find(" bin",bin_name_start_position) + 4
+            bin_name_end_position = bins.find(" bin", bin_name_start_position) + 4
             bin_name = bins[bin_name_start_position:bin_name_end_position]
-            bin_date_pos = bins.find("<p>",bin_name_start_position)
-            bin_date_exc_day_of_week_pos = bins.find(", ",bin_date_pos)+2
-            bin_date_end_pos = bins.find("</p>",bin_date_exc_day_of_week_pos)
+            bin_date_pos = bins.find("<p>", bin_name_start_position)
+            bin_date_exc_day_of_week_pos = bins.find(", ", bin_date_pos) + 2
+            bin_date_end_pos = bins.find("</p>", bin_date_exc_day_of_week_pos)
             bin_date_string = bins[bin_date_exc_day_of_week_pos:bin_date_end_pos]
-            bin_date = datetime.strptime(bin_date_string,'%d %B %Y').date()
+            bin_date = datetime.strptime(bin_date_string, "%d %B %Y").date()
             entries.append(
                 Collection(
                     date=bin_date,
                     t=bin_name,
-                    icon=ICONS[bin_name],
+                    icon=ICON_MAP.get(bin_name),
                 )
             )
-            bin_name_start_position = bins.find(header_string,bin_date_end_pos)
+            bin_name_start_position = bins.find(header_string, bin_date_end_pos)
 
         return entries
