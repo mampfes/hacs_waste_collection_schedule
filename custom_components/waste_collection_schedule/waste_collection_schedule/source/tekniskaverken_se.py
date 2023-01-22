18ae1f106f13d1ea16963be3918011ece8b23cb0
mampfes <mampfes@users.noreply.github.com>
2023-01-22 17:14:04 +0100

    fix code page errors

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/tekniskaverken_se.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/tekniskaverken_se.py
@@ -1,24 +1,24 @@
-﻿import locale
+import locale
+from datetime import date, datetime
+
 import requests
 from bs4 import BeautifulSoup
 from waste_collection_schedule import Collection
-from datetime import datetime
-from datetime import date
 
 TITLE = "Linköping - Tekniska Verken"
 DESCRIPTION = "Source for Tekniska Verken in Linköping waste collection"
 URL = "https://www.tekniskaverken.se/"
-API_URL = "https://www.tekniskaverken.se/privat/avfall-och-atervinning/mat-och-restavfall/"
+API_URL = (
+    "https://www.tekniskaverken.se/privat/avfall-och-atervinning/mat-och-restavfall/"
+)
 TEST_CASES = {
     "Somewhere": {"street": "Roshagsvägen 2", "city": "Linköping"},
     "Away": {"street": "Roshagsvägen 6", "city": "Linköping"},
     "Home": {"street": "Roshagsvägen 66", "city": "Linköping"},
-    "Police": {"street": "Brigadgatan 4", "city": "Linköping"}
+    "Police": {"street": "Brigadgatan 4", "city": "Linköping"},
 }
-ICON_MAP = {
-    "Hushållsavfall": "mdi:trash-can",
-    "Trädgårdsavfall": "mdi:leaf"
-}
+ICON_MAP = {"Hushållsavfall": "mdi:trash-can", "Trädgårdsavfall": "mdi:leaf"}
+
 
 class Source:
     def __init__(self, street, city):
@@ -27,13 +27,15 @@ class Source:
         self._city = city
 
     def fetch(self):
-        data = { "postback": "true", "street": self._street, "city": self._city }
+        data = {"postback": "true", "street": self._street, "city": self._city}
 
         response = requests.post(API_URL, params=data)
-        response.encoding = 'UTF-8'
-        soup = BeautifulSoup(response.text, 'html.parser')
-        addresses = soup.find_all('div', attrs={'class': 'wastecollections-selectedaddress'})
-        infos = soup.find_all('ul', attrs={'class': 'wastecollections-results'})
+        response.encoding = "UTF-8"
+        soup = BeautifulSoup(response.text, "html.parser")
+        addresses = soup.find_all(
+            "div", attrs={"class": "wastecollections-selectedaddress"}
+        )
+        infos = soup.find_all("ul", attrs={"class": "wastecollections-results"})
 
         for (addressTag, info) in zip(addresses, infos):
             street = addressTag.text.split(",")[0]
@@ -44,10 +46,14 @@ class Source:
 
     def get_entries(self, info):
         entries = []
-        waste_types_info = info.find_all("li", attrs={'class': 'wastecollections-results-item'})
+        waste_types_info = info.find_all(
+            "li", attrs={"class": "wastecollections-results-item"}
+        )
 
         for waste_type_info in waste_types_info:
-            waste_type = waste_type_info.find("strong", attrs={'class': 'wastecollections-results-item-type-label'}).text
+            waste_type = waste_type_info.find(
+                "strong", attrs={"class": "wastecollections-results-item-type-label"}
+            ).text
             pickup_date = self.get_date(waste_type_info)
             icon = ICON_MAP.get(waste_type)
             entries.append(Collection(date=pickup_date, t=waste_type, icon=icon))
@@ -55,13 +61,15 @@ class Source:
         return entries
 
     def get_date(self, waste_type_info):
-        pickup_date_metas = waste_type_info.find_all("div", attrs={'class': 'wastecollections-results-item__meta'})
+        pickup_date_metas = waste_type_info.find_all(
+            "div", attrs={"class": "wastecollections-results-item__meta"}
+        )
 
         for pickup_date_meta in pickup_date_metas:
             if "Nästa" in pickup_date_meta.text:
-                today =date.today()
+                today = date.today()
                 pickup_date_tag = pickup_date_meta.find("strong")
-                pickup_datetime = datetime.strptime(pickup_date_tag.text, '%d %B')
+                pickup_datetime = datetime.strptime(pickup_date_tag.text, "%d %B")
                 pickup_datetime = pickup_datetime.replace(year=today.year)
                 if pickup_datetime.month < today.month:
                     pickup_datetime.replace(year=today.year + 1)
--- doc/source/tekniskaverken_se.md
+++ doc/source/tekniskaverken_se.md
@@ -1,6 +1,6 @@
-# Linköping - Tekniska Verken
+# LinkÃ¶ping - Tekniska Verken
 
-Support for schedules provided by [Linköping - Tekniska Verken](https://www.tekniskaverken.se/privat/avfall-och-atervinning/mat-och-restavfall/), serving the municipalities of Linköping, Sweden.
+Support for schedules provided by [LinkÃ¶ping - Tekniska Verken](https://www.tekniskaverken.se/privat/avfall-och-atervinning/mat-och-restavfall/), serving the municipalities of LinkÃ¶ping, Sweden.
 
 ## Configuration via configuration.yaml
 
@@ -29,10 +29,10 @@ waste_collection_schedule:
     - name: tekniskaverken_se
       args:
         street: Brigadgatan 4
-        city: Linköping
+        city: Linkï¿½ping
 ```
 
 ## How to get the source argument
 
-The source argument is the street including number and the city to the house with waste collection. 
-The address can be tested [here]((https://www.tekniskaverken.se/privat/avfall-och-atervinning/mat-och-restavfall/).
+The source argument is the street including number and the city to the house with waste collection.
+The address can be tested [here](https://www.tekniskaverken.se/privat/avfall-och-atervinning/mat-och-restavfall/).
