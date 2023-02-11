badb0612e27ff8a2fd0a0b05f72a03109b98d4ab
mampfes <mampfes@users.noreply.github.com>
2023-02-11 12:15:04 +0100

    refactor ead_darmstadt_de

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/ead_darmstadt_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/ead_darmstadt_de.py
@@ -1,9 +1,9 @@
-import datetime
-from waste_collection_schedule import Collection
+from datetime import datetime
+
 import requests
+from waste_collection_schedule import Collection
 
-
-TITLE = "EAD Darmstadt" # Title will show up in README.md and info.md
+TITLE = "EAD Darmstadt"  # Title will show up in README.md and info.md
 DESCRIPTION = "Source script for waste collection in Darmstadt ead.darmstadt.de"  # Describe your source
 URL = "https://ead.darmstadt.de/"  # Insert url to service homepage. URL will show up in README.md and info.md
 TEST_CASES = {  # Insert arguments for test cases to be used by test_sources.py script
@@ -13,11 +13,10 @@ TEST_CASES = {  # Insert arguments for test cases to be used by test_sources.py 
     "Heinheimer Straße": {"street": "Heinheimer Straße 1-15, 2-16"},
     "Kleyerstraße": {"street": "Kleyerstraße"},
     "Untere Mühlstraße": {"street": "Untere Mühlstraße 1-29, 2-36"},
-
 }
 
 API_URL = "https://ead.darmstadt.de/unser-angebot/privathaushalte/abfallkalender/singleStreet/"
-ICON_MAP = {   # Optional: Dict of waste types and suitable mdi icons
+ICON_MAP = {  # Optional: Dict of waste types and suitable mdi icons
     "RM1": "mdi:trash-can",
     "RM2": "mdi:trash-can",
     "RM4": "mdi:trash-can",
@@ -26,46 +25,32 @@ ICON_MAP = {   # Optional: Dict of waste types and suitable mdi icons
     "PPK": "mdi:package-variant",
 }
 
-TRASH_TYPES_NAME = {
-    "RM1": "Restabfall (jede Woche, Schwarz)",
-    "RM2": "Restabfall (Alle zwei Wochen, Orange)",
-    "RM4": "Restabfall (Alle vier Wochen, Rot)",
-    "WET": "Wertstoffe (Gelb)",
-    "BIO": "Bioabfall (Braun)",
-    "PPK": "Altpapier (Grün)",
-}
 
 class Source:
-    def __init__(self, street):  # argX correspond to the args dict in the source configuration
+    def __init__(
+        self, street
+    ):  # argX correspond to the args dict in the source configuration
         self.street = street
 
     def fetch(self):
-
-        #  replace this comment with
-        #  api calls or web scraping required
-        #  to capture waste collection schedules
-        #  and extract date and waste type details
-
         params = {"type": "742394", "street": self.street}
+        r = requests.get(API_URL, params=params)
+        r.raise_for_status()
 
-        r = requests.get(
-            API_URL, params=params
-        )
+        schedule = r.json()
+        if schedule is None or len(schedule) == 0:
+            raise Exception("address not found")
 
         entries = []  # List that holds collection schedule
-
-        entrie_dict = r.json()
-        
-        if entrie_dict is None or entrie_dict == []:
-            return entries
-        
-        for date, waste_type_list in r.json().items():
+        for date, waste_type_list in schedule.items():
             for waste_type in waste_type_list:
                 entries.append(
                     Collection(
-                        date = datetime.datetime.strptime(date, "%d.%m.%Y").date(),  # Collection date
-                        t = TRASH_TYPES_NAME.get(waste_type),  # Collection type
-                        icon = ICON_MAP.get(waste_type),  # Collection icon
+                        date=datetime.strptime(
+                            date, "%d.%m.%Y"
+                        ).date(),  # Collection date
+                        t=waste_type,  # Collection type
+                        icon=ICON_MAP.get(waste_type),  # Collection icon
                     )
                 )
 
