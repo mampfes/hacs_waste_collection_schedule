ce3bd9bbdd30832dfb3a7a0f95799579b4a422a6
mampfes <mampfes@users.noreply.github.com>
2023-02-23 20:36:38 +0100

    refactor abfallwirtschaft_germersheim_de

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/abfallwirtschaft_germersheim_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/abfallwirtschaft_germersheim_de.py
@@ -1,4 +1,3 @@
-import logging
 import re
 
 import requests
@@ -12,7 +11,7 @@ URL = "https://www.abfallwirtschaft-germersheim.de"
 TEST_CASES = {
     "Bellheim": {"city": "Bellheim", "street": "Albert-Schweitzer-Str."},
     "Hatzenbuehl": {"city": "Hatzenbühl"},
-    "hoerdt": {"city": "Hördt", "street":""},
+    "hoerdt": {"city": "Hördt", "street": ""},
 }
 
 API_URL = "https://www.abfallwirtschaft-germersheim.de/online-service/abfall-termine/abfalltermine-ics-export-bis-240-liter.html"
@@ -28,30 +27,44 @@ class Source:
         s = requests.Session()
         params = {
             "icsortschaft": self._city,
-            "icsabfallart[]": [
-            ],
+            "icsabfallart[]": [],
         }
-        
+
         if self._street != "":
             params["icsstrasse"] = self._street
 
         r = s.get(API_URL, params=params)
+        r.raise_for_status()
+
         soup = BeautifulSoup(r.text, "html.parser")
         ics_download = soup.find(
-            "input", {"type": "hidden", "name": "ICS_DOWNLOAD"}).get('value')
+            "input", {"type": "hidden", "name": "ICS_DOWNLOAD"}
+        ).get("value")
         request_token = soup.find(
-            "input", {"type": "hidden", "name": "REQUEST_TOKEN"}).get('value')
-        
-        checkbox_class = re.compile('id_form_icsabfallart_[0-9][0-9]?')
-        waste_types = soup.find("div", {"class": "ctlg_form_field checkbox"}).find_all("label", {"for": checkbox_class,})
+            "input", {"type": "hidden", "name": "REQUEST_TOKEN"}
+        ).get("value")
+
+        checkbox_class = re.compile("id_form_icsabfallart_[0-9][0-9]?")
+        waste_types = soup.find("div", {"class": "ctlg_form_field checkbox"}).find_all(
+            "label",
+            {
+                "for": checkbox_class,
+            },
+        )
         for waste_type in waste_types:
             params["icsabfallart[]"].append(waste_type.text)
 
-        r = s.post(API_URL, params=params, data={
-                   "ICS_DOWNLOAD": ics_download, "REQUEST_TOKEN": request_token})
-        dates = self._ics.convert(r.text)
+        r = s.post(
+            API_URL,
+            params=params,
+            data={"ICS_DOWNLOAD": ics_download, "REQUEST_TOKEN": request_token},
+        )
+        r.raise_for_status()
 
         entries = []
+
+        dates = self._ics.convert(r.text)
         for d in dates:
             entries.append(Collection(d[0], d[1]))
+
         return entries
