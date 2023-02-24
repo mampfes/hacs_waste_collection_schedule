425904ca1283b706a01b8d58ffe3ae4c7bd6256f
mampfes <mampfes@users.noreply.github.com>
2023-02-24 07:10:05 +0100

    refactor onkaparingacity_com

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/onkaparingacity_com.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/onkaparingacity_com.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from waste_collection_schedule import Collection  # type: ignore[attr-defined]
-import json
+
 import requests
 from bs4 import BeautifulSoup
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = "City of Onkaparinga Council"
 DESCRIPTION = "Source for City of Onkaparinga Council, Australia."
@@ -18,63 +18,58 @@ ICON_MAP = {
     "Green Waste": "mdi:leaf",
 }
 
+API_URL = "https://www.onkaparingacity.com"
+HEADERS = {"referer": f"{API_URL}/Services/Waste-and-recycling/Bin-collections"}
+
+
 class Source:
     def __init__(self, address):
         self._address = address
 
     def fetch(self):
-        url = "https://www.onkaparingacity.com/api/v1/myarea/search"
 
-        headers = {
-        'referer': 'https://www.onkaparingacity.com/Services/Waste-and-recycling/Bin-collections'
-        }
+        params = {"keywords": self._address}
 
-        params = {
-        'keywords': self._address
-        }
-
-        r = requests.get(url, params=params, headers=headers)
+        r = requests.get(
+            f"{API_URL}/api/v1/myarea/search", params=params, headers=HEADERS
+        )
+        r.raise_for_status()
 
         addresses = r.json()
-        
+
         if addresses == 0:
-          return []
+            raise Exception("address not found")
 
-        url = 'https://www.onkaparingacity.com/ocapi/Public/myarea/wasteservices'
+        params = {"geolocationid": addresses["Items"][0]["Id"], "ocsvclang": "en-AU"}
 
-        params = {
-        'geolocationid': addresses['Items'][0]['Id'],
-        'ocsvclang': 'en-AU'
-
-        }
-
-        r = requests.get(url, params=params, headers=headers)
+        r = requests.get(
+            f"{API_URL}/ocapi/Public/myarea/wasteservices",
+            params=params,
+            headers=HEADERS,
+        )
+        r.raise_for_status()
 
         waste = r.json()
 
-        soup = BeautifulSoup(waste['responseContent'], "html.parser")
+        soup = BeautifulSoup(waste["responseContent"], "html.parser")
 
         waste_type = []
 
         for tag in soup.find_all("h3"):
-            if tag.text.startswith('Calendar'):
+            if tag.text.startswith("Calendar"):
                 continue
             waste_type.append(tag.text)
 
         waste_date = []
-        for tag in soup.find_all("div", {"class":"next-service"}):
-          tag_text = tag.text.strip()
-          if tag_text != "":
-            date_object = datetime.strptime(tag_text, '%a %d/%m/%Y').date()
-            waste_date.append(date_object)
+        for tag in soup.find_all("div", {"class": "next-service"}):
+            tag_text = tag.text.strip()
+            if tag_text != "":
+                date_object = datetime.strptime(tag_text, "%a %d/%m/%Y").date()
+                waste_date.append(date_object)
 
         waste = list(zip(waste_type, waste_date))
 
         entries = []
         for item in waste:
-            icon = ICON_MAP.get(item[0])
-            entries.append(
-                Collection(item[1], item[0], icon=icon)
-            )
-
+            entries.append(Collection(item[1], item[0], icon=ICON_MAP.get(item[0])))
         return entries
