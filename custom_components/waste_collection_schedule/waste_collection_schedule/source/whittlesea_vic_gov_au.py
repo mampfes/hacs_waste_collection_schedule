8b8fcfd2112d500d5729b60df3a7202ea42601b0
mampfes <mampfes@users.noreply.github.com>
2023-02-24 20:14:12 +0100

    refactor whittlesea_vic_gov_au
    - remove name mapping
    - remove requests timeouts
    - refactor extra waste type handling
    - remove unnecessary headers

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/whittlesea_vic_gov_au.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/whittlesea_vic_gov_au.py
@@ -1,10 +1,8 @@
-from datetime import date, timedelta
+import json
 
 import requests
-from waste_collection_schedule import Collection  # type: ignore[attr-defined]
-import json
 from dateutil.parser import parse
-from datetime import datetime
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = "Whittlesea Council"
 DESCRIPTION = "Source for Whittlesea Council (VIC) rubbish collection."
@@ -22,19 +20,7 @@ TEST_CASES = {
         "street_name": "Ferres Boulevard",
         "suburb": "South Morang",
         "postcode": "3752",
-    }
-}
-
-HEADERS = {
-    "user-agent": "Mozilla/5.0",
-    "content-type": "application/json",
-}
-
-NAMES = {
-    "rubbish": "Red (rubbish)",
-    "recycling": "Yellow (recycling)",
-    "glass": "Purple (glass)",
-    "green": "Green (garden)",
+    },
 }
 
 ICON_MAP = {
@@ -47,8 +33,6 @@ ICON_MAP = {
 # Only a year's worth of dates is available
 WEEKS = 53
 
-# Time to wait for initial connection, then response
-TIMEOUT=(10,5)
 
 class Source:
     def __init__(self, suburb, street_name, street_number, postcode):
@@ -60,24 +44,36 @@ class Source:
     def fetch(self):
         # Retrieve geolocation for our address
         # (TODO: cache the LAT/LON results)
-        address = self.street_number+" "+self.street_name+" "+self.suburb+" "+self.postcode
+        address = (
+            self.street_number
+            + " "
+            + self.street_name
+            + " "
+            + self.suburb
+            + " "
+            + self.postcode
+        )
         PARAMS = {"address": address}
-        url="https://www.whittlesea.vic.gov.au/umbraco/api/vicmap/GetAddressResultsUsingArcGis/"
+        url = "https://www.whittlesea.vic.gov.au/umbraco/api/vicmap/GetAddressResultsUsingArcGis/"
         r = requests.get(
             url,
-            headers=HEADERS,
             params=PARAMS,
-            timeout=TIMEOUT,
         )
+        r.raise_for_status()
 
         # TODO: better error handling of parsing issues
-        json_string = r.text.encode('raw_unicode_escape').decode('unicode_escape').lstrip('\"').rstrip('\"')
+        json_string = (
+            r.text.encode("raw_unicode_escape")
+            .decode("unicode_escape")
+            .lstrip('"')
+            .rstrip('"')
+        )
         data = json.loads(json_string)
 
         if not isinstance(data, dict):
             raise Exception("malformed response from web query")
 
-        features=data.get("features")
+        features = data.get("features")
 
         # Find the coordinates for our address
         # TODO: check that there is only one geometry
@@ -87,38 +83,61 @@ class Source:
 
         # Armed with the LAT and LON coordinates, we construct
         # a request to fetch the waste pick-up schedules
-        url="https://www.whittlesea.vic.gov.au/umbraco/api/cartomap/GetQueryResultsArcGisWasteCollection"
+        url = "https://www.whittlesea.vic.gov.au/umbraco/api/cartomap/GetQueryResultsArcGisWasteCollection"
 
-        firstQuery = "geometry%3D"+geo_x+","+geo_y+"%26geometryType%3DesriGeometryPoint%26inSR%3D4326%26spatialRel%3DesriSpatialRelIntersects%26outFields%3DName%26returnGeometry%3Dfalse%26f%3Djson"
+        firstQuery = (
+            "geometry%3D"
+            + geo_x
+            + ","
+            + geo_y
+            + "%26geometryType%3DesriGeometryPoint%26inSR%3D4326%26spatialRel%3DesriSpatialRelIntersects%26outFields%3DName%26returnGeometry%3Dfalse%26f%3Djson"
+        )
 
-        secondQuery = "where%3Dzonename%253D%2527%7B0%7D%2527%2Band%2Bdate%3ECURRENT_TIMESTAMP-1%26time%3D%26topFilter%3D%257B%250D%250A%2B%2B%2522groupByFields%2522%253A%2B%2522zonename%2522%252C%250D%250A%2B%2B%2522topCount%2522%253A%2B"+str(WEEKS)+"%252C%250D%250A%2B%2B%2522orderByFields%2522%253A%2B%2522date%2522%250D%250A%257D%26outFields%3D*%26orderByFields%3Ddate%26resultRecordCount%3D"+str(WEEKS)+"%26f%3Djson"
+        secondQuery = (
+            "where%3Dzonename%253D%2527%7B0%7D%2527%2Band%2Bdate%3ECURRENT_TIMESTAMP-1%26time%3D%26topFilter%3D%257B%250D%250A%2B%2B%2522groupByFields%2522%253A%2B%2522zonename%2522%252C%250D%250A%2B%2B%2522topCount%2522%253A%2B"
+            + str(WEEKS)
+            + "%252C%250D%250A%2B%2B%2522orderByFields%2522%253A%2B%2522date%2522%250D%250A%257D%26outFields%3D*%26orderByFields%3Ddate%26resultRecordCount%3D"
+            + str(WEEKS)
+            + "%26f%3Djson"
+        )
 
-        url += "?firstQuery="+firstQuery+"&secondQuery="+secondQuery
+        url += "?firstQuery=" + firstQuery + "&secondQuery=" + secondQuery
 
         r = requests.get(
             url,
-            headers=HEADERS,
-            timeout=(5,3),
         )
+        r.raise_for_status()
 
-        json_string = r.text.encode('raw_unicode_escape').decode('unicode_escape').lstrip('\"').rstrip('\"')
+        json_string = (
+            r.text.encode("raw_unicode_escape")
+            .decode("unicode_escape")
+            .lstrip('"')
+            .rstrip('"')
+        )
         data = json.loads(json_string)
 
         entries = []
 
-        today = datetime.utcnow().date()
         for item in data["rows"]:
             if "cartodb_id" in item:
-              collection_date = parse(item["date"]).date()
-              # Only return dates in the future
-              if (collection_date - today).days >= 0:
-                #print(f"found date {collection_date}")
-                entries.append(Collection(date=collection_date,t=NAMES.get("rubbish"),icon=ICON_MAP.get("rubbish"),))
-                if item["recycling"] == 1:
-                    entries.append(Collection(date=collection_date,t=NAMES.get("recycling"),icon=ICON_MAP.get("recycling")))
-                if item["green"] == 1:
-                    entries.append(Collection(date=collection_date,t=NAMES.get("green"),icon=ICON_MAP.get("green")))
-                if item["glass"] == 1:
-                    entries.append(Collection(date=collection_date,t=NAMES.get("glass"),icon=ICON_MAP.get("glass")))
+                collection_date = parse(item["date"]).date()
+                entries.append(
+                    Collection(
+                        date=collection_date,
+                        t="rubbish",
+                        icon=ICON_MAP.get("rubbish"),
+                    )
+                )
+
+                # test extra waste types
+                for waste_type in ["recycling", "green", "glass"]:
+                    if item[waste_type] == 1:
+                        entries.append(
+                            Collection(
+                                date=collection_date,
+                                t=waste_type,
+                                icon=ICON_MAP.get(waste_type),
+                            )
+                        )
 
         return entries
