f23805302579fcc75c0b23306fa34f8a8dfa26f8
mampfes <mampfes@users.noreply.github.com>
2023-03-05 20:33:42 +0100

    refactor sepan_remondis_pl

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/sepan_remondis_pl.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/sepan_remondis_pl.py
@@ -1,30 +1,26 @@
-import logging
 import datetime
+import json
+import logging
 import xml.etree.ElementTree
 
-import json
 import requests
 from waste_collection_schedule import Collection
 
 TITLE = "Poznań/Koziegłowy/Objezierze/Oborniki"
 DESCRIPTION = "Source for Poznań/Koziegłowy/Objezierze/Oborniki city garbage collection"
-URL = "https://sepan.remondis.pl/harmonogram/"
+URL = "https://sepan.remondis.pl"
 TEST_CASES = {
-    "Street Name": {"city": "Poznań", "street_name": "ŚWIĘTY MARCIN", "street_number": "1"},
+    "Street Name": {
+        "city": "Poznań",
+        "street_name": "ŚWIĘTY MARCIN",
+        "street_number": "1",
+    },
 }
 
 _LOGGER = logging.getLogger(__name__)
 
 
-class SourceConfigurationError(ValueError):
-    pass
-
-
-class SourceParseError(ValueError):
-    pass
-
-
-PAGE_URL = "https://sepan.remondis.pl/harmonogram"
+API_URL = "https://sepan.remondis.pl/harmonogram"
 
 NAME_MAP = {
     1: "Zmieszane odpady komunalne",
@@ -46,70 +42,68 @@ ICON_MAP = {
     7: "mdi:trash-can",
 }
 
+
 class Source:
-    def __init__(self, city=None, street_name=None, street_number=None):
-        if city is None or street_name is None or street_number is None:
-            raise SourceConfigurationError(
-                "All params city, street_name and street_number must have a value"
-            )
+    def __init__(self, city, street_name, street_number):
         self._city = city.upper()
         self._street_name = street_name.upper()
         self._street_number = street_number.upper()
 
     def fetch(self):
-        entries = []
-
-        url = f"{PAGE_URL}/addresses/cities"
-        r = requests.get(url)
+        r = requests.get(f"{API_URL}/addresses/cities")
+        r.raise_for_status()
         city_id = 0
         cities = json.loads(r.text)
         for item in cities:
-          if item["value"] == self._city:
-            city_id = item["id"]
+            if item["value"] == self._city:
+                city_id = item["id"]
         if city_id == 0:
-          _LOGGER.debug(f"No such city")
-          return entries
+            raise Exception("city not found")
 
-        url = f"{PAGE_URL}/addresses/streets/{city_id}"
-        r = requests.get(url)
+        r = requests.get(f"{API_URL}/addresses/streets/{city_id}")
+        r.raise_for_status()
         street_id = 0
         streets = json.loads(r.text)
         for item in streets:
-          if item["value"] == self._street_name:
-            street_id = item["id"]
+            if item["value"] == self._street_name:
+                street_id = item["id"]
         if street_id == 0:
-          _LOGGER.debug(f"No such street")
-          return entries
-        
-        url = f"{PAGE_URL}/addresses/numbers/{city_id}/{street_id}"
-        r = requests.get(url)
+            raise Exception("street not found")
+
+        r = requests.get(f"{API_URL}/addresses/numbers/{city_id}/{street_id}")
+        r.raise_for_status()
         number_id = 0
         numbers = json.loads(r.text)
         for item in numbers:
-          if item["value"] == self._street_number:
-            number_id = item["id"]
+            if item["value"] == self._street_number:
+                number_id = item["id"]
         if number_id == 0:
-          _LOGGER.debug(f"No such number")
-          return entries
+            raise Exception("number not found")
 
-        url = f"{PAGE_URL}/reports?type=html&id={number_id}"
-        r = requests.get(url)
+        r = requests.get(f"{API_URL}/reports?type=html&id={number_id}")
+        r.raise_for_status()
         report = json.loads(r.text)
-        if report["status"] == "success":
-          url = report["filePath"]
-        if url == "":
-          _LOGGER.debug(f"Error fetching report")
-          return entries
+        if report["status"] != "success":
+            raise Exception("fetch report failed")
 
-        r = requests.get(url)
-        table = r.text[r.text.find('<table'):r.text.rfind('</table>')+8]
+        r = requests.get(report["filePath"])
+        r.raise_for_status()
+        table = r.text[r.text.find("<table") : r.text.rfind("</table>") + 8]
         tree = xml.etree.ElementTree.fromstring(table)
         year = datetime.date.today().year
-        for row_index, row in enumerate(tree.findall('.//tr')):
-          if row_index > 0:
-            for cell_index, cell in enumerate(row.findall('.//td')):
-              if cell_index > 0 and isinstance(cell.text, str):
-                for day in cell.text.split(','):
-                  entries.append(Collection(datetime.date(year, row_index-1, int(day)), NAME_MAP[cell_index], ICON_MAP[cell_index]))
+
+        entries = []
+        for row_index, row in enumerate(tree.findall(".//tr")):
+            if row_index > 0:
+                for cell_index, cell in enumerate(row.findall(".//td")):
+                    if cell_index > 0 and isinstance(cell.text, str):
+                        for day in cell.text.split(","):
+                            entries.append(
+                                Collection(
+                                    datetime.date(year, row_index - 1, int(day)),
+                                    NAME_MAP[cell_index],
+                                    ICON_MAP[cell_index],
+                                )
+                            )
 
         return entries
