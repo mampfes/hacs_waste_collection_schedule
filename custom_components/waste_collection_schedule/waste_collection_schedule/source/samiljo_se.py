aa627d4e168b7c68d842072ed55a60bfd95b5e21
mampfes <mampfes@users.noreply.github.com>
2023-03-07 07:32:11 +0100

    refactor samiljo_se
    - add raise_for_status
    - replace DOS line feeds with Unix line feeds

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/samiljo_se.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/samiljo_se.py
@@ -1,8 +1,9 @@
 import datetime
-from waste_collection_schedule import Collection
+import urllib.parse
+
 import requests
 from bs4 import BeautifulSoup
-import urllib.parse
+from waste_collection_schedule import Collection
 
 TITLE = "Samverkan Återvinning Miljö (SÅM)"
 DESCRIPTION = "Source script for samiljo.se"
@@ -16,7 +17,7 @@ TEST_CASES = {
 
 API_URLS = {
     "address_search": "https://webbservice.indecta.se/kunder/sam/kalender/basfiler/laddaadresser.php",
-    "collection": "https://webbservice.indecta.se/kunder/sam/kalender/basfiler/onlinekalender.php"
+    "collection": "https://webbservice.indecta.se/kunder/sam/kalender/basfiler/onlinekalender.php",
 }
 
 ICON_MAP = {
@@ -33,10 +34,10 @@ ICON_MAP = {
 }
 
 NAME_MAP = {
-    "HKARL1": "Fyrfackskärl 1", # Matavfall, Restavfall, Tidningar & Färgat glas
-    "HKARL1-H": "Fyrfackskärl 1 (Helgvecka - hämtningsdagen kan avika)", # Matavfall, Restavfall, Tidningar & Färgat glas
-    "HKARL2": "Fyrfackskärl 2", # Förpackningar av Papper, Plast & Metall samt Ofärgat glas
-    "HKARL2-H": "Fyrfackskärl 2 (Helgvecka - hämtningsdagen kan avika)", # Förpackningar av Papper, Plast & Metall samt Ofärgat glas
+    "HKARL1": "Fyrfackskärl 1",  # Matavfall, Restavfall, Tidningar & Färgat glas
+    "HKARL1-H": "Fyrfackskärl 1 (Helgvecka - hämtningsdagen kan avika)",  # Matavfall, Restavfall, Tidningar & Färgat glas
+    "HKARL2": "Fyrfackskärl 2",  # Förpackningar av Papper, Plast & Metall samt Ofärgat glas
+    "HKARL2-H": "Fyrfackskärl 2 (Helgvecka - hämtningsdagen kan avika)",  # Förpackningar av Papper, Plast & Metall samt Ofärgat glas
     "HMAT": "Matavfall",
     "HMAT-H": "Matavfall (Helgvecka - hämtningsdagen kan avika)",
     "HREST": "Restavfall",
@@ -57,18 +58,21 @@ MONTH_MAP = {
     "September": 9,
     "Oktober": 10,
     "November": 11,
-    "December": 12
-    }
+    "December": 12,
+}
+
 
 class Source:
     def __init__(self, street, city):
         self.street = street
         self.city = city
 
-
     def fetch(self):
-        #request to get an adresslist containing the parameter A used for the wasteschedule request.
-        adresslist = requests.get(API_URLS["address_search"], params={"svar": self.street})
+        # request to get an adresslist containing the parameter A used for the wasteschedule request.
+        adresslist = requests.get(
+            API_URLS["address_search"], params={"svar": self.street}
+        )
+        adresslist.raise_for_status()
 
         adresstaddict = (self.street.lower(), self.city.lower())
         adresstadjoined = "|".join(adresstaddict)
@@ -77,37 +81,40 @@ class Source:
             if adresstadjoined in line:
                 A = line.split("|")[-1]
 
-        payload = {
-            "hsG": self.street,
-            "hsO": self.city,
-            "nrA": A
-        }
+        payload = {"hsG": self.street, "hsO": self.city, "nrA": A}
         payload_str = urllib.parse.urlencode(payload, encoding="cp1252")
-        #request for the wasteschedule
+        # request for the wasteschedule
         wasteschedule = requests.get(API_URLS["collection"], params=payload_str)
+        wasteschedule.raise_for_status()
 
         soup = BeautifulSoup(wasteschedule.text, "html.parser")
 
         entries = []
-        #get a list of all tags with waste collection days for the current year
+        # get a list of all tags with waste collection days for the current year
         for wasteday in soup.find_all("td", "styleDayHit"):
             wasteday_wastetype = wasteday.parent.parent
-            #find month and year for given day
-            monthyear = wasteday_wastetype.find_previous("td", "styleMonthName").contents
-            monthyear_parts = str(monthyear).split('-')
+            # find month and year for given day
+            monthyear = wasteday_wastetype.find_previous(
+                "td", "styleMonthName"
+            ).contents
+            monthyear_parts = str(monthyear).split("-")
             month = MONTH_MAP[monthyear_parts[0].strip(" []/'")]
             year = int(monthyear_parts[1].strip(" []/'"))
-            day = int(str(wasteday_wastetype.find("div", "styleInteIdag").contents).strip(" []/'"))
-            #list of bins collected for given day
+            day = int(
+                str(wasteday_wastetype.find("div", "styleInteIdag").contents).strip(
+                    " []/'"
+                )
+            )
+            # list of bins collected for given day
             for td in wasteday_wastetype.contents[3].find_all("td"):
-              if td.has_attr("class"):
-                  waste = str(td.get_attribute_list('class')).strip(" []/'")
-                  entries.append(
-                      Collection(
-                        t = NAME_MAP.get(waste),
-                        #t = waste, #used when identifying new types of bins
-                        date = datetime.date(year, month, day),
-                        icon = ICON_MAP.get(waste),
+                if td.has_attr("class"):
+                    waste = str(td.get_attribute_list("class")).strip(" []/'")
+                    entries.append(
+                        Collection(
+                            t=NAME_MAP.get(waste),
+                            # t = waste, #used when identifying new types of bins
+                            date=datetime.date(year, month, day),
+                            icon=ICON_MAP.get(waste),
+                        )
                     )
-                  )
         return entries
