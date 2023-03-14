b56a46ecd9ed74b6151576a80e4dbd381d32be37
mampfes <mampfes@users.noreply.github.com>
2023-03-14 07:24:10 +0100

    refactor swindon_gov_uk

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/swindon_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/swindon_gov_uk.py
@@ -10,7 +10,7 @@ TEST_CASES = {
     "1 Nyland Road": {"uprn": "100121147490"},
     "74 Standen Way": {"uprn": "200002922415"},
     "1 Eastbury Way": {"uprn": "10010424600"},
-    "33 Ulysses Road": {"uprn": "10010427033"}
+    "33 Ulysses Road": {"uprn": "10010427033"},
 }
 
 ICON_MAP = {
@@ -23,25 +23,28 @@ ICON_MAP = {
 
 class Source:
     def __init__(self, uprn: str):
-        self.uprn = uprn
-        self.url = f"{URL}/info/20122/rubbish_and_recycling_collection_days?uprnSubmit=Yes&addressList={self.uprn}"
+        self._uprn = uprn
 
     def fetch(self):
-        collection_lookup = requests.post(
-            f"{URL}/info/20122/rubbish_and_recycling_collection_days?uprnSubmit=Yes&addressList={self.uprn}"
+        params = {"uprnSubmit": "Yes", "addressList": self._uprn}
+        r = requests.post(
+            "https://www.swindon.gov.uk/info/20122/rubbish_and_recycling_collection_days",
+            params=params,
         )
-        collection_lookup.raise_for_status()
-        
+        r.raise_for_status()
+
         entries = []
-        for results in BeautifulSoup(collection_lookup.text, "html.parser").find_all(
+        for results in BeautifulSoup(r.text, "html.parser").find_all(
             "div", class_="bin-collection-content"
         ):
-            
+
             try:
-                recyclingdate = results.find("span", class_="nextCollectionDate");
-                
-                if recyclingdate != None :
-                    recyclingtype = results.find('div', class_='content-left').find("h3");
+                recyclingdate = results.find("span", class_="nextCollectionDate")
+
+                if recyclingdate is not None:
+                    recyclingtype = results.find("div", class_="content-left").find(
+                        "h3"
+                    )
                     entries.append(
                         Collection(
                             date=parser.parse(recyclingdate.text, dayfirst=True).date(),
