e4a15783a7c802165737703fc03f838585a60284
mampfes <mampfes@users.noreply.github.com>
2023-03-28 07:45:26 +0200

    refactor fenland_gov_uk

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/fenland_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/fenland_gov_uk.py
@@ -1,7 +1,7 @@
-from datetime import datetime
-import requests
 import re
+from datetime import datetime
 
+import requests
 from waste_collection_schedule import Collection
 
 TITLE = "Fenland District Council"
@@ -9,12 +9,7 @@ DESCRIPTION = "Source script for fenland.gov.uk services for Fenland"
 URL = "https://www.fenland.gov.uk/"
 TEST_CASES = {
     "Address1": {"post_code": "PE13 1JR", "house_number": "Flat 1"},
-    "Address2": {"post_code": "PE15 0SD", "house_number": 1}
-}
-
-API_URLS = {
-    "address_search": "https://www.fenland.gov.uk/find?type=postcodesearch&postcode={}",
-    "collection": "https://www.fenland.gov.uk/find?type=loadlayer&layerId=2&uprn={uprn}&lat={lat}&lng={lng}",
+    "Address2": {"post_code": "PE15 0SD", "house_number": 1},
 }
 
 ICON_MAP = {
@@ -32,38 +27,54 @@ class Source:
         self._house_number = house_number
 
     def fetch(self):
-
+        params = {"type": "postcodesearch", "postcode": self._post_code}
         r = requests.get(
-            API_URLS["address_search"].format(self._post_code), headers={"Accept":"application/json"}
+            "https://www.fenland.gov.uk/find",
+            params=params,
+            headers={"Accept": "application/json"},
         )
         r.raise_for_status()
-        addresses = r.json()
 
+        addresses = r.json()
         address_ids = [
-            address for address in addresses if re.search(f"^{self._house_number} ", address["line1"]) # House numbers are not separated from address line
+            address
+            for address in addresses
+            if re.search(
+                f"^{self._house_number} ", address["line1"]
+            )  # House numbers are not separated from address line
         ]
 
         if len(address_ids) == 0:
-            print(addresses)
-            raise Exception(f"Could not find address :: Post Code: {self._post_code} House Number: {self._house_number}")
+            raise Exception(
+                f"Could not find address :: Post Code: {self._post_code} House Number: {self._house_number}"
+            )
 
+        params = {
+            "type": "loadlayer",
+            "layerId": 2,
+            "uprn": address_ids[0]["udprn"],
+            "lat": address_ids[0]["latitude"],
+            "lng": address_ids[0]["longitude"],
+        }
         r = requests.get(
-            API_URLS["collection"].format(uprn = address_ids[0]["udprn"], lat = address_ids[0]["latitude"], lng = address_ids[0]["longitude"]), headers={"Accept":"application/json"}
+            "https://www.fenland.gov.uk/find",
+            params=params,
+            headers={"Accept": "application/json"},
         )
         r.raise_for_status()
-        collections = r.json()["features"][0]["properties"]["upcoming"]
 
         entries = []
 
+        collections = r.json()["features"][0]["properties"]["upcoming"]
         for collectionDate in collections:
             for collection in collectionDate["collections"]:
                 entries.append(
                     Collection(
-                        date = datetime.strptime(
-                                collectionDate["date"], "%Y-%m-%dT%H:%M:%SZ"
-                            ).date(),
-                        t = collection["desc"],
-                        icon = ICON_MAP.get(collection["name"]),
+                        date=datetime.strptime(
+                            collectionDate["date"], "%Y-%m-%dT%H:%M:%SZ"
+                        ).date(),
+                        t=collection["desc"],
+                        icon=ICON_MAP.get(collection["name"]),
                     )
                 )
 
