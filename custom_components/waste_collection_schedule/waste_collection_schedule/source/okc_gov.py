b07382e8def4cfaf1331c8b9d1589b69d1d29b6c
dt215git <86194065+dt215git@users.noreply.github.com>
2023-04-08 11:14:11 +0100

    json decode error caught

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/okc_gov.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/okc_gov.py
@@ -12,7 +12,7 @@ URL = "https://www.okc.gov"
 COUNTRY = "us"
 TEST_CASES = {
     "Test_001": {"objectID": "1781151"},
-    "Test_002": {"objectID": "2022902"},
+    "Test_002": {"objectID": "2002902"},
     "Test_003": {"objectID": 1935340},
 }
 HEADERS = {
@@ -33,44 +33,42 @@ class Source:
     def fetch(self):
 
         s = requests.Session()
+        r = s.get(
+            f"https://data.okc.gov/services/portal/api/data/records/Address%20Trash%20Services?recordID={self._resourceID}",
+            headers=HEADERS
+        )
 
-        try: # API response can be inconsistent. Sometimes it returns json, sometimes not. So catch the error
-            r = s.get(
-                f"https://data.okc.gov/services/portal/api/data/records/Address%20Trash%20Services?recordID={self._resourceID}",
-                headers=HEADERS
-            )
-        except:
-            print("JSON decode error - invalid response returned from data.okc.gov")
-        else:
+        # api response can be inconsistent, so catch cases were it doesn't return a collection schedule
+        try:
             json_data = json.loads(r.text)
+        except:
+            print("JSON Decode Error - invalid response returned from data.okc.gov")
+        else:
+            # Build list of collection categories
+            waste_types = []
+            for item in json_data["Fields"][3:-1]:  # limit to those entries containing collection info
+                waste_types.append(item["FieldName"].replace("Next_", "").split("_")[0])
+            # Build list of collection days/dates
+            waste_dates = []
+            action_day = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+            for item in json_data["Records"][0][3:-1]:# limit to those entries containing collection info
+                if item != "Not Available":  # ignore missing collections
+                    if "day" in item:  # convert day of week into next collection date
+                        while action_day.strftime("%A") != item:
+                            action_day += timedelta(days=+1)
+                        waste_dates.append(action_day.date())
+                    else:
+                        waste_dates.append(datetime.strptime(item, "%b %d, %Y").date())
+            schedule = list(zip(waste_types, waste_dates))
 
-        # Build list of collection categories
-        waste_types = []
-        for item in json_data["Fields"][3:-1]:  # limit to those entries containing collection info
-            waste_types.append(item["FieldName"].replace("Next_", "").split("_")[0])
-
-        # Build list of collection days/dates
-        waste_dates = []
-        action_day = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-        for item in json_data["Records"][0][3:-1]:# limit to those entries containing collection info
-            if item != "Not Available":  # ignore missing collections
-                if "day" in item:  # convert day of week into next collection date
-                    while action_day.strftime("%A") != item:
-                        action_day += timedelta(days=+1)
-                    waste_dates.append(action_day.date())
-                else:
-                    waste_dates.append(datetime.strptime(item, "%b %d, %Y").date())
-
-        schedule = list(zip(waste_types, waste_dates))
-
-        entries = []
-        for waste in schedule:
-            entries.append(
-                Collection(
-                    date=waste[1],
-                    t=waste[0],
-                    icon=ICON_MAP.get(waste[0].upper()),
-                )
-            )            
+            entries = []
+            for waste in schedule:
+                entries.append(
+                    Collection(
+                        date=waste[1],
+                        t=waste[0],
+                        icon=ICON_MAP.get(waste[0].upper()),
+                    )
+                )            
 
         return entries
