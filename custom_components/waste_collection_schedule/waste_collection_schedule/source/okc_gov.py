58f43ba25c48b129348c3f4c223e72524880a0f6
dt215git <86194065+dt215git@users.noreply.github.com>
2023-04-10 10:39:29 +0100

    Exception handling updated as per comment thread

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/okc_gov.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/okc_gov.py
@@ -13,7 +13,7 @@ COUNTRY = "us"
 TEST_CASES = {
     "Test_001": {"objectID": "1781151"},
     "Test_002": {"objectID": "2002902"},
-    "Test_003": {"objectID": 1935340},
+    "Test_003": {"objectID": "1935340"},
 }
 HEADERS = {
 "content-type": "text/json",
@@ -28,24 +28,23 @@ ICON_MAP = {
 
 class Source:
     def __init__(self, objectID):
-        self._resourceID = str(objectID)
+        self._recordID = str(objectID)
 
     def fetch(self):
 
         s = requests.Session()
         r = s.get(
-            f"https://data.okc.gov/services/portal/api/data/records/Address%20Trash%20Services?recordID={self._resourceID}",
-            headers=HEADERS
+            f"https://data.okc.gov/services/portal/api/data/records/Address%20Trash%20Services?recordID={self._recordID}",
+            headers=HEADERS,
         )
 
-        # api response can be inconsistent, so catch cases were it doesn't return a collection schedule
         try:
             json_data = json.loads(r.text)
-        except:
-            print("JSON Decode Error - invalid response returned from data.okc.gov")
+        except Exception as e:
+            raise Exception("invalid response returned from data.okc.gov") from e
         else:
-            # Build list of collection categories
             waste_types = []
+            # Build list of collection categories
             for item in json_data["Fields"][3:-1]:  # limit to those entries containing collection info
                 waste_types.append(item["FieldName"].replace("Next_", "").split("_")[0])
             # Build list of collection days/dates
@@ -72,3 +71,44 @@ class Source:
                 )            
 
         return entries
+
+
+        # # api response can be inconsistent, so catch cases were it doesn't return a collection schedule
+        ## try:
+        ##     json_data = json.loads(r.text)
+        
+        # try:
+        #     json_data = json.loads(r.text)
+        # except Exception as e:
+        #     raise Exception("Invalid response returned from data.okc.gov") from e
+        ## except:
+        ##     print("JSON Decode Error - invalid response returned from data.okc.gov")
+        # else:
+        #     # Build list of collection categories
+        #     waste_types = []
+        #     for item in json_data["Fields"][3:-1]:  # limit to those entries containing collection info
+        #         waste_types.append(item["FieldName"].replace("Next_", "").split("_")[0])
+        #     # Build list of collection days/dates
+        #     waste_dates = []
+        #     action_day = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        #     for item in json_data["Records"][0][3:-1]:# limit to those entries containing collection info
+        #         if item != "Not Available":  # ignore missing collections
+        #             if "day" in item:  # convert day of week into next collection date
+        #                 while action_day.strftime("%A") != item:
+        #                     action_day += timedelta(days=+1)
+        #                 waste_dates.append(action_day.date())
+        #             else:
+        #                 waste_dates.append(datetime.strptime(item, "%b %d, %Y").date())
+        #     schedule = list(zip(waste_types, waste_dates))
+
+        #     entries = []
+        #     for waste in schedule:
+        #         entries.append(
+        #             Collection(
+        #                 date=waste[1],
+        #                 t=waste[0],
+        #                 icon=ICON_MAP.get(waste[0].upper()),
+        #             )
+        #         )            
+
+        # return entries
