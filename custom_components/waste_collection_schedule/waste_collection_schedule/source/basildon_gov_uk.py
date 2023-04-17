8f03045f5b28f6cd7a97197a8c647f6a246c3ace
Ian Hartwell <contactcentrevision+ian@gmail.com>
2023-04-17 13:39:48 +0100

    adding basildon gov uk

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/basildon_gov_uk.py
@@ -0,0 +1,63 @@
+import requests
+import bs4
+import re
+
+from datetime import datetime
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
+
+TITLE = "Basildon Borough Council"
+DESCRIPTION = "Source for basildon.gov.uk services for Basildon Borough Council, UK."
+URL = "https://basildon.gov.uk"
+TEST_CASES = {
+    "Test_001": {"postcode": "CM111BJ", "address": "6, HEADLEY ROAD"}
+}
+HEADERS = {
+    "user-agent": "Mozilla/5.0",
+}
+ICON_MAP = {
+    "DryNext": "mdi:recycle",
+    "GlassNext": "mdi:glass-fragile",
+    "RubbishNext": "mdi:trash-can",
+    "GardenAndFoodNext": "mdi:leaf",
+}
+
+
+class Source:
+    def __init__(self, postcode, address):
+        self._postcode = postcode
+        self._address = address
+
+    def fetch(self):
+
+        s = requests.Session()
+
+        data = {
+            "__Click": "$Refresh",
+            "txtPostcode": self._postcode,
+            "txtAddress": self._address
+        }
+        schedule_request = s.post(
+            f"https://www3.basildon.gov.uk/website2/postcodes.nsf/frmMyBasildon",
+            headers=HEADERS,
+            data=data
+        )
+        html_rowdata = schedule_request.content
+        rowdata = bs4.BeautifulSoup(html_rowdata, "html.parser")
+
+        # Extract bin types and next collection dates
+        entries = []
+        for bintype in ICON_MAP:
+            for i in range(1,7):
+                bintype_date = rowdata.find('input', {'name': f'{bintype}{i}'})
+                if bintype_date:
+                    date_str = bintype_date['value']
+                    date_str = re.sub(r'\b(\d+)(st|nd|rd|th)\b', r'\1', date_str)
+                    entries.append(
+                        Collection(
+                            t=bintype,
+                            date=datetime.strptime(date_str, "%a, %d %b %Y").date(),
+                            icon=ICON_MAP.get(bintype)
+                        )
+                    )
+
+        return entries
---
+++ doc/source/basildon_gov_uk.md
@@ -0,0 +1,36 @@
+# Basingstoke and Deane Borough Council
+
+Support for schedules provided by [Basildon Borough Council](https://www3.basildon.gov.uk/website2/postcodes.nsf/frmMyBasildon), UK.
+
+## Configuration via configuration.yaml
+
+```yaml
+waste_collection_schedule:
+    sources:
+    - name: basildon_gov_uk
+      args:
+        postcode: POSTCODE
+        address: FIRST LINE OF ADDRESS (X, STREET NAME)
+```
+
+### Configuration Variables
+
+**postcode**  
+*(string) (required)*
+**address**  
+*(string) (required)*
+
+## Example
+
+```yaml
+waste_collection_schedule:
+    sources:
+    - name: basildon_gov_uk
+      args:
+        postcode: CM111BJ
+        address: 6, HEADLEY ROAD
+```
+
+## How to get the source argument
+
+An easy way to discover your Postcode and address used by this service is by going to <https://www3.basildon.gov.uk/website2/postcodes.nsf/frmMyBasildon> and entering in your address details.
