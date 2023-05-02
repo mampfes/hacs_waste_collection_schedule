5096eefc10a904c2f5468b575e90537d8792684a
dt215git <86194065+dt215git@users.noreply.github.com>
2023-05-02 17:05:39 +0100

    bin_type edit removed

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/binzone_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/binzone_uk.py
@@ -79,7 +79,7 @@ class Source:
             try:
                 # No date validation since year isn't included on webpage
                 bin_date = bin_info[0].strip().replace("Your usual collection day is different this week", "")
-                bin_type = bin_info[1].strip().replace("Your usual collection day is different this week", "")
+                bin_type = bin_info[1].strip()
             except Exception as ex:
                 raise ValueError(f"Error parsing bin data: {ex}")
 
