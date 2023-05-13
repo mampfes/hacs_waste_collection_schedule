484066f84c26dff2a07df66e161acd8e717e4159
5ila5 <5ila5@users.noreply.github.com>
2023-05-13 23:05:38 +0200

    updated potsdam Fisished. More like js

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/potsdam_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/potsdam_de.py
@@ -9,49 +9,27 @@ TITLE = "Potsdam"
 DESCRIPTION = "Source for Potsdam."
 URL = "https://www.potsdam.de"
 TEST_CASES = {
-    # "Golm Akazienweg All weekly": {
-    #    "ortsteil": "Golm",
-    #    "strasse": "Akazienweg",
-    #    "rest_rythm": 2,
-    #    "papier_rythm": 2,
-    #    "bio_rythm": 2,
-    # },
-    # "Golm Akazienweg All 2 weekly": {
-    #    "ortsteil": "Golm",
-    #    "strasse": "Akazienweg",
-    #    "rest_rythm": 3,
-    #    "papier_rythm": 3,
-    #    "bio_rythm": 3,
-    # }
-    #"Golm Akazienweg mixed ": {
-    #"ortsteil": "Golm",
-    #"strasse": "Akazienweg",
-    #"rest_rythm": 4,
-    #"papier_rythm": 4,
-    #"bio_rythm": 3,
-    #},
-    # "Teltower Vorstadt  Albert-Einstein-Str. 2 weekly ": {
-    #    "ortsteil": "Teltower Vorstadt",
-    #    "strasse": "Albert-Einstein-Str.",
-    #    "rest_rythm": 3,
-    #    "papier_rythm": 3,
-    #    "bio_rythm": 3,
-    # }
-    #"Teltower Vorstadt  Albert-Einstein-Str. 2 weekly ": {
-    #    "ortsteil": "Teltower Vorstadt",
-    #    "strasse": "Albert-Einstein-Str.",
-    #    "rest_rythm": 4,
-    #    "papier_rythm": 3,
-    #    "bio_rythm": 3,
-    #}
+    "Golm Akazienweg mixed ": {
+        "ortsteil": "Golm",
+        "strasse": "Akazienweg",
+        "rest_rhythm": 4,
+        "papier_rhythm": 2,
+        "bio_rhythm": 3,
+    },
+    "Teltower Vorstadt  Albert-Einstein-Str. 2": {
+        "ortsteil": "Teltower Vorstadt",
+        "strasse": "Albert-Einstein-Str.",
+        "rest_rhythm": 1,
+        "papier_rhythm": 3,
+        "bio_rhythm": 5,
+    },
     "Uetz  Kanalweg ": {
         "ortsteil": "Uetz",
         "strasse": "Kanalweg",
-        "rest_rythm": 4,
-        "papier_rythm": 4,
-        "bio_rythm": 3,
+        "rest_rhythm": 4,
+        "papier_rhythm": 4,
+        "bio_rhythm": 2,
     }
-
 }
 
 
@@ -73,37 +51,44 @@ TYPE_MAP = {
     6: "Weihnachtsbaumabholung",
 }
 
-RYTHM_MAP = {
-    1: {"name": "2x pro Woche", "rule": 1},
-    2: {"name": "wöchentlich", "rule": 1},
-    3: {"name": "14-tägig", "rule": 2},
-    4: {"name": "4-wöchentlich", "rule": 4},
-    5: {"name": "Kombileerung", "rule": 1},
+RHYTHM_MAP = {
+    1: "2x pro Woche",
+    2: "wöchentlich",
+    3: "14-tägig",
+    4: "4-wöchentlich",
+    5: "Kombileerung",
 }
 
 
 API_URL = "https://www.geben-und-nehmen-markt.de/abfallkalender/potsdam/{year}/page-data/index/page-data.json"
 
 
-# tag[2] correct for rythm 4 for trash type 1
-
 class Source:
-    def __init__(self, ortsteil: str, strasse: str, rest_rythm: int, papier_rythm: int, bio_rythm: int, gelb_rythm: int = 3):
+    def __init__(self, ortsteil: str, strasse: str, rest_rhythm: int = 0, papier_rhythm: int = 0, bio_rhythm: int = 0, gelb_rhythm: int = 3):
         self._ortsteil: str = ortsteil
         self._strasse: str = strasse
-        self._rythms = {
-            1: rest_rythm,
-            2: bio_rythm,
-            3: gelb_rythm,
-            4: papier_rythm,
+        self._rhythms = {
+            1: int(rest_rhythm),
+            2: int(bio_rhythm),
+            3: int(gelb_rhythm),
+            4: int(papier_rhythm),
         }
 
-        # self._rest_rythm: int = rest_rythm
-        # self._papier_rythm: int= papier_rythm
-        # self._bio_rythm:int=bio_rythm
-        # self._gelb_rythm:int = gelb_rythm
+    def __get_entries_for_street(self, year: int, today: datetime) -> list[dict[str, str | int]] | None:
+        try:
+            # get json file
+            r = requests.get(API_URL.format(year=year))
+            r.raise_for_status()
+        except:
+            if year == today.year:
+                raise Exception("culd not get data from url exit")
+            return
 
-    def __get_entries_for_street(self, data) -> list[dict[str, str | int]]:
+        try:
+            data = r.json()["result"]["data"]["dbapi"]
+        except Exception as e:
+            raise Exception("culd not decode server response") from e
+
         for ortsteil in data["allRegionen"][0]["regionen"][0]["ortsteile"]:
             if ortsteil["name"].lower().strip() != self._ortsteil.lower().strip():
                 continue
@@ -112,136 +97,82 @@ class Source:
                     continue
                 return strasse["eintraege"]
 
-    def __get_dates(self, entry, year, rythm_interval, now, days:dict[str,int] ) -> list[datetime]:
-        # Need to capture older dates if I want to get their replacement dates if they are in the future
-        date_start_calc = now - timedelta(weeks=4)
-        dates: list[datetime] = []
+    # generate dates, typematch and date_is_collection* are nearly 1:1 implementation of teir original crappy js code
+    def __generate_dates(self, start, end):
+        while start <= end:
+            yield start
+            start += timedelta(days=1)
+
+    def __typematch(self, partm, partd, bin_type, turnus, schwarz, gruen, blau):
+        startkombi = '0401'
+        endekombi = '1031'
+
+        return ((bin_type == 1) and (schwarz == turnus)) or \
+            ((bin_type == 2) and ((gruen == turnus) or ((gruen == 5) and (((turnus == 2) and (partm + partd >= startkombi) and (partm + partd <= endekombi)) or ((turnus == 3) and ((partm + partd < startkombi) or (partm + partd > endekombi))))))) or \
+            ((bin_type == 4) and (blau == turnus)) or \
+            (bin_type == 3)
+
+    def __date_is_collection(self, node, day: datetime, exceptions) -> bool:
+        return self.__date_is_collection_1_to_4(node, day, exceptions) or self.__date_is_collection5_6(node, day)
+
+    def __date_is_collection5_6(self, node, day: datetime) -> bool:
+        if not node["typ"] in (5, 6):
+            return False
         termin1 = datetime.fromisoformat(
-            entry["termin1"].replace("Z", "+00:00"))
+            node["termin1"].replace("Z", "+00:00")).date()
         termin2 = datetime.fromisoformat(
-            entry["termin2"].replace("Z", "+00:00"))
-        if days["tag1"] == 0:
-            if termin1.date() > now.date():
-                dates.append(termin1)
-            if termin1 != termin2 and termin2.date() > now.date():
-                dates.append(termin2)
+            node["termin2"].replace("Z", "+00:00")).date()
+        day = day.date()
+        return (termin1 == day) or (termin2 == day)
 
-        else:
-            print("typ", entry["typ"], "rythm", entry["rhythmus"],  "woche", entry["woche"],"woche2", entry["woche2"], "tag1", days["tag1"], "termin1", entry["termin1"], "tag2", days["tag2"], "termin2", entry["termin2"])
-            if entry["typ"] in self._rythms and self._rythms[entry["typ"]] == 1: #twice a week
-                start1 = (termin1 + timedelta(weeks=(entry["rhythmus"])%4+1+entry["woche"])).date()
-            else:
-                start1 = (termin1 + timedelta(weeks=entry["woche"])).date()
-                
+    def __date_is_collection_1_to_4(self, node, day: datetime, exceptions, check_exception=True) -> bool:
+        weekno = day.isocalendar()[1]
+        weekeven = weekno % 2 == 0
+        dow = day.weekday() + 1
 
-            dates.extend(list(rrule.rrule(interval=rythm_interval, freq=rrule.WEEKLY, dtstart=start1, byweekday=entry["tag1"]-1).between(date_start_calc, datetime(
-                year+1, 2, 1), inc=True)))
+        if check_exception:
+            for e_from, e_to in exceptions.items():
+                if day == e_from:
+                    return False
+                if day == e_to:
+                    return self.__date_is_collection_1_to_4(node, e_from, exceptions, check_exception=False)
 
-            # bio Kombileerung (* 1.4. bis 31.10. = weekly 1.11. bis 31.3. = every 14 days)
-            if entry["typ"] == 2 and self._rythms[2] == 5:
-                # remove dates that are not in the weekly scedule
-                dates = list(filter(lambda x: 4 <= x.month <= 10, dates))
-
-                winter_dates = list(rrule.rrule(interval=2, freq=rrule.WEEKLY, dtstart=start1, byweekday=entry["tag1"]-1).between(date_start_calc, datetime(
-                    year+1, 2, 1), inc=True))
-
-                winter_dates = list(
-                    filter(lambda x: not (4 <= x.month <= 10), winter_dates))
-                dates.extend(winter_dates)
-
-            if (entry["typ"] in self._rythms and self._rythms[entry["typ"]] == 1 and days["tag2"] != 0 and days["tag2"] != days["tag1"]):
-                print("TAHG2")
-                start2 = (termin2 + timedelta(weeks=entry["woche2"]-2)).date()
-                dates.extend(list(rrule.rrule(interval=rythm_interval, freq=rrule.WEEKLY, dtstart=start2, byweekday=days["tag2"]-1).between(date_start_calc, datetime(
-                    year+1, 2, 1), inc=True)))
-
-        return dates
+        return (((node["rhythmus"] == 1) and self.__typematch(day.strftime("%m"), day.strftime("%d"), node["typ"], 2, self._rhythms[1], self._rhythms[2], self._rhythms[4])) and (((node["tag1"] == dow) and (node["tag2"] == 0)) or (((node["tag1"] == dow) and not weekeven) or ((node["tag2"] == dow) and weekeven)))) or \
+            (((node["rhythmus"] == 2) and self.__typematch(day.strftime("%m"), day.strftime("%d"), node["typ"], 3, self._rhythms[1], self._rhythms[2], self._rhythms[4])) and (((node["woche"] == 2) and not weekeven) or ((node["woche"] == 3) and weekeven)) and (node["tag1"] == dow)) or \
+            (((node["rhythmus"] == 3) and self.__typematch(day.strftime("%m"), day.strftime("%d"), node["typ"], 4, self._rhythms[1], self._rhythms[2], self._rhythms[4])) and ((weekno - node["beginn"]) % 4 == 0) and (node["tag1"] == dow)) or \
+            (((node["rhythmus"] == 4) and self.__typematch(day.strftime("%m"), day.strftime("%d"), node["typ"], 1, self._rhythms[1], self._rhythms[2], self._rhythms[4])) and ((node["tag1"] == dow) or ((node["tag2"] > 0) and (node["tag2"] == dow)))) and \
+            (weekno >= node["beginn"])
 
     def fetch(self):
-        today = datetime.now()
+        today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        collections: list[Collection] = []
+
         years = [today.year]
         if today.month == 12:
             years.append(today.year + 1)
 
-        collection_entries = []
-
         for year in years:
-            try:
-                # get json file
-                r = requests.get(API_URL.format(year=year))
-                r.raise_for_status()
-            except:
-                if year == today.year:
-                    raise Exception("culd not get data from url exit")
+            entries = self.__get_entries_for_street(year, today)
+            if today.year != year:
+                today = datetime(year, 1, 1)
+
+            if entries is None:
                 continue
 
-            data = r.json()["result"]["data"]["dbapi"]
-            today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+            for entry in entries:
+                exceptions = {datetime.strptime(d_from, "%d.%m.%Y"): datetime.strptime(d_to, "%d.%m.%Y") for d_from, d_to in json.loads(entry["ausnahmen"]).items()}
+                icon = ICON_MAP.get(entry["typ"])
+                type = TYPE_MAP.get(entry["typ"])
+                rhythm_type = self._rhythms.get(entry["typ"])
+                if (rhythm_type in RHYTHM_MAP):
+                    type += " (" + RHYTHM_MAP.get(rhythm_type) + ")"
 
-            street_entries = self.__get_entries_for_street(data)
-
-
-            entries_categoriezed: dict[int, list[dict[str, str | int]]] = {}
-            entries_to_use: list[dict[str, str | int]] = []
-            for entry in street_entries:
-
-                if entry["typ"] in entries_categoriezed:
-                    entries_categoriezed[entry["typ"]].append(entry)
-                else:
-                    entries_categoriezed[entry["typ"]] = [entry]
-            
-            days_by_type:dict[int,dict[str,int]] = {}
-            
-            for type, type_entries in entries_categoriezed.items():
-                days_by_type[type] = {"tag1": 0, "tag2": 0}
-                for entry in type_entries:
-                    if entry["tag1"] != 0:
-                        days_by_type[type]["tag1"] = entry["tag1"]
-                    if entry["tag2"] != 0:
-                        days_by_type[type]["tag2"] = entry["tag2"]
-                        
-                type_entries = filter(lambda x: (x["rhythmus"] <=2 or x["rhythmus"] <= self._rythms[x["typ"]]) and x["rhythmus"] <4, type_entries)
-                
-                if type == 2 and self._rythms[2] == 5:  # bio Kombileerung
-                    entries_to_use.extend(
-                        filter(lambda x: x["rhythmus"] == 2, type_entries))    
-                else:
-                    # add best fitting entry
-                    sorted_type_entries = sorted(
-                        type_entries, key=lambda x: x["rhythmus"], reverse=True)
-                    entries_to_use.append(sorted_type_entries[0])
-
-            
-            collection_entries = []
-
-            for entry in entries_to_use:
-
-                exception_map = {datetime.strptime(key, "%d.%m.%Y"): datetime.strptime(
-                    value, "%d.%m.%Y") for key, value in json.loads(entry["ausnahmen"]).items()}
-
-                rythm_type = self._rythms[entry["typ"]
-                                          ] if entry["typ"] in self._rythms else entry["rhythmus"]
-                rythm = RYTHM_MAP[rythm_type]["rule"]
-
-                dates: list[datetime] = self.__get_dates(
-                    entry, year, rythm, today, days_by_type[entry["typ"]])
-
-                for date in dates:
-                    if date in exception_map:
-                        date = exception_map[date]
-                    if date.date() < today.date():
+                for day in self.__generate_dates(today, datetime(year+1, 1, 1)):
+                    if not self.__date_is_collection(entry, day, exceptions):
                         continue
-                    # Collection icon
-                    icon = ICON_MAP.get(entry["typ"])
-                    type = TYPE_MAP.get(entry["typ"])
-                    if (rythm_type in RYTHM_MAP and entry["tag1"] != 0):
-                        type += " (" + RYTHM_MAP.get(
-                            rythm_type)["name"] + ")"
-                    collection_entries.append(Collection(
-                        date=date.date(), t=type, icon=icon))
 
-            # entries = []
+                    collections.append(Collection(
+                        date=day.date(), t=type, icon=icon))
 
-            collection_entries.sort(key=lambda x: x.date)
-
-        return collection_entries
+        collections.sort(key=lambda x: x.date)
+        return collections
--- doc/source/potsdam_de.md
+++ doc/source/potsdam_de.md
@@ -11,6 +11,10 @@ waste_collection_schedule:
       args:
         ortsteil: ORTSTEIL
         strasse: STRAßE
+        rest_rhythm: RHYTMUS Restabfall
+        papier_rhythm: RHYTMUS Altpapier
+        bio_rhythm: RHYTMUS Bioabfall
+        gelb_rhythm: RHYTMUS Leichtverpackungen
         
 ```
 
@@ -18,22 +22,64 @@ waste_collection_schedule:
 
 **ortsteil**  
 *(String) (required)*
+
 **strasse**  
 *(String) (required)*
 
+**rest_rhythm**  
+(Integer) (optional)*
+Rhythm for `Restabfall`
 
-## Example
+**papier_rhythm**  
+(Integer) (optional)*
+Rhythm for `Altpapier`
+
+**bio_rhythm**  
+(Integer) (optional)*  
+Rhythm for `Bioabfall`
+
+**gelb_rhythm**  
+(Integer) (optional)*  
+Rhythm for `Leichtverpackungen`. Defaults to 3 probably does not need to change
+
+rhythm parameters must be selected like on the Potsdam website as follows:
+|Rhythm|Value|
+|-|-|
+|kein ...|0|
+|2x pro Woche|1|
+|Wöchentlich|2|
+|14-taglich|3|
+|4-wöchentlich|4|
+|Kombileerung|5|
+
+## Examples
 
 ```yaml
 waste_collection_schedule:
     sources:
     - name: potsdam_de
       args:
-        ortsteil: Golm
-        strasse: Akazienweg
-        
+        ortsteil: Teltower Vorstadt
+        strasse: Albert-Einstein-Str.
+        rest_rhythm: 1
+        papier_rhythm: 3
+        bio_rhythm: 5
+```
+
+```yaml
+waste_collection_schedule:
+    sources:
+    - name: potsdam_de
+      args:
+        ortsteil: Uetz
+        strasse: Kanalweg
+        rest_rhythm: 4
+        papier_rhythm: 4
+        bio_rhythm: 2
 ```
 
 ## How to get the source argument
 
 Find the parameter of your address using [https://www.potsdam.de/abfallkalender-fuer-potsdam](https://www.potsdam.de/abfallkalender-fuer-potsdam) and write them exactly like on the web page.
+
+Set the rhythm parameters like you would on the webpage (see list above for rhythm - value mapping).
