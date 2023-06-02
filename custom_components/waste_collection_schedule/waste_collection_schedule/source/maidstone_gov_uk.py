97b2b3bab9a6aaf9f4eb8de3165d6dc7f51de340
CalamityJames <me@calamityjames.dev>
2023-06-02 17:42:02 +0100

    Added test for uprn with garden waste

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/maidstone_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/maidstone_gov_uk.py
@@ -12,7 +12,8 @@ DESCRIPTION = "Source for maidstone.gov.uk services for Maidstone Borough Counci
 URL = "https://maidstone.gov.uk"
 TEST_CASES = {
     "Test_001": {"uprn": "10022892379"}, # has mutliple collections on same week per bin type
-    "Test_002": {"uprn": "10014307164"} # has duplicates of the same collection (two bins for this block of flats?)
+    "Test_002": {"uprn": "10014307164"}, # has duplicates of the same collection (two bins for this block of flats?)
+    "Test_003": {"uprn": "200003674881"} # has garden waste collection, at time of coding
 }
 HEADERS = {
     "user-agent": "Mozilla/5.0",
