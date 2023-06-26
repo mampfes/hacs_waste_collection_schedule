941cff6656b8f556785ccf62ca779e262220d011
dt215git <86194065+dt215git@users.noreply.github.com>
2023-06-26 17:31:54 +0100

    Residential test case added

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/republicservices_com.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/republicservices_com.py
@@ -12,6 +12,7 @@ TEST_CASES = {
     "Scott Country Clerk": {"street_address": "101 E Main St, Georgetown, KY 40324"},
     "Branch County Clerk": {"street_address": "31 Division St. Coldwater, MI 49036"},
     "Contract Collection": {"street_address": "8957 Park Meadows Dr, Elk Grove, CA 95624"},
+    "Residential Collection": {"street_address": "117 Roxie Ln, Georgetown, KY 40324"},
 
 }
 DELAYS = {
@@ -38,13 +39,9 @@ class Source:
             params={"addressLine1": self._street_address},
         )
         r0_json = json.loads(r0.text)["data"][0]
-
         address_hash = r0_json["addressHash"]
         longitude = r0_json["longitude"]
         latitude = r0_json["latitude"]
-        # postal = r0_json["postalCode"]
-        service = ""
-        day_offset = 0
 
         # get raw schedule and build dict
         r1 = requests.get(
@@ -52,7 +49,7 @@ class Source:
             params={"siteAddressHash": address_hash},
         )
         r1_json = json.loads(r1.text)["data"]
-
+        service = ""
         schedule = {}
         for service_type in r1_json:
             if hasattr(service_type, "__iter__") and service_type != "isColaAccount":
@@ -73,13 +70,13 @@ class Source:
                         i += 1
 
         # compile holidays that impact collections
-        r3 = s.get(f"https://www.republicservices.com/api/v3/holidaySchedules/schedules", params={"latitude": latitude, "longitude": longitude})
-        r3_json = json.loads(r3.text)["data"]
-
+        r2 = s.get(f"https://www.republicservices.com/api/v3/holidaySchedules/schedules", params={"latitude": latitude, "longitude": longitude})
+        r2_json = json.loads(r2.text)["data"]
+        day_offset = 0
         i = 0
         holidays = {}
-        for item in r3_json:
-            if item["serviceImpacted"] == True and item["LOB"] == service:
+        for item in r2_json:
+            if item["serviceImpacted"] == "true" and item["LOB"] == service:
                 for delay in DELAYS:
                     if delay in item["description"]:
                         day_offset = DELAYS[delay]
@@ -107,7 +104,6 @@ class Source:
                     if date_difference <= 5 and date_difference >=0: # is this right???
                         revised_date = p + timedelta(days = d)
                         schedule[pickup]["date"] = revised_date
-                        # increment marker
                         changes += 1
             if changes == 0:
                 break
