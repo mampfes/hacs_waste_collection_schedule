be9d61f3c2f4982d0c7f9f099060a9244a047b8a
5ila5 <5ila5@users.noreply.github.com>
2023-07-18 18:04:48 +0200

    minor fixed + reofrmatting - add numeric test case - fix payload to support numeric uprns - reformatting - make uprn required not optional

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/stratford_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/stratford_gov_uk.py
@@ -1,47 +1,60 @@
+from datetime import datetime
+
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = "Stratford District Council"
-DESCRIPTION = "Source for Stratford District Council and their 123+ bin collection system"
+DESCRIPTION = (
+    "Source for Stratford District Council and their 123+ bin collection system"
+)
 URL = "https://stratford.gov.uk"
 
-TEST_CASES = { # if you want your address removed, please submit a request and this will be done
-    "Stratford DC": {"uprn": "100071513500"}, # doesnt have food waste
-    "Alscot Estate": {"uprn": "10024633309"}
-}
+TEST_CASES = (
+    {  # if you want your address removed, please submit a request and this will be done
+        "Stratford DC": {"uprn": "100071513500"},  # doesn't have food waste
+        "Alscot Estate": {"uprn": 10024633309},
+    }
+)
 
 ICON_MAP = {
     "Garden waste": "mdi:leaf",
     "Refuse": "mdi:trash-can",
     "Recycling": "mdi:recycle",
-    "Food waste": "mdi:food-apple"
+    "Food waste": "mdi:food-apple",
 }
 # order of BINS is important, it's the order they appear left-to-right in the table.
 # these names have been chosen to accurately reflect naming convention on Stratford.gov
-BINS = [ "Food waste", "Recycling", "Refuse", "Garden waste" ]
+BINS = ["Food waste", "Recycling", "Refuse", "Garden waste"]
 
-API_URL = "https://www.stratford.gov.uk/waste-recycling/when-we-collect.cfm/part/calendar"
-HEADERS = {
-    # "User-Agent": "Mozilla/5.0",
-    "Content-Type": "application/x-www-form-urlencoded"
-}
+API_URL = (
+    "https://www.stratford.gov.uk/waste-recycling/when-we-collect.cfm/part/calendar"
+)
+HEADERS = {"Content-Type": "application/x-www-form-urlencoded"}
 
-DATE_FORMAT = "%A, %d/%m/%Y" # format of the date string in the collection table
+DATE_FORMAT = "%A, %d/%m/%Y"  # format of the date string in the collection table
+
 
 class Source:
-    def __init__(self, uprn=""):
-        self._payload = "frmAddress1=&frmAddress2=&frmAddress3=&frmAddress4=&frmPostcode=" # fill in the address with blanks, dont need it
-        self._payload += "&frmUPRN=" + uprn # only need to provide uprn. but we DO need to have the keys for the rest of the address.
+    def __init__(self, uprn):
+        # fill in the address with blanks, dont need it
+        # self._payload += "&frmUPRN=" + uprn # only need to provide uprn. but we DO need to have the keys for the rest of the address.
+        self._payload = {
+            "frmAddress1": "",
+            "frmAddress2": "",
+            "frmAddress3": "",
+            "frmAddress4": "",
+            "frmPostcode": "",
+            "frmUPRN": uprn,
+        }
 
     def fetch(self):
-        r = requests.post(API_URL, data = self._payload, headers=HEADERS)
+        r = requests.post(API_URL, data=self._payload, headers=HEADERS)
         soup = BeautifulSoup(r.text, features="html.parser")
 
         # Retrieve collection details
         entries = []
-        table = soup.find("table", class_="table") # yes really
+        table = soup.find("table", class_="table")  # yes really
 
         # each row is a date, and its given collections
         for row in table.tbody.find_all("tr"):
@@ -52,16 +65,12 @@ class Source:
             # there are 4 bins per row, this gets them
             all_bins = row.find_all("td", class_="text-center")
 
-            bins_collections = []
-
             # each bin may be "checked" to show it can be collected on that date
             for idx, cell in enumerate(all_bins):
                 if cell.find("img", class_="check-img"):
 
-                    entries.append(Collection(
-                        date = date,
-                        t = BINS[idx],
-                        icon = ICON_MAP.get(BINS[idx])
-                    ))
+                    entries.append(
+                        Collection(date=date, t=BINS[idx], icon=ICON_MAP.get(BINS[idx]))
+                    )
 
         return entries
--- doc/source/stratford_gov_uk.md
+++ doc/source/stratford_gov_uk.md
@@ -15,9 +15,7 @@ waste_collection_schedule:
 ### Configuration Variables
 
 **uprn**  
-*(string) (optional) (preferred method)*
-
-This is required if you do not supply any other options. Using a UPRN removes the need to do an address look up using web requests.
+*(string) (required)*
 
 ## Example using UPRN
 
@@ -36,4 +34,3 @@ An easy way to find your Unique Property Reference Number (UPRN) is by going to 
 Otherwise you can use the Inspect tool after typing your address on Stratford-on-Avon's [When We Collect](https://www.stratford.gov.uk/waste-recycling/when-we-collect.cfm) page.
 
 Simply search for your address with the Inspect tool set to view Network requests, look for the POST request and view its payload. You're looking for `frmUPRN=`.
-
