6c3cd09acaf375ea8818484914bb1dd58ee439aa
5ila5 <38183212+5ila5@users.noreply.github.com>
2023-07-19 17:54:41 +0200

    fixed Norfolk and Broadland (#1104)
    Co-authored-by: 5ila5 <5ila5@users.noreply.github.com>

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/south_norfolk_and_broadland_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/south_norfolk_and_broadland_gov_uk.py
@@ -11,11 +11,11 @@ from waste_collection_schedule import Collection
 
 TITLE = "Broadland District Council"
 DESCRIPTION = "Source for southnorfolkandbroadland.gov.uk services for South Norfolk and Broadland, UK"
-URL = "https://southnorfolkandbroadland.gov.uk/"
+URL = "https://area.southnorfolkandbroadland.gov.uk/"
 EXTRA_INFO = [
     {
         "title": "South Norfolk Council",
-        "url": "https://southnorfolkandbroadland.gov.uk/"
+        "url": "https://southnorfolkandbroadland.gov.uk/",
     },
 ]
 TEST_CASES = {
@@ -29,7 +29,7 @@ TEST_CASES = {
             "Parish": "Sprowston",
             "Village": "Sprowston",
             "Street": "Mallard Way",
-            "Authority": "2610"
+            "Authority": "2610",
         }
     },
     "Random address new Method": {
@@ -46,35 +46,45 @@ TEST_CASES = {
             "Parish": "Sprowston",
             "Village": "Sprowston",
             "Street": "Blue Boar Lane",
-            "Authority": "2610"
+            "Authority": "2610",
         }
-    }
+    },
 }
 
 ICON_MAP = {
     "Rubbish": "mdi:trash-can",
     "Recycling": "mdi:recycle",
-    "Garden (if applicable)": "mdi:leaf"
+    "Garden (if applicable)": "mdi:leaf",
 }
 
 matcher = re.compile(r"^([A-Z][a-z]+) (\d{1,2}) ([A-Z][a-z]+) (\d{4})$")
 
+
 def parse_date(date_str: str) -> date:
     match = matcher.match(date_str)
+    if match is None:
+        raise ValueError(f"Unable to parse date {date_str}")
+
     return date(
         int(match.group(4)),
         strptime(match.group(3)[:3], "%b").tm_mon,
-        int(match.group(2))
+        int(match.group(2)),
     )
 
 
-def comparable(data:str) -> str:
+def comparable(data: str) -> str:
     return data.replace(",", "").replace(" ", "").lower()
 
-class Source:
-    _address_payload: dict
 
-    def __init__(self, address_payload: dict = None, postcode: str = None, address: str = None):
+class Source:
+    _address_payload: dict | None
+
+    def __init__(
+        self,
+        address_payload: dict | None = None,
+        postcode: str | None = None,
+        address: str | None = None,
+    ):
         self._address_payload = address_payload
         self._postcode = comparable(postcode) if postcode else None
         self._address = address if address else None
@@ -82,13 +92,14 @@ class Source:
     def fetch(self) -> List[Collection]:
         if self._address_payload:
             return self.__fetch_by_payload()
-        if not self._postcode or not self._address:
-            raise ValueError(
-                "Either (address_payload) or (postcode and address) must be provided")
-
         return self.__fetch_by_postcode_and_address()
 
     def __fetch_by_postcode_and_address(self) -> List[Collection]:
+        if not self._postcode or not self._address:
+            raise ValueError(
+                "Either (address_payload) or (postcode and address) must be provided"
+            )
+
         session = requests.Session()
         r = session.get(URL + "FindAddress")
         r.raise_for_status()
@@ -96,26 +107,32 @@ class Source:
 
         args = {
             "Postcode": self._postcode,
-            "__RequestVerificationToken": page.find("input", {"name": "__RequestVerificationToken"})["value"]
+            "__RequestVerificationToken": page.find(
+                "input", {"name": "__RequestVerificationToken"}
+            )["value"],
         }
         r = session.post(URL + "FindAddress", data=args)
         r.raise_for_status()
         page = soup(r.text, "html.parser")
-        addresses = page.find(
-            "select", {"id": "UprnAddress"}).find_all("option")
+        addresses = page.find("select", {"id": "UprnAddress"}).find_all("option")
 
         if not addresses:
             raise ValueError(f"no addresses found for postcode {self._postcode}")
 
-        args["__RequestVerificationToken"] = page.find("input", {"name": "__RequestVerificationToken"})["value"]
+        args["__RequestVerificationToken"] = page.find(
+            "input", {"name": "__RequestVerificationToken"}
+        )["value"]
 
         found = False
         compare_address = self._address.replace(",", "").replace(" ", "").lower()
 
         for address in addresses:
             address_text = comparable(address.text)
-            
-            if address_text == compare_address or address_text == compare_address.replace(self._postcode, ""):
+
+            if (
+                address_text == compare_address
+                or address_text == compare_address.replace(self._postcode, "")
+            ):
                 args["UprnAddress"] = address["value"]
                 found = True
                 break
@@ -123,12 +140,17 @@ class Source:
         if not found:
             raise ValueError(f"Address {self._address} not found")
 
-        r = session.post(URL+"FindAddress/Submit", data=args)
+        r = session.post(URL + "FindAddress/Submit", data=args)
         r.raise_for_status()
         return self.__get_data(r)
 
     def __fetch_by_payload(self) -> List[Collection]:
-        r = requests.get(URL, headers={"Cookie": f"MyArea.Data={quote(json.dumps(self._address_payload))}"})
+        r = requests.get(
+            URL,
+            headers={
+                "Cookie": f"MyArea.Data={quote(json.dumps(self._address_payload))}"
+            },
+        )
         r.raise_for_status()
         return self.__get_data(r)
 
@@ -141,8 +163,7 @@ class Source:
             Collection(
                 parse_date(tuple(bin_category.children)[3].strip()),
                 tuple(bin_category.children)[1].text.strip(),
-                icon=ICON_MAP.get(tuple(bin_category.children)[1].text.strip())
+                icon=ICON_MAP.get(tuple(bin_category.children)[1].text.strip()),
             )
-            for bin_category
-            in bin_categories
+            for bin_category in bin_categories
         ]
