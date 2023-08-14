9e2a4651be569468a94099159634e8b8f8919ba1
dt215git <86194065+dt215git@users.noreply.github.com>
2023-08-14 17:05:53 +0100

    test cases added

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/aylesburyvaledc_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/aylesburyvaledc_gov_uk.py
@@ -1,16 +1,15 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
-
 import requests
-from waste_collection_schedule import Collection  # type: ignore[attr-defined]
+from waste_collection_schedule import Collection
 
 TITLE = "Aylesbury Vale District Council"
 DESCRIPTION = "Source for aylesburyvaledc.gov.uk services for Aylesbury Vale District Council, UK."
 URL = "https://aylesburyvaledc.gov.uk"
 TEST_CASES = {
-    "Test_001": {"uprn": "100011821616"},
-    "Test_002": {"uprn": "100011776859"},
-    "Test_003": {"uprn": 100011749007},
+    "Test_001": {"uprn": "766292368"},
+    "Test_002": {"uprn": 766310306},
+    "Test_003": {"uprn": "766029949"},
 }
 ICON_MAP = {
     "REFUSE": "mdi:trash-can",
@@ -34,11 +33,12 @@ class Source:
             <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
                 <soap12:Body>
                     <GetCollections xmlns="http://tempuri.org/">
-                        <uprn>{uprn}</uprn>
+                        <uprn>{self._uprn}</uprn>
                     </GetCollections>
                 </soap12:Body>
             </soap12:Envelope>
         """
+        
         response = requests.post(url,data=body,headers=headers)
         soup = BeautifulSoup(response.content, "xml")
         bins = soup.find_all("BinCollection")
@@ -51,11 +51,10 @@ class Source:
                 if w.text == "true":
                     entries.append(
                         Collection(
-                            date=datetime.strptime(dt, "").date(),
+                            date=datetime.strptime(dt.text, "%Y-%m-%dT07:00:00").date(),
                             t=waste,
                             icon=ICON_MAP.get(waste),
                         )
                     )
-                    # print(dt.text, waste)   
 
         return entries
