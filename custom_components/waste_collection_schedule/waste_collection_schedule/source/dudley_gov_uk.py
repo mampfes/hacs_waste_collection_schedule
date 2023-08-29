8f98959315f1ceb5bd1a7eea83bc3af6f5b82b1a
dt215git <86194065+dt215git@users.noreply.github.com>
2023-08-29 12:38:23 +0100

    New Source: Dudley, UK (#1195)
    * initial commit
    
    * script backbone implemented
    
    * test cases added
    
    * dudley .md added
    
    * typo
    
    * update_docu_links run
    
    * reformatting (@5ila5)

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/dudley_gov_uk.py
@@ -0,0 +1,92 @@
+import re
+from datetime import datetime, timedelta
+
+import requests
+from bs4 import BeautifulSoup
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
+
+TITLE = "Dudley Metropolitan Borough Council"
+DESCRIPTION = "Source for Dudley Metropolitan Borough Council, UK."
+URL = "https://dudley.gov.uk"
+TEST_CASES = {
+    "Test_001": {"uprn": "90090715"},
+    "Test_002": {"uprn": 90104555},
+    "Test_003": {"uprn": "90164803"},
+    "Test_004": {"uprn": 90092621},
+}
+ICON_MAP = {"RECYCLING": "mdi:recycle", "GARDEN": "mdi:leaf", "REFUSE": "mdi:trash-can"}
+REGEX = r"(\d+ \w{3})"
+
+
+class Source:
+    def __init__(self, uprn: str | int):
+        self._uprn = str(uprn)
+
+    def check_date(self, d: str, t: datetime, y: int):
+        """
+        Get date, append year, and increment year if date is >1 month in the past.
+
+        This tries to deal year-end dates when the YEAR is missing
+        """
+        d += " " + str(y)
+        date = datetime.strptime(d, "%d %b %Y")
+        if (date - t) < timedelta(days=-31):
+            date = date.replace(year=date.year + 1)
+        return date.date()
+
+    def append_entries(self, d: datetime, w: str, e: list) -> list:
+        """
+        Append provided entry and Refuse entry for the same day.
+
+        Refuse is collected on the same dates as alternating Recycling/Garden collections,
+        so create two entries for each date Refuse & Recycling, or Refuse & Garden
+        """
+        e.append(
+            Collection(
+                date=d,
+                t=w,
+                icon=ICON_MAP.get(w.upper()),
+            )
+        )
+        e.append(
+            Collection(
+                date=d,
+                t="Refuse",
+                icon=ICON_MAP.get("REFUSE"),
+            )
+        )
+        return e
+
+    def fetch(self):
+
+        today = datetime.now()
+        today = today.replace(hour=0, minute=0, second=0, microsecond=0)
+        yr = int(today.year)
+
+        s = requests.Session()
+        r = s.get(
+            f"https://maps.dudley.gov.uk/?action=SetAddress&UniqueId={self._uprn}"
+        )
+        soup = BeautifulSoup(r.text, "html.parser")
+
+        panel = soup.find("div", {"aria-label": "Refuse and Recycling Collection"})
+        panel_data = panel.find("div", {"class": "atPanelData"})
+        panel_data = panel_data.text.split("Next")[
+            1:
+        ]  # remove first element it just contains general info
+
+        entries = []
+        for item in panel_data:
+            text = item.replace("\r\n", "").strip()
+            if "recycling" in text:
+                dates = re.findall(REGEX, text)
+                for dt in dates:
+                    dt = self.check_date(dt, today, yr)
+                    self.append_entries(dt, "Recycling", entries)
+            elif "garden" in text:
+                dates = re.findall(REGEX, text)
+                for dt in dates:
+                    dt = self.check_date(dt, today, yr)
+                    self.append_entries(dt, "Garden", entries)
+
+        return entries
