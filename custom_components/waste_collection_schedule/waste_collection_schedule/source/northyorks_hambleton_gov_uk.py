6b7ebbe1856d5da41b0d544b7cbff87dc1a954a7
5ila5 <5ila5@users.noreply.github.com>
2023-08-31 22:10:18 +0200

    Hambleton fix wrong dates (before merge)

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/northyorks_hambleton_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/northyorks_hambleton_gov_uk.py
@@ -1,11 +1,9 @@
 # Highly based on milton_keynes_gov_uk.py
 
-import re
-from datetime import datetime, timedelta
+from datetime import datetime
 from time import time_ns
 
 import requests
-from bs4 import BeautifulSoup
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = "North Yorkshire Council - Hambleton"
@@ -20,7 +18,7 @@ TEST_CASES = {
 
 ICON_MAP = {
     "REFUSE": "mdi:trash-can",
-    "GARDEN": "mdi:leaf",
+    "GARDEN WASTE": "mdi:leaf",
     "RECYCLING": "mdi:recycle",
 }
 
@@ -28,21 +26,19 @@ HEADERS = {
     "user-agent": "Mozilla/5.0",
 }
 
-DATE_REGEX = r"\d{1,2}\s\w{3,4}\s\d{4}"
-
 API_URL = "https://hambletondc-self.achieveservice.com/service/Bin_collection_finder"
 
 
 class Source:
     def __init__(self, uprn: str | int):
-        self._uprn: str | int = uprn
+        self._uprn: str = str(uprn).zfill(12)
 
     def fetch(self):
         s = requests.Session()
 
         # Set up session
         timestamp = time_ns() // 1_000_000  # epoch time in milliseconds
-        r = s.get(
+        s.get(
             "https://hambletondc-self.achieveservice.com/apibroker/domain/hambletondc-self.achieveservice.com",
             params={
                 "_": timestamp,
@@ -67,12 +63,8 @@ class Source:
         payload = {
             "formValues": {
                 "Address search": {
-                    "pccUPRN": {
-                        "value": self._uprn
-                    },  # Probably not needed but does not hurt
+                    "pccUPRN": {"value": self._uprn},
                     "selectedUPRN": {"value": self._uprn},
-                    "GWStatusNumLicences_PurchasePeriod": {"value": "1"},
-                    "pccNumWeeks": {"value": "23"},
                 }
             }
         }
@@ -80,8 +72,7 @@ class Source:
             "https://hambletondc-self.achieveservice.com/apibroker/runLookup",
             headers=HEADERS,
             params={
-                # "id": "62b1d2c960a47",
-                "id": "5b3254c44316c",
+                "id": "62b1d2c960a47",
                 "repeat_against": "",
                 "noRetry": "true",
                 "getOnlyTokens": "undefined",
@@ -96,29 +87,12 @@ class Source:
 
         # Extract bin types and next collection dates
         entries = []
-        for index, item in rowdata.items():
-            bin_types = []
-            soup = BeautifulSoup(item["pccTableHTML"], "html.parser")
-            headline_table, collection_table = soup.find_all("table")[:2]
-            for bin_type in headline_table.find_all("td")[1:]:
-                if not bin_type.text.strip():
-                    continue
-                bin_types.append(bin_type.text.strip())
-
-            for collection_table_row in collection_table.find_all("tr"):
-                tds = collection_table_row.find_all("td")
-                date_str = re.search(DATE_REGEX, tds[0].text.strip()).group()
-
-                date = datetime.strptime(date_str, "%d %b %Y").date()
-                for index, td in enumerate(tds[1:]):
-                    if td.find("img"):
-                        if r := re.search(
-                            r"\d+ days? later than usual", td.text.strip()
-                        ):
-                            delta = int(re.search(r"\d", r.group()).group())
-                            date = date + timedelta(days=delta)
-                        icon = ICON_MAP.get(bin_types[index].upper())
-                        entries.append(
-                            Collection(t=bin_types[index], date=date, icon=icon),
-                        )
+        for item in rowdata.values():
+            bin_type = item["Collection_Type"]
+            date_str = item["Collection_Date"]
+            date = datetime.strptime(date_str, "%Y-%m-%d").date()
+            icon = ICON_MAP.get(bin_type.upper().replace("COLLECTION", ""))
+            entries.append(
+                Collection(t=bin_type, date=date, icon=icon),
+            )
         return entries
