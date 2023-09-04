26661ca5573f4880bc1dea2e78aa1e6597899ff2
dt215git <86194065+dt215git@users.noreply.github.com>
2023-09-04 19:47:16 +0100

    script  updated with 'address' option

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/wokingham_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/wokingham_gov_uk.py
@@ -14,6 +14,7 @@ TEST_CASES = {
     "Test_001": {"postcode": "RG40 1GE", "property": "56199"},
     "Test_002": {"postcode": "RG413BP", "property": "55588"},
     "Test_003": {"postcode": "rg41 1ph", "property": 61541},
+    "Test_004": {"postcode": "RG40 2LW", "address": "16 Davy Close"},
 }
 ICON_MAP = {
     "HOUSEHOLD WASTE AND RECYCLING": "mdi:trash-can",
@@ -29,9 +30,11 @@ HEADERS = {
 
 
 class Source:
-    def __init__(self, postcode, property):
-        self._postcode = str(postcode.upper().strip().replace(" ", ""))
-        self._property = str(property)
+    def __init__(self, postcode=None, property=None, address=None):
+        # self._postcode = str(postcode.upper().strip().replace(" ", ""))
+        self._postcode = postcode
+        self._property = property
+        self._address = address
 
     def get_form_id(self, txt: str) -> str:
         soup = BeautifulSoup(txt, "html.parser")
@@ -39,17 +42,24 @@ class Source:
         id = x.get("value")
         return id
 
+    def match_address(self, lst: list, addr: str) -> str:
+        for item in lst:
+            if addr in item.text.replace(",", ""):
+                a = item.get("value")
+        return a
+
     def fetch(self):
 
         s = requests.Session()
 
-        # load page to generate token needed for subsequent query
+        # Load page to generate token needed for subsequent query
         r = s.get(
             API_URL,
         )
         form_id = self.get_form_id(r.text)
 
         # Perform postcode search to generate token needed for following query
+        self._postcode = str(self._postcode.upper().strip().replace(" ", ""))
         payload = {
             "postcode_search": self._postcode,
             "op": "Find address",
@@ -62,6 +72,14 @@ class Source:
             data=payload,
         )
         form_id = self.get_form_id(r.text)
+
+        # Use address to get an ID if property wasn't supplied. Assumes first match is correct.
+        if self._property is None:
+            soup = BeautifulSoup(r.text, "html.parser")
+            dropdown = soup.find("div", {"class": "form-item__dropdown"})
+            addresses = dropdown.find_all("option")
+            self._address = self._address.upper()
+            self._property = self.match_address(addresses, self._address)
 
         # Now get the collection schedule
         payload = {
