eec22d49a3762b109b130d436bb409559eaf32e7
5ila5 <5ila5@users.noreply.github.com>
2023-09-09 01:58:45 +0200

    remove dependency + add next year check + reformatting

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/mamirolle_info.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/mamirolle_info.py
@@ -1,4 +1,5 @@
 import datetime
+
 import requests
 from bs4 import BeautifulSoup
 from waste_collection_schedule import Collection
@@ -8,9 +9,7 @@ DESCRIPTION = "Source script for mamirolle.info"
 COUNTRY = "fr"
 URL = "http://mamirolle.info/"
 
-TEST_CASES = {
-    "TestSource": {}
-}
+TEST_CASES = {"TestSource": {}}
 
 ICON_MAP = {
     "Poubelle grise": "mdi:trash-can",
@@ -32,30 +31,37 @@ MONTH_NAMES = [
     "décembre",
 ]
 
-class Source:
 
+class Source:
     def fetch(self):
+        now = datetime.datetime.now()
         # get list of regions and weblinks
         page = requests.get(URL)
         # A lenient HTML parser is need
-        soup = BeautifulSoup(page.text, "html5lib")
-
+        soup = BeautifulSoup(page.text.replace("<![endif]", ""), "html.parser")
         trash_domestic = soup.find("i", class_="poubelle-grise")
-        weekday, day, month = trash_domestic.next_sibling.string.split()
-        date_domestic = datetime.datetime.now().replace(month=MONTH_NAMES.index(month), day=int(day)).date()
+        _, day, month = trash_domestic.next_sibling.string.split()
+        date_domestic = now.replace(month=MONTH_NAMES.index(month), day=int(day)).date()
+        if date_domestic < now.date():
+            date_domestic = date_domestic.replace(year=date_domestic.year + 1)
 
         trash_recycle = soup.find("i", class_="poubelle-jaune")
-        weekday, day, month = trash_recycle.next_sibling.string.split()
-        date_recycle = datetime.datetime.now().replace(month=MONTH_NAMES.index(month), day=int(day)).date()
+        _, day, month = trash_recycle.next_sibling.string.split()
+        date_recycle = now.replace(month=MONTH_NAMES.index(month), day=int(day)).date()
+        if date_recycle < now.date():
+            date_recycle = date_recycle.replace(year=date_recycle.year + 1)
 
-        entries = [Collection(
-            date=date_domestic,
-            t="Poubelle grise",
-            icon=ICON_MAP.get("Poubelle grise"),
-        ), Collection(
-            date=date_recycle,
-            t="Poubelle jaune",
-            icon=ICON_MAP.get("Poubelle jaune"),
-        )]  # List that holds collection schedule
+        entries = [
+            Collection(
+                date=date_domestic,
+                t="Poubelle grise",
+                icon=ICON_MAP.get("Poubelle grise"),
+            ),
+            Collection(
+                date=date_recycle,
+                t="Poubelle jaune",
+                icon=ICON_MAP.get("Poubelle jaune"),
+            ),
+        ]  # List that holds collection schedule
 
         return entries
