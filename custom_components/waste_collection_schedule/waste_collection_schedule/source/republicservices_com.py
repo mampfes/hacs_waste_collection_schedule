d3c1b16391a36d62da21aee984e7e02b112aa126
dt215git <86194065+dt215git@users.noreply.github.com>
2023-10-22 13:37:25 +0100

    yard waste entry implemented

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/republicservices_com.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/republicservices_com.py
@@ -1,7 +1,7 @@
-import requests
 import json
-
 from datetime import datetime, timedelta
+
+import requests
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = "Republic Services"
@@ -9,11 +9,22 @@ DESCRIPTION = "Source for Republic Services Collection."
 URL = "https://www.republicservices.com"
 COUNTRY = "us"
 TEST_CASES = {
-    "Scott Country Clerk": {"street_address": "101 E Main St, Georgetown, KY 40324"},
-    "Branch County Clerk": {"street_address": "31 Division St. Coldwater, MI 49036"},
-    "Contract Collection": {"street_address": "8957 Park Meadows Dr, Elk Grove, CA 95624"},
-    "Residential Collection": {"street_address": "117 Roxie Ln, Georgetown, KY 40324"},
-
+    "Scott Country Clerk": {
+        "street_address": "101 E Main St, Georgetown, KY 40324",
+        "method": 1,
+    },
+    "Branch County Clerk": {
+        "street_address": "31 Division St. Coldwater, MI 49036",
+        "method": "1",
+    },
+    "Contract Collection": {
+        "street_address": "8957 Park Meadows Dr, Elk Grove, CA 95624",
+        "method": 2,
+    },
+    "Residential Collection": {
+        "street_address": "117 Roxie Ln, Georgetown, KY 40324",
+        "method": "2",
+    },
 }
 DELAYS = {
     " one ": 1,
@@ -27,8 +38,9 @@ DELAYS = {
 
 
 class Source:
-    def __init__(self, street_address):
+    def __init__(self, street_address, method=2):
         self._street_address = street_address
+        self._method = int(method)
 
     def fetch(self):
         s = requests.Session()
@@ -59,35 +71,42 @@ class Source:
                         dt = datetime.strptime(day, "%Y-%m-%d").date()
                         service = item["containerCategory"]
                         schedule.update(
-                            {i: {
-                                "date": dt,
-                                "waste_type": item["wasteTypeDescription"],
-                                "waste_description": item["productDescription"],
-                                "service": service,
+                            {
+                                i: {
+                                    "date": dt,
+                                    "waste_type": item["wasteTypeDescription"],
+                                    "waste_description": item["productDescription"],
+                                    "service": service,
                                 }
                             }
                         )
                         i += 1
 
         # Compile holidays that impact collections
-        r2 = s.get(f"https://www.republicservices.com/api/v3/holidaySchedules/schedules", params={"latitude": latitude, "longitude": longitude})
+        r2 = s.get(
+            "https://www.republicservices.com/api/v3/holidaySchedules/schedules",
+            params={"latitude": latitude, "longitude": longitude},
+        )
         r2_json = json.loads(r2.text)["data"]
         day_offset = 0
         i = 0
         holidays = {}
         for item in r2_json:
-            if item["serviceImpacted"] == True and item["LOB"] == service:
+            if item["serviceImpacted"] is True and item["LOB"] == service:
                 for delay in DELAYS:
                     if delay in item["description"]:
                         day_offset = DELAYS[delay]
-                dt = datetime.strptime(item["date"], "%Y-%m-%dT00:00:00.0000000Z").date()
+                dt = datetime.strptime(
+                    item["date"], "%Y-%m-%dT00:00:00.0000000Z"
+                ).date()
                 holidays.update(
-                    {i: {
-                        "date": dt,
-                        "name": item["name"],
-                        "description": item["description"],
-                        "delay": day_offset,
-                        "incorporated": False
+                    {
+                        i: {
+                            "date": dt,
+                            "name": item["name"],
+                            "description": item["description"],
+                            "delay": day_offset,
+                            "incorporated": False,
                         }
                     }
                 )
@@ -104,7 +123,7 @@ class Source:
                         p = schedule[pickup]["date"]
                         date_difference = (p - h).days
                         if date_difference <= 5 and date_difference >= 0:
-                            revised_date = p + timedelta(days = d)
+                            revised_date = p + timedelta(days=d)
                             schedule[pickup]["date"] = revised_date
                             holidays[holiday]["incorporated"] = True
                             # print(p, h, d, date_difference, revised_date)
@@ -114,19 +133,40 @@ class Source:
 
         # Build final schedule (implements original logic for assigning icon)
         entries = []
-        for item in schedule:
-            if "RECYCLE" in schedule[item]["waste_description"]:
-                icon = "mdi:recycle"
-            elif "YARD" in schedule[item]["waste_description"]:
-                icon = "mdi:leaf"
-            else:
-                icon = "mdi:trash-can"
-            entries.append(
-                Collection(
-                    date=schedule[item]["date"],
-                    t=schedule[item]["waste_type"],
-                    icon=icon,
-                ),
-            )
-        
+        if self._method == 1:  # Original logic
+            for item in schedule:
+                if "RECYCLE" in schedule[item]["waste_description"]:
+                    icon = "mdi:recycle"
+                elif "YARD" in schedule[item]["waste_description"]:
+                    icon = "mdi:leaf"
+                else:
+                    icon = "mdi:trash-can"
+                entries.append(
+                    Collection(
+                        date=schedule[item]["date"],
+                        t=schedule[item]["waste_type"],
+                        icon=icon,
+                    ),
+                )
+        elif (
+            self._method == 2
+        ):  # Updated to report yard waste as a separate category to recycling
+            for item in schedule:
+                if "YARD" in schedule[item]["waste_description"]:
+                    icon = "mdi:leaf"
+                    schedule[item]["waste_type"] = "Yard Waste"
+                elif "RECYCLE" in schedule[item]["waste_description"]:
+                    icon = "mdi:recycle"
+                elif "YARD" in schedule[item]["waste_description"]:
+                    icon = "mdi:leaf"
+                else:
+                    icon = "mdi:trash-can"
+                entries.append(
+                    Collection(
+                        date=schedule[item]["date"],
+                        t=schedule[item]["waste_type"],
+                        icon=icon,
+                    ),
+                )
+
         return entries
