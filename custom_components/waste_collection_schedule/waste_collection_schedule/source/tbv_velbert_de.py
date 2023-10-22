9ac4ce2609978515c253d4c93fafba5c79d4a889
5ila5 <5ila5@users.noreply.github.com>
2023-10-22 15:37:34 +0200

    reformatting

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/tbv_velbert_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/tbv_velbert_de.py
@@ -1,16 +1,16 @@
 import datetime
-import requests
 import logging
 
+import requests
 from bs4 import BeautifulSoup
-from waste_collection_schedule import Collection
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = "TBV Velbert"
 DESCRIPTION = "Source script for tbv-velbert.de, germany"
 URL = "https://www.tbv-velbert.de"
 TEST_CASES = {
     "Lindenkamp": {"street": "Am Lindenkamp 33"},
-    "Rathaus": {"street": "Thomasstraße 1"}
+    "Rathaus": {"street": "Thomasstraße 1"},
 }
 
 API_URL = "https://www.tbv-velbert.de/abfall/abfallkalender-und-abfuhrtermine/abfallabfuhr-suche"
@@ -24,6 +24,7 @@ ICON_MAP = {
 
 _LOGGER = logging.getLogger(__name__)
 
+
 class Source:
     def __init__(self, street):
         self._street = street
@@ -33,11 +34,13 @@ class Source:
             API_URL,
             headers=HEADERS,
             data={
-                'tx_tbvabfall_strassensuche[abgeschickt]': 1,
-                'tx_tbvabfall_strassensuche[suchbegriff]': self._street,
+                "tx_tbvabfall_strassensuche[abgeschickt]": 1,
+                "tx_tbvabfall_strassensuche[suchbegriff]": self._street,
             },
         )
-        _LOGGER.debug(f"Fetching waste schedule for \'{self._street}\' returned status: {r.status_code}.")
+        _LOGGER.debug(
+            f"Fetching waste schedule for '{self._street}' returned status: {r.status_code}."
+        )
         r.raise_for_status()
 
         parser = BeautifulSoup(r.text, "html.parser")
@@ -47,14 +50,16 @@ class Source:
 
         entries = []
         for i in range(0, len(elements), 2):
-            waste_type = elements[i].get_text()[0:-1]             # strip trailing colon
-            waste_dates = elements[i+1].get_text().split(":")[1]  # get dates after colon
-            waste_dates = waste_dates.strip().split(" ")          # convert to list
-            waste_dates[:] = [w for w in waste_dates if w]        # remove empty strings
+            waste_type = elements[i].get_text()[0:-1]  # strip trailing colon
+            waste_dates = (
+                elements[i + 1].get_text().split(":")[1]
+            )  # get dates after colon
+            waste_dates = waste_dates.strip().split(" ")  # convert to list
+            waste_dates[:] = [w for w in waste_dates if w]  # remove empty strings
             for d in waste_dates:
                 entries.append(
                     Collection(
-                        datetime.datetime.strptime(d, '%d.%m.%Y').date(),
+                        datetime.datetime.strptime(d, "%d.%m.%Y").date(),
                         waste_type,
                         ICON_MAP.get(waste_type),
                     )
