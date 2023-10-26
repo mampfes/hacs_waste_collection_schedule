a4f6846b99714e3e6dff778c20adaf1879a2f076
Aghilas MESSARA <aghilas.messara@gmail.com>
2023-10-26 17:38:14 +0200

    Fix month and refactor date

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/mamirolle_info.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/mamirolle_info.py
@@ -48,28 +48,19 @@ class Source:
         # A lenient HTML parser is need
         soup = BeautifulSoup(page.text.replace("<![endif]", ""), "html.parser")
         trash_domestic = soup.find("i", class_="poubelle-grise")
-        _, day, month = trash_domestic.next_sibling.string.split()
-        date_domestic = now.replace(month=MONTH_NAMES.index(month), day=int(day)).date()
-        if date_domestic < now.date():
-            date_domestic = date_domestic.replace(year=date_domestic.year + 1)
-
         trash_recycle = soup.find("i", class_="poubelle-jaune")
-        _, day, month = trash_recycle.next_sibling.string.split()
-        date_recycle = now.replace(month=MONTH_NAMES.index(month), day=int(day)).date()
-        if date_recycle < now.date():
-            date_recycle = date_recycle.replace(year=date_recycle.year + 1)
 
-        entries = [
-            Collection(
-                date=date_domestic,
-                t="Poubelle grise",
-                icon=ICON_MAP.get("Poubelle grise"),
-            ),
-            Collection(
-                date=date_recycle,
-                t="Poubelle jaune",
-                icon=ICON_MAP.get("Poubelle jaune"),
-            ),
-        ]  # List that holds collection schedule
+        entries = []  # List that holds collection schedule
+        for trash, label in [(trash_domestic, "Poubelle grise"), (trash_recycle, "Poubelle jaune")]:
+            _, day, month = trash.next_sibling.string.split()
+            date = now.replace(month=MONTH_NAMES.index(month) + 1, day=int(day)).date()
+            if date < now.date():
+                date = date.replace(year=date.year + 1)
+
+            entries.append(Collection(
+                date=date,
+                t=label,
+                icon=ICON_MAP.get(label),
+            ))
 
         return entries
