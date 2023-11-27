a8e60f1d5b02d65355aa9215ed9a25bfdd4a3f50
Pawel Szmajda <szmajdap@gmail.com>
2023-11-27 17:14:23 +0000

    Fixed duplicate collections for southampton_gov_uk.py (#1451)
    * filtered page text to avoid duplicates
    
    * changed filtering regex to lazy
    
    * reformat
    
    ---------
    
    Co-authored-by: 5ila5 <5ila5@users.noreply.github.com>

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/southampton_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/southampton_gov_uk.py
@@ -1,14 +1,12 @@
 import logging
 import re
-import requests
-
 from datetime import datetime
-from waste_collection_schedule import Collection
+
+import requests
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = "Southampton City Council"
-DESCRIPTION = (
-    "Source for southampton.gov.uk services for Southampton City Council"
-)
+DESCRIPTION = "Source for southampton.gov.uk services for Southampton City Council"
 URL = "https://southampton.gov.uk"
 TEST_CASES = {
     "UPRN_001": {"uprn": "100060731893"},
@@ -20,10 +18,10 @@ ICON_MAP = {
     "Glass": "mdi:glass-fragile",
     "Recycling": "mdi:recycle",
     "General Waste": "mdi:trash-can",
-    "Garden Waste": "mdi:leaf"
+    "Garden Waste": "mdi:leaf",
 }
 REGEX = (
-    "(Glass|Recycling|General Waste|Garden Waste).*?([0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4})"
+    r"(Glass|Recycling|General Waste|Garden Waste).*?([0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4})"
 )
 
 _LOGGER = logging.getLogger(__name__)
@@ -35,18 +33,25 @@ class Source:
 
     def fetch(self):
         s = requests.Session()
-        r = s.get(f"https://www.southampton.gov.uk/whereilive/waste-calendar?UPRN={self._uprn}")
+        r = s.get(
+            f"https://www.southampton.gov.uk/whereilive/waste-calendar?UPRN={self._uprn}"
+        )
         r.raise_for_status()
 
-        results = re.findall(REGEX, r.text)
+        # Limit search scope to avoid duplicates
+        calendar_view_only = re.search(
+            r"#calendar1.*?listView", r.text, flags=re.DOTALL
+        )[0]
+
+        results = re.findall(REGEX, calendar_view_only)
 
         entries = []
         for item in results:
             entries.append(
                 Collection(
-                    date = datetime.strptime(item[1], "%m/%d/%Y").date(),
-                    t = item[0],
-                    icon = ICON_MAP.get(item[0]),
+                    date=datetime.strptime(item[1], "%m/%d/%Y").date(),
+                    t=item[0],
+                    icon=ICON_MAP.get(item[0]),
                 )
             )
 
