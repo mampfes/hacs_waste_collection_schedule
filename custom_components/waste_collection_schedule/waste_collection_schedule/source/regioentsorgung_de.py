e222d56ad4aacd7fabff111adf5f40d945419afc
5ila5 <5ila5@users.noreply.github.com>
2023-12-01 10:08:19 +0100

    regioentsorgugn_de get entries for all years + reformatting

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/regioentsorgung_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/regioentsorgung_de.py
@@ -1,12 +1,14 @@
+from datetime import datetime
+
 import requests
-from waste_collection_schedule import Collection
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 from waste_collection_schedule.service.ICS import ICS
 
 TITLE = "RegioEntsorgung Städteregion Aachen"
 DESCRIPTION = "RegioEntsorgung Städteregion Aachen"
 URL = "https://regioentsorgung.de"
 TEST_CASES = {
-    "Merzbrück": {"city": "Würselen", "street": "Merzbrück", "house_number": 200 },
+    "Merzbrück": {"city": "Würselen", "street": "Merzbrück", "house_number": 200},
 }
 
 API_URL = "https://tonnen.regioentsorgung.de/WasteManagementRegioentsorgung/WasteManagementServlet"
@@ -24,66 +26,75 @@ class Source:
         self._ics = ICS()
 
     def fetch(self):
+        now = datetime.now()
+        year = now.year
+        entries = self.get_collection(year)
+        if now.month == 12:
+            entries += self.get_collection(year + 1)
+        return entries
+
+    def get_collection(self, year):
         # Use a session to keep cookies
         session = requests.Session()
 
         payload = {
-            'SubmitAction': 'wasteDisposalServices',
+            "SubmitAction": "wasteDisposalServices",
         }
         r = session.get(API_URL, headers=HEADERS, params=payload)
         r.raise_for_status()
 
         payload = {
-            'ApplicationName': 'com.athos.kd.regioentsorgung.CheckAbfuhrtermineModel',
-            'SubmitAction': 'CITYCHANGED',
-            'Ort': self.city,
-            'Strasse': '',
-            'Hausnummer': '',
+            "ApplicationName": "com.athos.kd.regioentsorgung.CheckAbfuhrtermineModel",
+            "SubmitAction": "CITYCHANGED",
+            "Ort": self.city,
+            "Strasse": "",
+            "Hausnummer": "",
         }
         r = session.post(API_URL, headers=HEADERS, data=payload)
         r.raise_for_status()
 
         payload = {
-            'ApplicationName': 'com.athos.kd.regioentsorgung.CheckAbfuhrtermineModel',
-            'SubmitAction': 'STREETCHANGED',
-            'Ort': self.city,
-            'Strasse': self.street,
-            'Hausnummer': '',
+            "ApplicationName": "com.athos.kd.regioentsorgung.CheckAbfuhrtermineModel",
+            "SubmitAction": "STREETCHANGED",
+            "Ort": self.city,
+            "Strasse": self.street,
+            "Hausnummer": "",
         }
         r = session.post(API_URL, headers=HEADERS, data=payload)
         r.raise_for_status()
 
         payload = {
-            'ApplicationName': 'com.athos.kd.regioentsorgung.CheckAbfuhrtermineModel',
-            'SubmitAction': 'forward',
-            'Ort': self.city,
-            'Strasse': self.street,
-            'Hausnummer': self.house_number,
+            "ApplicationName": "com.athos.kd.regioentsorgung.CheckAbfuhrtermineModel",
+            "SubmitAction": "forward",
+            "Ort": self.city,
+            "Strasse": self.street,
+            "Hausnummer": self.house_number,
+            "Zeitraum": f"Jahresübersicht {year}",
         }
         r = session.post(API_URL, headers=HEADERS, data=payload)
         r.raise_for_status()
 
         payload = {
-            'ApplicationName': 'com.athos.kd.regioentsorgung.AbfuhrTerminModel',
-            'SubmitAction': 'forward',
+            "ApplicationName": "com.athos.kd.regioentsorgung.AbfuhrTerminModel",
+            "SubmitAction": "forward",
         }
         r = session.post(API_URL, headers=HEADERS, data=payload)
         r.raise_for_status()
 
         payload = {
-            'ApplicationName': 'com.athos.kd.regioentsorgung.AbfuhrTerminDownloadModel',
-            'ContainerGewaehlt_1': 'on',
-            'ContainerGewaehlt_2': 'on',
-            'ContainerGewaehlt_3': 'on',
-            'ContainerGewaehlt_4': 'on',
-            'ContainerGewaehlt_5': 'on',
-            'ContainerGewaehlt_6': 'on',
-            'ContainerGewaehlt_7': 'on',
-            'ContainerGewaehlt_8': 'on',
-            'ContainerGewaehlt_9': 'on',
-            'ICalErinnerung': 'keine Erinnerung',
-            'ICalZeit': '06:00 Uhr',
-            'SubmitAction': 'filedownload_ICAL',
+            "ApplicationName": "com.athos.kd.regioentsorgung.AbfuhrTerminDownloadModel",
+            "ContainerGewaehlt_1": "on",
+            "ContainerGewaehlt_2": "on",
+            "ContainerGewaehlt_3": "on",
+            "ContainerGewaehlt_4": "on",
+            "ContainerGewaehlt_5": "on",
+            "ContainerGewaehlt_6": "on",
+            "ContainerGewaehlt_7": "on",
+            "ContainerGewaehlt_8": "on",
+            "ContainerGewaehlt_9": "on",
+            "ICalErinnerung": "keine Erinnerung",
+            "ICalZeit": "06:00 Uhr",
+            "SubmitAction": "filedownload_ICAL",
         }
         r = session.post(API_URL, headers=HEADERS, data=payload)
         r.raise_for_status()
