aee9febe25bfcc968948021f8a1301b53913e062
Florian Fish <florian@poissonmail.fr>
2023-12-01 09:18:01 +0100

    Create vernsurseiche_info.py

---
+++ doc/source/vernsurseiche_info.py
@@ -0,0 +1,17 @@
+# Mairie de Vern-sur-Seiche / Corps-Nuds / Nouvoitou / Saint-Armel
+
+Support for Mairie de Vern-sur-Seiche / Corps-Nuds / Nouvoitou / Saint-Armel in France
+
+## Configuration via configuration.yaml
+
+```yaml
+waste_collection_schedule:
+  sources:
+    - name: vernsurseiche_info
+```
+## Sensor setup
+
+There are following types of garbage parsed:
+
+- Poubelle grise
+- Poubelle jaune
