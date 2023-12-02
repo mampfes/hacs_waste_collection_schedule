55ab0bff3ef0eb5060627032b838cbccb8398d32
5ila5 <5ila5@users.noreply.github.com>
2023-12-02 13:52:27 +0100

    reforamtting + ./update_doculinks.py

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/alchenstorf_ch.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/alchenstorf_ch.py
@@ -1,15 +1,15 @@
-import logging
 import json
+import logging
 from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
-from waste_collection_schedule import Collection
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = "Alchenstorf"
 DESCRIPTION = " Source for 'Alchenstorf, CH'"
 URL = "https://www.alchenstorf.ch"
-TEST_CASES = {"TEST": {}}
+TEST_CASES: dict[str, dict] = {"TEST": {}}
 
 ICON_MAP = {
     "Grünabfuhr Alchenstorf": "mdi:leaf",
@@ -24,7 +24,7 @@ _LOGGER = logging.getLogger(__name__)
 
 class Source:
     def __init__(self):
-        self = None
+        pass
 
     def fetch(self):
         response = requests.get("https://www.alchenstorf.ch/abfalldaten")
@@ -36,19 +36,18 @@ class Source:
 
         entries = []
         for item in data["data"]:
+            next_pickup = item["_anlassDate-sort"].split()[0]
+            next_pickup_date = datetime.fromisoformat(next_pickup).date()
 
-                next_pickup = item["_anlassDate-sort"].split()[0]
-                next_pickup_date = datetime.fromisoformat(next_pickup).date()
+            waste_type = BeautifulSoup(item["name"], "html.parser").text
+            waste_type_sorted = BeautifulSoup(item["name-sort"], "html.parser").text
 
-                waste_type = BeautifulSoup(item["name"], "html.parser").text
-                waste_type_sorted = BeautifulSoup(item["name-sort"], "html.parser").text
-
-                entries.append(
-                    Collection(
-                        date=next_pickup_date,
-                        t=waste_type,
-                        icon=ICON_MAP.get(waste_type_sorted, "mdi:trash-can"),
-                    )
+            entries.append(
+                Collection(
+                    date=next_pickup_date,
+                    t=waste_type,
+                    icon=ICON_MAP.get(waste_type_sorted, "mdi:trash-can"),
                 )
+            )
 
         return entries
