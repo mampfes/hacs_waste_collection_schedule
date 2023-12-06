a18e20939d952ee0b4ddee9c27886bfdcf5d6b42
Cameron Clough <cameronjclough@gmail.com>
2023-12-06 15:01:08 +0000

    implement Blackpool Council source

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/blackpool_gov_uk.py
@@ -0,0 +1,62 @@
+import re
+from datetime import datetime
+
+import requests
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
+
+TITLE = "Blackpool Council"
+DESCRIPTION = "Source for blackpool.gov.uk services for Blackpool Council, UK."
+URL = "https://blackpool.gov.uk"
+TEST_CASES = {
+    "Test1": {"postcode": "FY1 4DZ", "uprn": "100010802829"},
+    "Test2": {"postcode": "FY3 9RQ", "uprn": "100010842301"},
+    "Test3": {"postcode": "FY1 2HR", "uprn": "100012606962"},
+}
+
+API_URL = "https://api.blackpool.gov.uk/api/bartec"
+REGEX_JOB_NAME = r"^Empty((?: [A-Za-z]+)+) \d+\w$"
+NAME_MAP = {
+    "Domestic Refuse": "Grey bin or Red sack",
+    "Dry Recycling": "Blue bin",
+}
+ICON_MAP = {
+    "Domestic Refuse": "mdi:trash-can",
+    "Dry Recycling": "mdi:recycle",
+}
+
+
+class Source:
+    def __init__(self, postcode, uprn):
+        self._postcode = str(postcode)
+        self._uprn = str(uprn)
+
+    def fetch(self):
+        s = requests.Session()
+        r0 = s.get(f"{API_URL}/security/token")
+        r0.raise_for_status()
+
+        payload = {
+            "UPRN": self._uprn,
+            "USRN": "",
+            "PostCode": self._postcode,
+            "StreetNumber": "",
+            "CurrentUser": {
+                "UserId": "",
+                "Token": r0.text.strip('"'),
+            },
+        }
+        r1 = s.post(f"{API_URL}/collection/PremiseJobs", json=payload)
+        r1.raise_for_status()
+
+        entries = []
+        for job in r1.json()["jobsField"]:
+            jobName = re.search(REGEX_JOB_NAME, job["jobField"]["nameField"]).group(1).strip()
+            entries.append(
+                Collection(
+                    date=datetime.strptime(job["jobField"]["scheduledStartField"], "%Y-%m-%dT%H:%M:%S").date(),
+                    t=NAME_MAP.get(jobName),
+                    icon=ICON_MAP.get(jobName),
+                )
+            )
+
+        return entries
