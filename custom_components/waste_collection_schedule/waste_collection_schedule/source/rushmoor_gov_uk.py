58763fc1ec3b6b59b2e9b92126d04d2c18a2b43c
5ila5 <5ila5@users.noreply.github.com>
2023-12-09 18:21:23 +0100

    fix rushmoor_gov_uk

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/rushmoor_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/rushmoor_gov_uk.py
@@ -1,6 +1,8 @@
+import json
+from datetime import datetime
+
 import requests
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
-from waste_collection_schedule.service.ICS import ICS
 
 TITLE = "Rushmoor Borough Council"
 DESCRIPTION = "Source for rushmoor.gov.uk services for Rushmoor, UK."
@@ -16,49 +18,31 @@ ICON_MAP = {
     "Food": "mdi:food-apple",
 }
 
-API_URL = "https://www.rushmoor.gov.uk/recycling-rubbish-and-environment/bins-and-recycling/download-or-print-your-bin-collection-calendar/"
+API_URL = "https://www.rushmoor.gov.uk/Umbraco/Api/BinLookUpWorkAround/Get"
 
 
 class Source:
     def __init__(self, uprn):
         self._uprn = uprn
-        self._ics = ICS()
 
     def fetch(self):
-        params = {"uprn": self._uprn, "weeks": "16"}
-        r = requests.post(API_URL, params=params)
+        params = {"selectedAddress": self._uprn, "weeks": "16"}
+        r = requests.get(API_URL, params=params)
         r.raise_for_status()
-
-        dates = self._ics.convert(r.text)
+        # Douple decode to get rid of the escaped quotes
+        data = json.loads(str(r.json()))
 
         entries = []
-        for d in dates:
-            for wasteType in d[1].split("&"):
-                wasteType = wasteType.replace("bin", "")
-                wasteType = wasteType.strip()
-                entries.append(
-                    Collection(
-                        d[0],
-                        wasteType,
-                        icon=ICON_MAP.get(wasteType),
-                    )
-                )
-                # If wasteType is "Refuse" then add a second entry for "Garden"
-                if wasteType == "Refuse":
-                    entries.append(
-                        Collection(
-                            d[0],
-                            "Garden",
-                            icon=ICON_MAP.get("Garden"),
-                        )
-                    )
-
-            # Always add Food as that is collected weekly
+        for key, value in data["NextCollection"].items():
+            if not key.endswith("Date"):
+                continue
+            wasteType = key.split("Collection")[0]
+            date = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S").date()
             entries.append(
                 Collection(
-                    d[0],
-                    "Food",
-                    icon=ICON_MAP.get("Food"),
+                    date,
+                    wasteType,
+                    icon=ICON_MAP.get(wasteType),
                 )
             )
         return entries
