50f5d0dc7aa7b783e7e9ecbcf23d655ac0f1d836
Julien Boriasse <julien@boriasse.com>
2024-01-02 04:45:07 +0100

    Add few test + Try except pass

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/montreal_ca.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/montreal_ca.py
@@ -11,7 +11,9 @@ TITLE = "Montreal"
 DESCRIPTION = "Source script for montreal.ca/info-collectes"
 URL = "https://montreal.ca/info-collectes"
 TEST_CASES = {
-    "Secteur MHM_42-5_B (2358 Monsabre)": {"address": "2358 rue Monsabre"},
+    "6280 Chambord": {"address": "6280 rue Chambord"},
+    "2358 Monsabre": {"address": "2358 rue Monsabre"},
+    "10785 Clark": {"address": "10785 rue Clark"},
 }
 
 API_URL = [
@@ -93,188 +95,201 @@ class Source:
 
         entries = []
         for source in API_URL:
-            # Get waste collection zone by longitude and latitude
-            print("======== Search entries for", source["type"], "waste. ========")
 
-            r = requests.get(source["url"])
-            r.raise_for_status()
+            try:
+                # Get waste collection zone by longitude and latitude
 
-            schedule = r.json()
+                r = requests.get(source["url"])
+                r.raise_for_status()
 
-            # check each polygon to see if it contains the point
-            for feature in schedule['features']:
-                sector_shape = shape(feature['geometry'])
+                schedule = r.json()
 
-                if sector_shape.contains(point):
-                    sector_name = feature['properties']['SECTEUR']
-                    print('Found containing sector:', sector_name)
+                # check each polygon to see if it contains the point
+                for feature in schedule['features']:
+                    sector_shape = shape(feature['geometry'])
 
-                    schedule_message = feature['properties']['MESSAGE_EN']
+                    if sector_shape.contains(point):
+                        sector_name = feature['properties']['SECTEUR']
+                        #print('Found containing sector:', sector_name)
 
-                    days = []
+                        schedule_message = feature['properties']['MESSAGE_EN']
 
-                    # HOUSEHOLD WASTE
-                    if source["type"] == "Waste":
+                        days = []
 
-                        split_waste_schedule_message = schedule_message.split('\n')
-                        month_pattern = r'\b(?:January|February|March|April|May|June|July|August|September|October|November|December) 20[0-9][0-9]\b'
 
-                        for months in split_waste_schedule_message:
-                            if re.search(month_pattern, months):
-                                split_months = months.split(':')
-                                month_year = split_months[0].split(' ')
-                                month_name = month_year[1]
-                                year = int(month_year[2])
+                        # HOUSEHOLD WASTE
+                        if source["type"] == "Waste":
 
-                                # remove * character
-                                split_months[1] = split_months[1].replace('*', '')
+                            split_waste_schedule_message = schedule_message.split('\n')
+                            month_pattern = r'\b(?:January|February|March|April|May|June|July|August|September|October|November|December) 20[0-9][0-9]\b'
 
-                                # Splitting the string by ',' and 'and' to extract individual numbers
-                                days = re.split(r', | and ', split_months[1])
-                                # Converting the extracted strings to integers
-                                days = [int(num) for num in days]
+                            for months in split_waste_schedule_message:
+                                if re.search(month_pattern, months):
+                                    split_months = months.split(':')
+                                    month_year = split_months[0].split(' ')
+                                    month_name = month_year[1]
+                                    year = int(month_year[2])
 
-                                for d in days:
-                                    datetime_obj = datetime(year, datetime.strptime(month_name, '%B').month, d)
-                                    date_obj = datetime_obj.date()
-                                    entries.append(
-                                        Collection(
-                                            date = date_obj,
-                                            t = "Rubbish",
-                                            icon = ICON_MAP.get("Rubbish"),
+                                    # remove * character
+                                    split_months[1] = split_months[1].replace('*', '')
+
+                                    # Splitting the string by ',' and 'and' to extract individual numbers
+                                    days = re.split(r', | and ', split_months[1])
+                                    # Converting the extracted strings to integers
+                                    days = [int(num) for num in days]
+
+                                    for d in days:
+                                        datetime_obj = datetime(year, datetime.strptime(month_name, '%B').month, d)
+                                        date_obj = datetime_obj.date()
+                                        entries.append(
+                                            Collection(
+                                                date = date_obj,
+                                                t = "Rubbish",
+                                                icon = ICON_MAP.get("Rubbish"),
+                                            )
                                         )
-                                    )
 
 
-                    # GREEN WATE
-                    if source["type"] == "Green":
+                        # GREEN WATE
+                        if source["type"] == "Green":
 
-                        # Dictionary of weekdays in English with their corresponding numbers
-                        weekdays = {
-                            "Monday": 0,
-                            "Tuesday": 1,
-                            "Tuesay": 1, # Typo in message "Collections take place on TUESAYS" (instead fo TUESDAYS).
-                            "Wednesday": 2,
-                            "Thursday": 3,
-                            "Friday": 4,
-                            "Saturday": 5,
-                            "Sunday": 6
-                        }
+                            #print(schedule_message)
 
-                        # Searching for the weekday in the sentence
-                        collection_day = None
-                        for day in weekdays:
-                            if re.search(day, schedule_message, re.IGNORECASE):
-                                collection_day = weekdays[day]
-                                break  # Stop searching if the day is found
+                            # Dictionary of weekdays in English with their corresponding numbers
+                            weekdays = {
+                                "Monday": 0,
+                                "Tuesday": 1,
+                                "Tuesay": 1, # Typo in message "Collections take place on TUESAYS" (instead fo TUESDAYS).
+                                "Wednesday": 2,
+                                "Thursday": 3,
+                                "Friday": 4,
+                                "Saturday": 5,
+                                "Sunday": 6
+                            }
 
-                        months = {
-                            "January": 1,
-                            "Febuary": 2,
-                            "March": 3,
-                            "April": 4,
-                            "May": 5,
-                            "June": 6,
-                            "July": 7,
-                            "August": 8,
-                            "September": 9,
-                            "October": 10,
-                            "November": 11,
-                            "December": 12,
-                        }
+                            # Searching for the weekday in the sentence
+                            collection_day = None
+                            for day in weekdays:
+                                if re.search(day, schedule_message, re.IGNORECASE):
+                                    collection_day = weekdays[day]
+                                    break  # Stop searching if the day is found
+
+                            months = {
+                                "January": 1,
+                                "February": 2,
+                                "March": 3,
+                                "April": 4,
+                                "May": 5,
+                                "June": 6,
+                                "July": 7,
+                                "August": 8,
+                                "September": 9,
+                                "October": 10,
+                                "November": 11,
+                                "December": 12,
+                            }
 
 
 
-                        split_green_schedule_message = schedule_message.split('-')
+                            split_green_schedule_message = schedule_message.split('-')
 
-                        for month in months:
-                            for line in split_green_schedule_message:
-                                line = line.split('\n')[0]
-                                if re.search(month, line):
+                            for month in months:
+                                for line in split_green_schedule_message:
+                                    line = line.split('\n')[0]
+                                    line = line.split('.')[0]
+                                    line = line.replace('*', '')
 
-                                    if re.search("weekly", line):
-                                        for day in range(1, 32):
-                                            try:
+                                    if re.search(month, line):
+
+                                        if re.search("weekly", line):
+                                            for day in range(1, 32):
+                                                try:
+                                                    date = datetime(2024, months[month], day)
+                                                    if date.weekday() == collection_day:  # Tuesday has index 1
+                                                        days.append(date.date())
+                                                except ValueError:
+                                                    pass  # Skip if the day is out of range for the month
+
+                                        else:
+                                            # Splitting the string by ',' and 'and' to extract individual numbers
+                                            line = line.replace(';', '')
+
+                                            line = line.replace('.', '')
+
+                                            # Constructing the regex pattern using the variable
+                                            month_to_remove = r'\b{}\b'.format(re.escape(month))
+                                            line = re.sub(month_to_remove, "", line)
+
+                                            line = re.split(r', | and ', line)
+
+                                            line = [l.lstrip().split(' ')[0] for l in line]
+
+                                            # Converting the extracted strings to integers
+                                            days_numbers = [int(num) for num in line]
+
+                                            for day in days_numbers:
                                                 date = datetime(2024, months[month], day)
-                                                if date.weekday() == collection_day:  # Tuesday has index 1
-                                                    days.append(date.date())
-                                            except ValueError:
-                                                pass  # Skip if the day is out of range for the month
+                                                days.append(date.date())
+                                        break
 
-                                    else:
-                                        # Splitting the string by ',' and 'and' to extract individual numbers
-                                        line = line.replace(';', '')
-                                        line = line.replace('.', '')
+                            for d in days:
+                                entries.append(
+                                    Collection(
+                                        date = d,
+                                        t = source["type"],
+                                        icon = ICON_MAP.get(source["type"]),
+                                    )
+                                )
 
-                                        # Constructing the regex pattern using the variable
-                                        month_to_remove = r'\b{}\b'.format(re.escape(month))
-                                        line = re.sub(month_to_remove, "", line)
+                        # RECYCLING OR FOOD
+                        elif source["type"] == "Recycling" or  source["type"] == "Food":
 
-                                        line = re.split(r', | and ', line)
-                                        # Converting the extracted strings to integers
-                                        days_numbers = [int(num) for num in line]
+                            # Dictionary of weekdays in English with their corresponding numbers
+                            weekdays = {
+                                "Monday": 0,
+                                "Tuesday": 1,
+                                "Tuesay": 1, # Typo in message "Collections take place on TUESAYS" (instead fo TUESDAYS).
+                                "Wednesday": 2,
+                                "Thursday": 3,
+                                "Friday": 4,
+                                "Saturday": 5,
+                                "Sunday": 6
+                            }
 
-                                        for day in days_numbers:
-                                            date = datetime(2024, months[month], day)
+
+                            # Searching for the weekday in the sentence
+                            collection_day = None
+                            for day in weekdays:
+                                if re.search(day, schedule_message, re.IGNORECASE):
+                                    collection_day = weekdays[day]
+                                    break  # Stop searching if the day is found
+
+                            # Iterate through each month and day, and handle the "out of range" error
+                            for month in range(1, 13):
+                                for day in range(1, 32):
+                                    try:
+                                        date = datetime(2024, month, day)
+                                        if date.weekday() == collection_day:  # Tuesday has index 1
                                             days.append(date.date())
+                                    except ValueError:
+                                        pass  # Skip if the day is out of range for the month
 
-
-                        for d in days:
-                            entries.append(
-                                Collection(
-                                    date = d,
-                                    t = source["type"],
-                                    icon = ICON_MAP.get(source["type"]),
+                            for d in days:
+                                entries.append(
+                                    Collection(
+                                        date = d,
+                                        t = source["type"],
+                                        icon = ICON_MAP.get(source["type"]),
+                                    )
                                 )
-                            )
-
-                    # RECYCLING OR FOOD
-                    elif source["type"] == "Recycling" or  source["type"] == "Food":
-
-                        # Dictionary of weekdays in English with their corresponding numbers
-                        weekdays = {
-                            "Monday": 0,
-                            "Tuesday": 1,
-                            "Tuesay": 1, # Typo in message "Collections take place on TUESAYS" (instead fo TUESDAYS).
-                            "Wednesday": 2,
-                            "Thursday": 3,
-                            "Friday": 4,
-                            "Saturday": 5,
-                            "Sunday": 6
-                        }
 
 
-                        # Searching for the weekday in the sentence
-                        collection_day = None
-                        for day in weekdays:
-                            if re.search(day, schedule_message, re.IGNORECASE):
-                                collection_day = weekdays[day]
-                                break  # Stop searching if the day is found
+                        else:
+                            #source["type"] == "Bulky" not implmented
+                            pass
 
-                        # Iterate through each month and day, and handle the "out of range" error
-                        for month in range(1, 13):
-                            for day in range(1, 32):
-                                try:
-                                    date = datetime(year, month, day)
-                                    if date.weekday() == collection_day:  # Tuesday has index 1
-                                        days.append(date.date())
-                                except ValueError:
-                                    pass  # Skip if the day is out of range for the month
-
-                        for d in days:
-                            entries.append(
-                                Collection(
-                                    date = d,
-                                    t = source["type"],
-                                    icon = ICON_MAP.get(source["type"]),
-                                )
-                            )
-
-
-                    else:
-                        print(schedule_message)
-
-
+            except:
+                pass
 
 
 
