5f2e7b667159e4e3bb62aa8c9ed023fcca7031b2
5ila5 <5ila5@users.noreply.github.com>
2024-01-03 12:20:11 +0100

    basildon_gov_uk allow uprn or address + reformating

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/basildon_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/basildon_gov_uk.py
@@ -12,9 +12,11 @@ DESCRIPTION = "Source for basildon.gov.uk services for Basildon Council, UK."
 URL = "https://basildon.gov.uk"
 
 TEST_CASES = {
-    "Test_001": {"uprn": "100090277795"},
-    "Test_002": {"uprn": "10024197625"},
-    "Test_003": {"uprn": "10090455610"}
+    "Test_Addres_001": {"postcode": "CM111BJ", "address": "6, HEADLEY ROAD"},
+    "Test_Addres_002": {"postcode": "SS14 1QU", "address": "25 LONG RIDING"},
+    "Test_UPRN_001": {"uprn": "100090277795"},
+    "Test_UPRN_002": {"uprn": 10024197625},
+    "Test_UPRN_003": {"uprn": "10090455610"},
 }
 ICON_MAP = {
     "green_waste": "mdi:leaf",
@@ -22,7 +24,7 @@ ICON_MAP = {
     "food_waste": "mdi:food",
     "glass_waste": "mdi:bottle-wine",
     "papercard_waste": "mdi:package-varient",
-    "plasticcans_waste": "mdi:bottle-soda-classic"
+    "plasticcans_waste": "mdi:bottle-soda-classic",
 }
 NAME_MAP = {
     "green_waste": "Garden",
@@ -30,7 +32,7 @@ NAME_MAP = {
     "food_waste": "Food",
     "glass_waste": "Glass",
     "papercard_waste": "Paper/Cardboard",
-    "plasticcans_waste": "Plastic/Cans"
+    "plasticcans_waste": "Plastic/Cans",
 }
 HEADERS = {
     "Accept": "*/*",
@@ -48,11 +50,41 @@ HEADERS = {
 
 
 class Source:
-    def __init__(self, uprn):
-        self._uprn = str(uprn).zfill(12)
+    def __init__(self, postcode=None, address=None, uprn=None):
+        if uprn is None and (postcode is None or address is None):
+            raise ValueError("Either uprn or postcode and address must be provided")
+
+        self._uprn = str(uprn).zfill(12) if uprn is not None else None
+        self._postcode = postcode
+        self._address = address
+
+    def compare_address(self, address) -> bool:
+        return (
+            self._address.replace(",", "").replace(" ", "").upper()
+            == address.replace(",", "").replace(" ", "").upper()
+        )
+
+    def get_uprn(self, s):
+        r = s.post(
+            "https://basildonportal.azurewebsites.net/api/listPropertiesByPostcode",
+            headers=HEADERS,
+            json={"postcode": self._postcode},
+        )
+        r.raise_for_status()
+        data = r.json()
+        if data["result"] != "success":
+            raise ValueError("Invalid postcode")
+        for item in data["properties"]:
+            if self.compare_address(item["line1"]):
+                self._uprn = item["uprn"]
+                break
+        if self._uprn is None:
+            raise ValueError("Invalid address")
 
     def fetch(self):
         s = requests.Session()
+        if self._uprn is None:
+            self.get_uprn(s)
 
         # Retrieve the schedule
         payload = {"uprn": self._uprn}
@@ -61,10 +93,14 @@ class Source:
             headers=HEADERS,
             json=payload,
         )
-        data = response.json()['refuse']['available_services']
+        data = response.json()["refuse"]["available_services"]
         entries = []
         for item in ICON_MAP:
-            for collection_date_key in ["current_collection_", "next_collection_", "last_collection_"]:
+            for collection_date_key in [
+                "current_collection_",
+                "next_collection_",
+                "last_collection_",
+            ]:
                 if data[item][collection_date_key + "active"]:
                     date_string = data[item][collection_date_key + "date"]
                     entries.append(
--- doc/source/basildon_gov_uk.md
+++ doc/source/basildon_gov_uk.md
@@ -12,9 +12,27 @@ waste_collection_schedule:
         uprn: UNIQUE_PROPERTY_REFERENCE_NUMBER
 ```
 
+or
+
+```yaml
+waste_collection_schedule:
+    sources:
+    - name: basildon_gov_uk
+      args:
+        postcode: POSTCODE
+        address: ADDRESS
+```
+
 ### Configuration Variables
+
 **uprn**  
-*(string) (required)*
+*(string) (required if postcode and address is not provided)*
+
+**postcode**  
+*(string) (required if no uprn provided)*
+
+**address**  
+*(string) (required if no uprn provided)*
 
 ## Example
 
@@ -26,6 +44,21 @@ waste_collection_schedule:
         uprn: "100090277795"
 ```
 
-## How to find your `UPRN`
+```yaml
+waste_collection_schedule:
+    sources:
+    - name: basildon_gov_uk
+      args:
+        postcode: "SS14 1QU"
+        address: "25 LONG RIDING"
+```
+
+## How to get the source arguments
+
+### Using postcode and address
+
+Go to <https://mybasildon.powerappsportals.com/check/where_i_live/> and enter your postcode and address. Then use the postcode and address as arguments.
+
+### Using UPRN
 
 An easy way to find your Unique Property Reference Number (UPRN) is by going to <https://www.findmyaddress.co.uk/> and entering your address details.
