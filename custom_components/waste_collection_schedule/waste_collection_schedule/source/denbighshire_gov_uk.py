6299fc30de7bf30cee89a8d5aeac6798167bb811
5ila5 <5ila5@users.noreply.github.com>
2024-01-31 14:59:37 +0100

    reformatting + ./update_docu_links.py

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/denbighshire_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/denbighshire_gov_uk.py
@@ -1,5 +1,6 @@
-from datetime import datetime
 import json
+from datetime import datetime
+
 import requests
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
@@ -8,7 +9,7 @@ DESCRIPTION = "Source for Denbighshire County Council."
 URL = "https://www.denbighshire.gov.uk/"
 TEST_CASES = {
     "10003928409": {"uprn": "10003928409"},
-    "100100183412": {"uprn": "100100183412"},
+    "100100183412": {"uprn": 100100183412},
 }
 
 ICON_MAP = {
@@ -20,19 +21,22 @@ ICON_MAP = {
 
 API_URL = "https://www.denbighshire.gov.uk/api/Custom/RefuseApi/GetCollectionDates"
 
+
 class Source:
     def __init__(self, uprn: str | int):
         self._uprn: str | int = uprn
 
     def fetch(self):
-        h = { "referer": "https://www.denbighshire.gov.uk/en/bins-and-recycling/bin-collections.aspx" }
+        h = {
+            "referer": "https://www.denbighshire.gov.uk/en/bins-and-recycling/bin-collections.aspx"
+        }
         r = requests.get(API_URL, params={"uprn": self._uprn}, headers=h)
         r.raise_for_status()
-        
-        message = json.loads(r.json()['message'])
-        entries = []
 
-        for type in ['Household', 'Recycling', 'Food']:
+        message = json.loads(r.json()["message"])
+        entries = []
+        print(message)
+        for type in ["Household", "Recycling", "Food"]:
             date_str = message[f"{type}Date"]
             date = datetime.strptime(date_str, "%A %d/%m/%Y").date()
 
