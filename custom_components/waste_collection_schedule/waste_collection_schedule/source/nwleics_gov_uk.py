6da72c38764442e5059914e2a6468df233115f5c
5ila5 <5ila5@users.noreply.github.com>
2024-02-15 19:43:04 +0100

    reformatting + ./update_docu_links.py

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/nwleics_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/nwleics_gov_uk.py
@@ -1,30 +1,26 @@
-from datetime import datetime
-from dateutil import parser
-from waste_collection_schedule import Collection
 import requests
 from bs4 import BeautifulSoup
+from dateutil import parser
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
-
-TITLE = "North West Leicestershire District Council" # Title will show up in README.md and info.md
+TITLE = "North West Leicestershire District Council"  # Title will show up in README.md and info.md
 DESCRIPTION = "Source for www.nwleics.gov.uk services for the city of North West Leicestershire District Council, UK"  # Describe your source
-URL = "https://my.nwleics.gov.uk/"  # Insert url to service homepage. URL will show up in README.md and info.md
+URL = "https://nwleics.gov.uk/"  # Insert url to service homepage. URL will show up in README.md and info.md
 TEST_CASES = {  # Insert arguments for test cases to be used by test_sources.py script
     "Dunmore": {"uprn": "10002359002"},
     "Station Road": {"uprn": "100030573554"},
 }
 
-API_URL = "https://my.nwleics.gov.uk/"
+API_URL = "https://my.nwleics.gov.uk/location?put=nwl{uprn}&rememberme=0&redirect=%2F"
 
 
-BINS = {
-    "Refuse": {"icon": "mdi:trash-can", "name": "General"},
-    "Garden Waste": {"icon": "mdi:leaf", "name": "Garden"},
-    "Yellow Bag":  {"icon": "mdi:recycle", "name": "Recycling"},
-    "Blue Bag":  {"icon": "mdi:recycle", "name": "Recycling"},
-    "Red Box":  {"icon": "mdi:recycle", "name": "Recycling"},
-   
+ICON_MAP = {
+    "Refuse": "mdi:trash-can",
+    "Garden Waste": "mdi:leaf",
+    "Yellow Bag": "mdi:recycle",
+    "Blue Bag": "mdi:recycle",
+    "Red Box": "mdi:recycle",
 }
-
 
 
 class Source:
@@ -32,39 +28,29 @@ class Source:
         self._uprn = uprn
 
     def fetch(self):
-
-
-   
-
-        r = requests.get(f"{API_URL}/location?put=nwl{self._uprn}&rememberme=0&redirect=%2F")
+        r = requests.get(API_URL.format(uprn=self._uprn))
         r.raise_for_status()
 
         soup = BeautifulSoup(r.text, "html.parser")
 
         refuse = soup.find("ul", {"class": "refuse"})
-        li_items=refuse.find_all('li')
+        li_items = refuse.find_all("li")
 
         entries = []
 
         for li in li_items:
-            strong_tag = li.find('strong')
-            a_tag=li.find('a')
-            
+            strong_tag = li.find("strong")
+            a_tag = li.find("a")
 
-            collection_date=parser.parse(strong_tag.contents[0]).date()
-            type=a_tag.contents[0]
-
+            collection_date = parser.parse(strong_tag.contents[0]).date()
+            bin_type = a_tag.contents[0]
 
             entries.append(
                 Collection(
-                    date = collection_date,  # Collection date
-                    t = type,  # Collection type
-                    icon = BINS.get(type),  # Collection icon
-                    )
+                    date=collection_date,  # Collection date
+                    t=bin_type,  # Collection type
+                    icon=ICON_MAP.get(bin_type),  # Collection icon
                 )
-      
+            )
+
         return entries
-
-
-
-     
