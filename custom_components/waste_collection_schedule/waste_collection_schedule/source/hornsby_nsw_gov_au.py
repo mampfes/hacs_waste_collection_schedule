6fa01d974af4c8c4e1487a4f92be2452cbdebd26
Dan Wheaton <dan@DESKTOP-FDSRCUR.localdomain>
2024-03-08 12:52:29 +1100

    Change what and how data is shown. Add general waste to every date

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/hornsby_nsw_gov_au.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/hornsby_nsw_gov_au.py
@@ -12,14 +12,16 @@ TITLE = "Hornsby Shire Council"
 DESCRIPTION = "Source for Hornsby Shire Council."
 URL = "https://hornsby.nsw.gov.au/"
 
-API_URL = "http://140.238.202.117/api.php"
+API_URL = "https://hscapi.teststuffs.com/"
 API_TOKEN = "fGygCt3dzKol9zVuYrzltPPrBPMIhYeQhGWPpGx13hWp2DpJsHF1al1vy6yZMRYw"
 
 ICON_MAP = {
-    "Refuse Bin for Non-Recycling": "mdi:trash-can",
+    "Refuse": "mdi:trash-can",
     "Yellow": "mdi:recycle",
     "Green": "mdi:leaf",
 }
+
+TONIGHT = False
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -33,20 +35,26 @@ class Source:
             "token": API_TOKEN,
             "property_id": self.property_id
         }
-        response = s.post(API_URL, data=json.dumps(payload))#, headers=HEADERS)
+        response = s.post(API_URL, data=json.dumps(payload))
         data = json.loads(response.text)
 
         entries = []
         bulk_waste = data['bulk_dates']
         for date in bulk_waste:
             real_date = datetime.datetime.strptime(date, "%d/%m/%Y").date()
-            entries.append(Collection(date=real_date, t="Bulk Waste", icon="mdi:delete"))
+            entries.append(Collection(date=real_date, t="Bulky Waste", icon="mdi:delete"))
         collection_day = data['bins']['day']
         green_collection_date = datetime.datetime.strptime(data['bins']['next_green'], "%d/%m/%Y").date()
         yellow_collection_date = datetime.datetime.strptime(data['bins']['next_yellow'], "%d/%m/%Y").date()
         bin_this_week = data['bins']['this_week']
-        entries.append(Collection(date=green_collection_date, t="Green Today", icon=ICON_MAP.get("Green")))
-        entries.append(Collection(date=yellow_collection_date, t="Yellow Today", icon=ICON_MAP.get("Yellow")))
-        entries.append(Collection(date=green_collection_date-datetime.timedelta(days=1), t="Green Tonight", icon=ICON_MAP.get("Green")))
-        entries.append(Collection(date=yellow_collection_date-datetime.timedelta(days=1), t="Yellow Tonight", icon=ICON_MAP.get("Yellow")))
+        entries.append(Collection(date=green_collection_date, t="Green Waste", icon=ICON_MAP.get("Green")))
+        entries.append(Collection(date=yellow_collection_date, t="Recycling", icon=ICON_MAP.get("Yellow")))
+        entries.append(Collection(date=yellow_collection_date, t="General Waste", icon=ICON_MAP.get("Refuse")))
+        entries.append(Collection(date=green_collection_date, t="General Waste", icon=ICON_MAP.get("Refuse")))
+        if TONIGHT:
+            entries.append(Collection(date=green_collection_date-datetime.timedelta(days=1), t="Green Waste Tonight", icon=ICON_MAP.get("Green")))
+            entries.append(Collection(date=yellow_collection_date-datetime.timedelta(days=1), t="Recycling Tonight", icon=ICON_MAP.get("Yellow")))
+            entries.append(Collection(date=yellow_collection_date-datetime.timedelta(days=1), t="General Waste Tonight", icon=ICON_MAP.get("Refuse")))
+            entries.append(Collection(date=green_collection_date-datetime.timedelta(days=1), t="General Waste Tonight", icon=ICON_MAP.get("Refuse")))
+
         return entries
