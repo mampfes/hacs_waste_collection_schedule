e510efcdbf4c4b5f41ef5a7b84d5732ceff07503
elijusgust <elijus.gustys@gmail.com>
2024-03-17 15:00:33 +0200

    add source tkeliai_lt

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/tkeliai_lt.py
@@ -0,0 +1,55 @@
+import datetime
+from waste_collection_schedule import Collection
+import json
+import requests
+
+TITLE = "Telšių keliai"
+DESCRIPTION = "Source script for UAB 'Telšių keliai'"
+URL = "https://tkeliai.lt/"
+TEST_CASES = {
+    "TestName1": {"location": "Butkų Juzės(1-31) g."},
+    "TestName2": {"location": "Paragai k."},
+    "TestName3": {"location": "Žalioji g."}
+}
+
+API_URL = "https://tkeliai.lt/ajax.php"
+ICON_MAP = {
+    "Mišrios komunalinės atliekos": "mdi:trash-can",
+    "Kitos pakuočių atliekos": "mdi:recycle",
+    "Stiklo pakuočių atliekos": "mdi:glass-fragile",
+}
+
+
+class Source:
+    def __init__(self, location):
+        self.location = location
+
+    def fetch(self):
+        data = {
+            "action": "getDataAll",
+            "module": "Atliekos",
+            "lang": "lt",
+            "location": self.location,
+        }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': ''
+        }
+        
+        response = requests.request("POST", API_URL, headers=headers, data=data)
+
+        data = json.loads(response.text)
+
+        entries = []
+
+        for collection in data["return"]:
+            for date in collection["dates"].keys():
+                entries.append(
+                    Collection(
+                        date = datetime.datetime.strptime(date, '%Y-%m-%d').date(),
+                        t = collection["title"],
+                        icon = ICON_MAP.get(collection["title"]),
+                    )
+                )
+
+        return entries
