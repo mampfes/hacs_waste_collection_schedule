61a07212bf1aacadaab5fe36eb3c9c0e3e7169b4
jeromehettich <136083747+jeromehettich@users.noreply.github.com>
2024-04-02 20:13:13 +0100

    add a source script

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/bcp_gov_uk.py
@@ -0,0 +1,51 @@
+import datetime
+import json
+
+import requests
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
+
+TITLE = "BCP Council"
+DESCRIPTION = "Source for Bournemouth, Chirstchurch and Poole  Council, UK."
+URL = "https://www.bcpcouncil.gov.uk/"
+TEST_CASES = {
+    "Test_001": {"uprn": 10013448804},
+    "Test_002": {"uprn": "100040783978"}
+}
+ICON_MAP = {
+    "Recycling": "mdi:recycle",
+    "Rubbish": "mdi:trash-can",
+    "Garden Waste": "mdi:leaf",
+    "Food Waste": "mdi:food",
+}
+API_URL = "https://online.bcpcouncil.gov.uk/bcp-apis?api=BinDayLookup&uprn={uprn}"
+
+class Source:
+    def __init__(self, uprn: str | int):
+        self._uprn: str = str(uprn)
+
+    def fetch(self):
+        r = requests.get(API_URL.format(uprn=self._uprn))
+        json_data = json.loads(r.content)
+
+        entries = []
+
+        for bin in json_data:
+            bin_type = bin["BinType"]
+            next_date = datetime.datetime.strptime(bin["Next"], "%m/%d/%Y %H:%M:%S %p").date()
+            subsequent_date = datetime.datetime.strptime(bin["Subsequent"], "%m/%d/%Y %H:%M:%S %p").date()
+            entries.append(
+                Collection(
+                    date=next_date,
+                    t=bin_type,
+                    icon=ICON_MAP.get(bin_type),
+                )
+            )
+            entries.append(
+                Collection(
+                    date=subsequent_date,
+                    t=bin_type,
+                    icon=ICON_MAP.get(bin_type),
+                )
+            )
+
+        return entries
