280a80ad27885ee65f2ac550ac85ff11bec41c9b
5ila5 <5ila5@users.noreply.github.com>
2024-04-03 13:24:59 +0200

    reformatting + refactor (remove duplicate code)

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/bcp_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/bcp_gov_uk.py
@@ -1,5 +1,5 @@
-from datetime import datetime, timedelta
 import json
+from datetime import datetime, timedelta
 
 import requests
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
@@ -10,7 +10,7 @@ URL = "https://www.bcpcouncil.gov.uk/"
 TEST_CASES = {
     "Test_001": {"uprn": 10013449141},
     "Test_002": {"uprn": "10001085438"},
-    "Test_003": {"uprn": "100040567667"}
+    "Test_003": {"uprn": "100040567667"},
 }
 ICON_MAP = {
     "Recycling": "mdi:recycle",
@@ -19,6 +19,7 @@ ICON_MAP = {
     "Food Waste": "mdi:food",
 }
 API_URL = "https://online.bcpcouncil.gov.uk/bcp-apis?api=BinDayLookup&uprn={uprn}"
+
 
 class Source:
     def __init__(self, uprn: str | int):
@@ -32,21 +33,17 @@ class Source:
 
         for bin in json_data:
             bin_type = bin["BinType"]
-            next_date = (datetime.strptime(bin["Next"], "%m/%d/%Y %I:%M:%S %p") + timedelta(hours=1)).date()
-            subsequent_date = (datetime.strptime(bin["Subsequent"], "%m/%d/%Y %I:%M:%S %p") + timedelta(hours=1)).date()
-            entries.append(
-                Collection(
-                    date=next_date,
-                    t=bin_type,
-                    icon=ICON_MAP.get(bin_type),
+            for date_str in [bin["Next"], bin["Subsequent"]]:
+                date = (
+                    datetime.strptime(date_str, "%m/%d/%Y %I:%M:%S %p")
+                    + timedelta(hours=1)
+                ).date()
+                entries.append(
+                    Collection(
+                        date=date,
+                        t=bin_type,
+                        icon=ICON_MAP.get(bin_type),
+                    )
                 )
-            )
-            entries.append(
-                Collection(
-                    date=subsequent_date,
-                    t=bin_type,
-                    icon=ICON_MAP.get(bin_type),
-                )
-            )
 
         return entries
