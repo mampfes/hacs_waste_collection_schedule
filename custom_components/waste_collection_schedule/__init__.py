101a2f64c7c13ff3cdef551c8d6509dbc0e94449
Daniel Raper <me@danr.uk>
2024-04-26 17:12:19 +0100

    Fix yaml config regression

--- custom_components/waste_collection_schedule/__init__.py
+++ custom_components/waste_collection_schedule/__init__.py
@@ -1,3 +1,3 @@
 """Waste Collection Schedule Component."""
-from .init_yaml import async_setup
+from .init_yaml import async_setup, CONFIG_SCHEMA
 from .init_ui import async_setup_entry, async_update_listener, async_unload_entry, async_migrate_entry
