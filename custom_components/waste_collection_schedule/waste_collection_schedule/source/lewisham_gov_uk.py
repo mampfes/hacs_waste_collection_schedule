d38ae8be904cc9c6fe77722fa566b7d4ce3ef42c
Danny Allen <me@dannya.com>
2024-05-07 18:10:38 +0100

    General cleanup of `lewisham_gov_uk` source

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/lewisham_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/lewisham_gov_uk.py
@@ -1,5 +1,4 @@
 import datetime
-import logging
 import re
 
 import requests
@@ -21,31 +20,30 @@ API_URLS = {
     "collection": "https://lewisham.gov.uk/api/roundsinformation",
 }
 
-URPN_DATA_ITEM = '{79b58e9a-0997-4f18-bb97-637fac570dd1}'
+UPRN_DATA_ITEM = '{79b58e9a-0997-4f18-bb97-637fac570dd1}'
 
 REGEX = "<strong>(?P<type>Food and garden waste|Recycling|Refuse).*?</strong>.*?>(?P<frequency>.*?)<.*?\\\\t(?P<weekday>[A-Za-z]*day).*?(?:<br><br>|[A-Za-z\\\\]*?(?P<next>\d{2}/\d{2}/\d{4}))"
-DAYS =  ["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY","SUNDAY"]
+DAYS = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"]
 
 BINS = {
     "Refuse": {
         "icon": "mdi:trash-can",
         "alias": "Black Refuse"
-        },
+    },
     "Recycling": {
         "icon": "mdi:recycle",
         "alias": "Green Recycling"
-        },
+    },
     "Food": {
         "icon": "mdi:food-apple",
         "alias": "Grey Food"
-        },
+    },
     "Garden": {
         "icon": "mdi:leaf",
         "alias": "Brown Garden"
-        }
+    }
 }
 
-#_LOGGER = logging.getLogger(__name__)
 
 class Source:
     def __init__(self, post_code=None, number=None, name=None, uprn=None):
@@ -55,9 +53,8 @@ class Source:
         self._uprn = uprn
 
     def fetch(self):
-        now = datetime.date.today()
         if not self._uprn:
-            
+
             # look up the UPRN for the address
             p = {'postcodeOrStreet': self._post_code}
             r = requests.post(API_URLS["address_search"], params=p)
@@ -77,7 +74,7 @@ class Source:
                 raise Exception(f"Could not find address {self._post_code} {self._number}{self._name}")
 
         p = {
-            'item': URPN_DATA_ITEM,
+            'item': UPRN_DATA_ITEM,
             'uprn': self._uprn
             }
         r = requests.post(API_URLS["collection"], params=p)
@@ -91,18 +88,18 @@ class Source:
         for collection in collections:
 
             if collection[0].__contains__(' and '):
-                collections.append([collection[0].split(" and ",2)[0].title().replace(" Waste",""), collection[1], collection[2], collection[3]])
-                collections.append([collection[0].split(" and ",2)[1].title().replace(" Waste",""), collection[1], collection[2], collection[3]])
+                collections.append([collection[0].split(" and ", 2)[0].title().replace(" Waste", ""), collection[1], collection[2], collection[3]])
+                collections.append([collection[0].split(" and ", 2)[1].title().replace(" Waste", ""), collection[1], collection[2], collection[3]])
             else:
                 if collection[3] != "":
-                    nextDate = datetime.datetime.strptime(collection[3], "%d/%m/%Y").date()
+                    next_date = datetime.datetime.strptime(collection[3], "%d/%m/%Y").date()
                 elif collection[1] == "WEEKLY":
                     d = datetime.date.today()
-                    nextDate = d + datetime.timedelta((DAYS.index(collection[2].upper())+1 - d.isoweekday()) % 7)
+                    next_date = d + datetime.timedelta((DAYS.index(collection[2].upper())+1 - d.isoweekday()) % 7)
 
                 entries.append(
                     Collection(
-                        date=nextDate,
+                        date=next_date,
                         t=BINS.get(collection[0])['alias'],
                         icon=BINS.get(collection[0])['icon']
                     )
