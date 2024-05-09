fd39b5cbfc7aad9b29e0234ba5a62e45e4fd3e48
5ila5 <5ila5@users.noreply.github.com>
2024-05-09 13:47:43 +0200

    reformatting + convert one test case to string

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/north_ayrshire_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/north_ayrshire_gov_uk.py
@@ -1,6 +1,3 @@
-import json
-from urllib.parse import parse_qs, urlparse
-
 import requests
 from dateutil import parser
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
@@ -11,7 +8,7 @@ URL = "https://www.north-ayrshire.gov.uk/"
 API_URL = "https://www.maps.north-ayrshire.gov.uk/arcgis/rest/services/AGOL/YourLocationLive/MapServer/8/query?f=json&outFields=*&returnDistinctValues=true&returnGeometry=false&spatialRel=esriSpatialRelIntersects&where=UPRN%20%3D%20%27{0}%27"
 
 TEST_CASES = {
-    "Test_001": {"uprn": 126043248},
+    "Test_001": {"uprn": "126043248"},
     "Test_002": {"uprn": 126021147},
     "Test_003": {"uprn": 126091148},
 }
@@ -33,16 +30,48 @@ class Source:
 
     def __get_bin_collection_info_json(self, uprn):
         r = requests.get(API_URL.format(uprn))
-        bin_json = r.json()['features']
+        bin_json = r.json()["features"]
         bin_list = []
-        if 'BLUE_DATE_TEXT' in bin_json[0]['attributes']:
-            bin_list.append(["Blue","/".join(reversed(bin_json[0]['attributes']['BLUE_DATE_TEXT'].split("/")))])
-        if 'GREY_DATE_TEXT' in bin_json[0]['attributes']:
-            bin_list.append(["Grey","/".join(reversed(bin_json[0]['attributes']['GREY_DATE_TEXT'].split("/")))])
-        if 'PURPLE_DATE_TEXT' in bin_json[0]['attributes']:
-            bin_list.append(["Purple","/".join(reversed(bin_json[0]['attributes']['PURPLE_DATE_TEXT'].split("/")))])
-        if 'BROWN_DATE_TEXT' in bin_json[0]['attributes']:
-            bin_list.append(["Brown","/".join(reversed(bin_json[0]['attributes']['BROWN_DATE_TEXT'].split("/")))])
+        if "BLUE_DATE_TEXT" in bin_json[0]["attributes"]:
+            bin_list.append(
+                [
+                    "Blue",
+                    "/".join(
+                        reversed(bin_json[0]["attributes"]["BLUE_DATE_TEXT"].split("/"))
+                    ),
+                ]
+            )
+        if "GREY_DATE_TEXT" in bin_json[0]["attributes"]:
+            bin_list.append(
+                [
+                    "Grey",
+                    "/".join(
+                        reversed(bin_json[0]["attributes"]["GREY_DATE_TEXT"].split("/"))
+                    ),
+                ]
+            )
+        if "PURPLE_DATE_TEXT" in bin_json[0]["attributes"]:
+            bin_list.append(
+                [
+                    "Purple",
+                    "/".join(
+                        reversed(
+                            bin_json[0]["attributes"]["PURPLE_DATE_TEXT"].split("/")
+                        )
+                    ),
+                ]
+            )
+        if "BROWN_DATE_TEXT" in bin_json[0]["attributes"]:
+            bin_list.append(
+                [
+                    "Brown",
+                    "/".join(
+                        reversed(
+                            bin_json[0]["attributes"]["BROWN_DATE_TEXT"].split("/")
+                        )
+                    ),
+                ]
+            )
 
         entries = []
         for bins in bin_list:
