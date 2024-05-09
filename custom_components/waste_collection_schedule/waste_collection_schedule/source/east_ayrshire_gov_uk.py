db0235461012884f86eb08f49c1da1e84359b34f
5ila5 <5ila5@users.noreply.github.com>
2024-05-09 13:41:59 +0200

    reformatting + convert one test case to string

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/east_ayrshire_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/east_ayrshire_gov_uk.py
@@ -1,5 +1,3 @@
-from urllib.parse import parse_qs, urlparse
-
 import requests
 from bs4 import BeautifulSoup
 from dateutil import parser
@@ -11,7 +9,7 @@ URL = "https://www.east-ayrshire.gov.uk/"
 API_URL = "https://www.east-ayrshire.gov.uk/Housing/RubbishAndRecycling/Collection-days/ViewYourRecyclingCalendar.aspx?r="
 
 TEST_CASES = {
-    "Test_001": {"uprn": 127071649},
+    "Test_001": {"uprn": "127071649"},
     "Test_002": {"uprn": 127072649},
     "Test_003": {"uprn": 127072016},
 }
@@ -40,9 +38,11 @@ class Source:
         for bins in bin_list:
             entries.append(
                 Collection(
-                    date=parser.parse(bins['datetime']).date(),
-                    t=bins.select_one('span.ScheduleItem').get_text().strip(),
-                    icon=ICON_MAP.get(bins.select_one('span.ScheduleItem').get_text().strip()),
+                    date=parser.parse(bins["datetime"]).date(),
+                    t=bins.select_one("span.ScheduleItem").get_text().strip(),
+                    icon=ICON_MAP.get(
+                        bins.select_one("span.ScheduleItem").get_text().strip()
+                    ),
                 )
             )
         return entries
