87919b63b1d30036338f72eecdce44803eafae15
5ila5 <5ila5@users.noreply.github.com>
2024-05-20 17:51:04 +0200

    automatically add args to translation

--- update_docu_links.py
+++ update_docu_links.py
@@ -2,10 +2,12 @@
 
 import argparse
 import importlib
+import inspect
+import json
 import re
 import site
 from pathlib import Path
-import json
+
 import yaml
 
 SECRET_FILENAME = "secrets.yaml"
@@ -38,6 +40,11 @@ class Section:
         return f"<!--End of {self._section} section-->"
 
 
+def split_camel_and_snake_case(s):
+    s = re.sub("([a-z0-9])([A-Z])", r"\1 \2", s)  # Split CamelCase
+    return s.replace("_", " ").split()  # Split snake_case
+
+
 def main():
     parser = argparse.ArgumentParser(description="Update docu links.")
     # args = parser.parse_args()
@@ -67,7 +74,7 @@ def main():
         print("Orphaned sources without country =========================")
         for o in orphans:
             print(o)
-    
+
     update_json(countries)
     update_readme_md(countries)
     update_info_md(countries)
@@ -103,10 +110,20 @@ def browse_sources():
         url = module.URL
         country = getattr(module, "COUNTRY", f.split("_")[-1])
 
+        sig = inspect.signature(module.Source.__init__)
+        params = [param.name for param in sig.parameters.values()]
+
         filename = f"/doc/source/{f}.md"
         if title is not None:
             sources.append(
-                SourceInfo(filename=filename, module=f, title=title, url=url, country=country)
+                SourceInfo(
+                    filename=filename,
+                    module=f,
+                    title=title,
+                    url=url,
+                    country=country,
+                    params=params,
+                )
             )
 
         extra_info = getattr(module, "EXTRA_INFO", [])
@@ -120,6 +137,7 @@ def browse_sources():
                     title=e.get("title", title),
                     url=e.get("url", url),
                     country=e.get("country", country),
+                    params=params,
                 )
             )
 
@@ -155,6 +173,7 @@ def browse_ics_yaml():
                     title=data["title"],
                     url=data["url"],
                     country=data.get("country", f.stem.split("_")[-1]),
+                    params=[],
                 )
             )
             if "extra_info" in data:
@@ -166,6 +185,7 @@ def browse_ics_yaml():
                             title=e.get("title"),
                             url=e.get("url"),
                             country=e.get("country"),
+                            params=[],
                         )
                     )
 
@@ -246,7 +266,9 @@ def beautify_url(url):
     url = url.removeprefix("www.")
     return url
 
+
 def update_json(countries):
+    params = set()
     # generate country list
     output = {}
     for country in sorted(countries):
@@ -259,16 +281,37 @@ def update_json(countries):
             if e.module is None:
                 continue
 
-            output[country].append({
-                "title": e.title,
-                "module": e.module
-            })
+            output[country].append({"title": e.title, "module": e.module})
+            params.update(e.params)
 
-    output["Generic"] = [
-        {"title": "ICS", "module": "ics"}
-    ]
+    output["Generic"] = [{"title": "ICS", "module": "ics"}]
 
-    with open("custom_components/waste_collection_schedule/sources.json", "w", encoding="utf-8") as f:
+    with open(
+        "custom_components/waste_collection_schedule/translations/en.json",
+        encoding="utf-8",
+    ) as f:
+        translations = json.load(f)
+
+    arg_translations = translations["config"]["step"]["args"]["data"]
+    for param in params:
+        if param in arg_translations:
+            continue
+        arg_translations[param] = " ".join(
+            [s.capitalize() for s in split_camel_and_snake_case(param)]
+        )
+
+    with open(
+        "custom_components/waste_collection_schedule/translations/en.json",
+        "w",
+        encoding="utf-8",
+    ) as f:
+        json.dump(translations, f, indent=2)
+
+    with open(
+        "custom_components/waste_collection_schedule/sources.json",
+        "w",
+        encoding="utf-8",
+    ) as f:
         f.write(json.dumps(output))
 
 
@@ -404,15 +447,16 @@ def _patch_file(filename, section_id, str):
 
 
 class SourceInfo:
-    def __init__(self, filename, module, title, url, country):
+    def __init__(self, filename, module, title, url, country, params):
         self._filename = filename
         self._module = module
         self._title = title
         self._url = url
         self._country = country
+        self._params = params
 
     def __repr__(self):
-        return f"filename:{self._filename}, title:{self._title}, url:{self._url}, country:{self._country}"
+        return f"filename:{self._filename}, title:{self._title}, url:{self._url}, country:{self._country}, params:{self._params}"
 
     @property
     def filename(self):
@@ -433,6 +477,10 @@ class SourceInfo:
     @property
     def country(self):
         return self._country
+
+    @property
+    def params(self):
+        return self._params
 
 
 def make_country_code_map():
