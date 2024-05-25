919218cc13975cd0de6c0e4da078d975685d33a4
5ila5 <5ila5@users.noreply.github.com>
2024-05-25 14:25:49 +0200

    reformatting init_ui.py and init_yaml.py

--- custom_components/waste_collection_schedule/init_yaml.py
+++ custom_components/waste_collection_schedule/init_yaml.py
@@ -10,102 +10,115 @@ import voluptuous as vol
 from homeassistant.core import HomeAssistant, ServiceCall, callback
 from homeassistant.helpers.discovery import async_load_platform
 from homeassistant.helpers.dispatcher import dispatcher_send
-from homeassistant.exceptions import ConfigEntryNotReady
+
 from homeassistant.helpers.event import async_call_later  # isort:skip
 from homeassistant.helpers.event import async_track_time_change  # isort:skip
 
-from .const import *
 
 # add module directory to path
 package_dir = Path(__file__).resolve().parents[0]
 site.addsitedir(str(package_dir))
+from . import const  # type: ignore # isort:skip # noqa: E402
 from waste_collection_schedule import Customize, SourceShell  # type: ignore # isort:skip # noqa: E402
 
 _LOGGER = logging.getLogger(__name__)
 
 CUSTOMIZE_CONFIG = vol.Schema(
     {
-        vol.Optional(CONF_TYPE): cv.string,
-        vol.Optional(CONF_ALIAS): cv.string,
-        vol.Optional(CONF_SHOW): cv.boolean,
-        vol.Optional(CONF_ICON): cv.icon,
-        vol.Optional(CONF_PICTURE): cv.string,
-        vol.Optional(CONF_USE_DEDICATED_CALENDAR): cv.boolean,
-        vol.Optional(CONF_DEDICATED_CALENDAR_TITLE): cv.string,
+        vol.Optional(const.CONF_TYPE): cv.string,
+        vol.Optional(const.CONF_ALIAS): cv.string,
+        vol.Optional(const.CONF_SHOW): cv.boolean,
+        vol.Optional(const.CONF_ICON): cv.icon,
+        vol.Optional(const.CONF_PICTURE): cv.string,
+        vol.Optional(const.CONF_USE_DEDICATED_CALENDAR): cv.boolean,
+        vol.Optional(const.CONF_DEDICATED_CALENDAR_TITLE): cv.string,
     }
 )
 
 SOURCE_CONFIG = vol.Schema(
     {
-        vol.Required(CONF_SOURCE_NAME): cv.string,
-        vol.Required(CONF_SOURCE_ARGS): dict,
-        vol.Optional(CONF_CUSTOMIZE, default=[]): vol.All(
+        vol.Required(const.CONF_SOURCE_NAME): cv.string,
+        vol.Required(const.CONF_SOURCE_ARGS): dict,
+        vol.Optional(const.CONF_CUSTOMIZE, default=[]): vol.All(
             cv.ensure_list, [CUSTOMIZE_CONFIG]
         ),
-        vol.Optional(CONF_SOURCE_CALENDAR_TITLE): cv.string,
+        vol.Optional(const.CONF_SOURCE_CALENDAR_TITLE): cv.string,
     }
 )
 
 CONFIG_SCHEMA = vol.Schema(
     {
-        DOMAIN: vol.Schema(
+        const.DOMAIN: vol.Schema(
             {
-                vol.Required(CONF_SOURCES): vol.All(cv.ensure_list, [SOURCE_CONFIG]),
-                vol.Optional(CONF_SEPARATOR, default=CONF_SEPARATOR_DEFAULT): cv.string,
-                vol.Optional(CONF_FETCH_TIME, default=CONF_FETCH_TIME_DEFAULT): cv.time,
+                vol.Required(const.CONF_SOURCES): vol.All(
+                    cv.ensure_list, [SOURCE_CONFIG]
+                ),
                 vol.Optional(
-                    CONF_RANDOM_FETCH_TIME_OFFSET, default=CONF_RANDOM_FETCH_TIME_OFFSET_DEFAULT
+                    const.CONF_SEPARATOR, default=const.CONF_SEPARATOR_DEFAULT
+                ): cv.string,
+                vol.Optional(
+                    const.CONF_FETCH_TIME, default=const.CONF_FETCH_TIME_DEFAULT
+                ): cv.time,
+                vol.Optional(
+                    const.CONF_RANDOM_FETCH_TIME_OFFSET,
+                    default=const.CONF_RANDOM_FETCH_TIME_OFFSET_DEFAULT,
                 ): cv.positive_int,
-                vol.Optional(CONF_DAY_SWITCH_TIME, default=CONF_DAY_SWITCH_TIME_DEFAULT): cv.time,
+                vol.Optional(
+                    const.CONF_DAY_SWITCH_TIME,
+                    default=const.CONF_DAY_SWITCH_TIME_DEFAULT,
+                ): cv.time,
             }
         )
     },
     extra=vol.ALLOW_EXTRA,
 )
 
+
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the component. config contains data from configuration.yaml."""
     # Skip for config flow
-    if DOMAIN not in config:
+    if const.DOMAIN not in config:
         return True
 
     # create empty api object as singleton
     api = WasteCollectionApi(
         hass,
-        separator=config[DOMAIN][CONF_SEPARATOR],
-        fetch_time=config[DOMAIN][CONF_FETCH_TIME],
-        random_fetch_time_offset=config[DOMAIN][CONF_RANDOM_FETCH_TIME_OFFSET],
-        day_switch_time=config[DOMAIN][CONF_DAY_SWITCH_TIME],
+        separator=config[const.DOMAIN][const.CONF_SEPARATOR],
+        fetch_time=config[const.DOMAIN][const.CONF_FETCH_TIME],
+        random_fetch_time_offset=config[const.DOMAIN][
+            const.CONF_RANDOM_FETCH_TIME_OFFSET
+        ],
+        day_switch_time=config[const.DOMAIN][const.CONF_DAY_SWITCH_TIME],
     )
 
     # create shells for source(s)
-    for source in config[DOMAIN][CONF_SOURCES]:
+    for source in config[const.DOMAIN][const.CONF_SOURCES]:
         # create customize object
         customize = {}
-        for c in source.get(CONF_CUSTOMIZE, {}):
-            customize[c[CONF_TYPE]] = Customize(
-                waste_type=c[CONF_TYPE],
-                alias=c.get(CONF_ALIAS),
-                show=c.get(CONF_SHOW, True),
-                icon=c.get(CONF_ICON),
-                picture=c.get(CONF_PICTURE),
-                use_dedicated_calendar=c.get(
-                    CONF_USE_DEDICATED_CALENDAR, False),
+        for c in source.get(const.CONF_CUSTOMIZE, {}):
+            customize[c[const.CONF_TYPE]] = Customize(
+                waste_type=c[const.CONF_TYPE],
+                alias=c.get(const.CONF_ALIAS),
+                show=c.get(const.CONF_SHOW, True),
+                icon=c.get(const.CONF_ICON),
+                picture=c.get(const.CONF_PICTURE),
+                use_dedicated_calendar=c.get(const.CONF_USE_DEDICATED_CALENDAR, False),
                 dedicated_calendar_title=c.get(
-                    CONF_DEDICATED_CALENDAR_TITLE, False),
+                    const.CONF_DEDICATED_CALENDAR_TITLE, False
+                ),
             )
         api.add_source_shell(
-            source_name=source[CONF_SOURCE_NAME],
+            source_name=source[const.CONF_SOURCE_NAME],
             customize=customize,
-            calendar_title=source.get(CONF_SOURCE_CALENDAR_TITLE),
-            source_args=source.get(CONF_SOURCE_ARGS, {}),
+            calendar_title=source.get(const.CONF_SOURCE_CALENDAR_TITLE),
+            source_args=source.get(const.CONF_SOURCE_ARGS, {}),
         )
 
     # store api object
-    hass.data.setdefault(DOMAIN, api)
+    hass.data.setdefault(const.DOMAIN, api)
 
     # load calendar platform
-    await async_load_platform(hass, "calendar", DOMAIN, {"api": api}, config)
+    await async_load_platform(hass, "calendar", const.DOMAIN, {"api": api}, config)
 
     # initial fetch of all data
     hass.add_job(api._fetch)
@@ -115,7 +128,7 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
     # Register new Service fetch_data
     hass.services.async_register(
-        DOMAIN, "fetch_data", async_fetch_data, schema=vol.Schema({})
+        const.DOMAIN, "fetch_data", async_fetch_data, schema=vol.Schema({})
     )
 
     return True
@@ -222,4 +235,4 @@ class WasteCollectionApi:
 
     @callback
     def _update_sensors_callback(self, *_):
-        dispatcher_send(self._hass, UPDATE_SENSORS_SIGNAL)
+        dispatcher_send(self._hass, const.UPDATE_SENSORS_SIGNAL)
