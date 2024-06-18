b829a8911abedeafda3436877402cc6ae82b8e9b
5ila5 <38183212+5ila5@users.noreply.github.com>
2024-06-18 19:00:52 +0200

    add_southkesteven_gov_uk (#2159)
    Co-authored-by: 5ila5 <5ila5@users.noreply.github.com>

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/southkesteven_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/southkesteven_gov_uk.py
@@ -1,5 +1,6 @@
+import logging
 import re
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import bs4
 import requests
@@ -7,10 +8,13 @@ from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 URL = "https://southkesteven.gov.uk"
 TEST_CASES = {
-    "Test_001": {"address_id": 70158},
+    "Long Bennington": {"address_id": 33399},
+    "Bourne": {"address_id": "7351"},
+    "Grantham": {"address_id": 18029},
 }
+_LOGGER = logging.getLogger(__name__)
 ICON_MAP = {
-    "Black": "mdi:trash-can",
+    "black": "mdi:trash-can",
     "gray": "mdi:recycle",
     "purple": "mdi:newspaper",
 }
@@ -38,25 +42,35 @@ class Source:
         r.raise_for_status()
 
         collections = []
-        print(r.text)
         soup = bs4.BeautifulSoup(r.content, "html.parser")
+
+        date_translate = {
+            "Tomorrow": datetime.now().date() + timedelta(days=1),
+            "Today": datetime.now().date(),
+        }
 
         # Find next collection
         # find p looks like                     <p>Your next bin collection date is <span class="alert__heading alpha">Wed 19 June 2024</span></p>
         next_collection_p = soup.find(
-            "p", text=re.compile("Your next bin collection date is")
+            lambda tag: tag.name == "p"
+            and "Your next bin collection date is" in tag.text
         )
         # above does not work so try this
 
-        if next_collection_p is not None:
+        if next_collection_p is None:
+            _LOGGER.warning(
+                "No next collection text found, continuing to look for future collections"
+            )
+        else:
             date_str = next_collection_p.find("span").text
-            date = datetime.strptime(date_str, "%A %d %B %Y").date()
+            date = (
+                date_translate.get(date_str)
+                or datetime.strptime(date_str, "%A %d %B %Y").date()
+            )
             bin_type = NEXT_BIN_TYPE_REGEX.search(
                 next_collection_p.find_next("p").text
             ).group(1)
             collections.append(Collection(date, bin_type, ICON_MAP.get(bin_type)))
-        else:
-            raise ValueError("No next collection found")
 
         # Find all Future collections
         s = soup.find_all("h3", text="Future collections")
@@ -70,13 +84,28 @@ class Source:
                 1
             )
 
-            print(collection_futher_info_link)
-
             for collection in collections_list.find_next_sibling("ul").find_all("li"):
                 # like: "Thu 29 August 2024"
                 date_str = collection.text
 
-                date = datetime.strptime(date_str, "%a %d %B %Y").date()
+                try:
+                    date = (
+                        date_translate.get(date_str)
+                        or datetime.strptime(date_str, "%a %d %B %Y").date()
+                    )
+                except ValueError:
+                    _LOGGER.warning(
+                        f"Failed to parse date {date_str}, skipping this collection"
+                    )
+                    continue
                 collections.append(Collection(date, bin_type, ICON_MAP.get(bin_type)))
 
-        return []
+        # filter out duplicate entries
+        collections = list(
+            {
+                (collection.date, collection.type): collection
+                for collection in collections
+            }.values()
+        )
+
+        return collections
