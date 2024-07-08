eb4f49a788dc11fa37841cd1c18fc0a65c084ddd
Zach Herberstein <Zach.herberstein@pernod-ricard.com>
2024-07-08 11:29:13 +1000

    removed print statement

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/hawkesbury_nsw_gov_au.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/hawkesbury_nsw_gov_au.py
@@ -96,7 +96,7 @@ class Source:
         r = requests.get(
             f"{self._url}/records/1.0/search/?sort=gisaddress&refine.gisaddress={self._houseNo} {address.title()} {self._suburb} NSW {self._postCode}&rows=1&dataset=bin-collection-days&timezone=Australia/Sydney&lang=en")
         data = json.loads(r.text)
-        print(r.url)
+        
         # Check if house record was found
         if len(data['records']) == 0:
             raise Exception(f"House not found: {self._houseNo}")
