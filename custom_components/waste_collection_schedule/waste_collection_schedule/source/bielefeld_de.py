69ffa4f1fc1356e74be9f0371e072c3d2019fb0a
5ila5 <5ila5@users.noreply.github.com>
2024-08-14 15:14:48 +0200

    fix bielefeld only getting results from next season

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/bielefeld_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/bielefeld_de.py
@@ -1,5 +1,7 @@
+import re
 from html.parser import HTMLParser
 
+import requests
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 from waste_collection_schedule.service.ICS import ICS
 from waste_collection_schedule.service.SSLError import get_legacy_session
@@ -26,21 +28,44 @@ ORIGINAL_SERVLET = (
     "https://anwendungen.bielefeld.de/WasteManagementBielefeld/WasteManagementServlet"
 )
 
+TEXT_REGEX = re.compile(r"var\s*text\s*=\s*'(.*?)'\s*;", re.DOTALL)
+
+
 # Parser for HTML input (hidden) text
 class HiddenInputParser(HTMLParser):
     def __init__(self):
         super().__init__()
         self._args = {}
+        self._radio_args: list[dict[str, str]] = []
 
     @property
     def args(self):
         return self._args
+
+    @property
+    def radio_args(self):
+        return self._radio_args
 
     def handle_starttag(self, tag, attrs):
         if tag == "input":
             d = dict(attrs)
             if str(d["type"]).lower() == "hidden":
                 self._args[d["name"]] = d["value"] if "value" in d else ""
+            if str(d["type"]).lower() == "radio":
+                if "value" in d and d["value"]:
+                    if not self._radio_args:
+                        self._radio_args.append({d["name"]: d["value"]})
+                    else:
+                        new_radio = self._radio_args.copy()
+                        for args in self._radio_args:
+                            if not d["name"] in args:
+                                args[d["name"]] = d["value"]
+                            else:
+                                args_copy = args.copy()
+                                args_copy = args_copy.copy()
+                                args_copy[d["name"]] = d["value"]
+                                new_radio.append(args_copy)
+                        self._radio_args = new_radio
 
 
 class Source:
@@ -48,7 +73,7 @@ class Source:
         self._street = street
         self._hnr = house_number
         self._suffix = address_suffix
-        self._ics = ICS()
+        self._ics = ICS(regex=r"(.*?) , (letze Leerung.*)?")
 
     def fetch(self):
         s = get_legacy_session()
@@ -57,22 +82,43 @@ class Source:
 
         r = s.get(
             servlet,
-            params={"SubmitAction": "wasteDisposalServices", "InFrameMode": "TRUE"},
+            params={"SubmitAction": "wasteDisposalServices"},
         )
         if r.status_code == 404:
             servlet = ORIGINAL_SERVLET
             r = s.get(
                 servlet,
-                params={"SubmitAction": "wasteDisposalServices", "InFrameMode": "TRUE"},
+                params={"SubmitAction": "wasteDisposalServices"},
             )
 
         r.raise_for_status()
         r.encoding = "utf-8"
 
         parser = HiddenInputParser()
-        parser.feed(r.text)
+        text = r.text
+        parser.feed(text)
 
-        args = parser.args
+        text_match = TEXT_REGEX.search(text)
+        if text_match:
+            text = text_match.group(1)
+            text = text.encode().decode("unicode-escape")
+            parser.feed(text)
+
+        if not parser.radio_args:
+            return self.fetch_with_args({}, parser, s, servlet=servlet)
+        entries = []
+        for args in parser.radio_args:
+            entries.extend(self.fetch_with_args(args, parser, s, servlet=servlet))
+        return entries
+
+    def fetch_with_args(
+        self,
+        radio_args: dict[str, str],
+        parser: HiddenInputParser,
+        s: requests.Session,
+        servlet: str,
+    ):
+        args = {**parser.args, **radio_args}
         args["Ort"] = self._street[0]
         args["Strasse"] = self._street
         args["Hausnummer"] = str(self._hnr)
