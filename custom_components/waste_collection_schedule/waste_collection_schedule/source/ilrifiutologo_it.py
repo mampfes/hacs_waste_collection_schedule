7c7f7c555bab3c80282344afefa4fb4bfdcc8d3f
5ila5 <5ila5@users.noreply.github.com>
2024-08-20 12:29:32 +0200

    reformatting

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/ilrifiutologo_it.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/ilrifiutologo_it.py
@@ -1,8 +1,8 @@
 import datetime
 import json
 import logging
-import requests
 
+import requests
 from waste_collection_schedule import Collection
 
 TITLE = "Il Rifiutologo"
@@ -22,7 +22,7 @@ HEADERS = {
     "Host": "www.ilrifiutologo.it",
     "Origin": "https://www.ilrifiutologo.it",
     "Referer": "http://www.ilrifiutologo.it/casa_rifiutologo/",
-    "X-Requested-With": "XMLHttpRequest"
+    "X-Requested-With": "XMLHttpRequest",
 }
 
 ICON_MAP = {
@@ -48,7 +48,12 @@ class Source:
         self.civico_id = None
 
         comuni = api_get_request(relative_path="/getComuni.php")
-        _LOGGER.debug("getComuni.php: %d s - %s - %s", comuni.elapsed.total_seconds(), comuni.status_code, comuni.reason)
+        _LOGGER.debug(
+            "getComuni.php: %d s - %s - %s",
+            comuni.elapsed.total_seconds(),
+            comuni.status_code,
+            comuni.reason,
+        )
 
         if comuni.status_code != 200 or not isinstance(comuni.json(), list):
             raise Exception("Errore durante il recupero dei comuni")
@@ -63,7 +68,12 @@ class Source:
         indirizzi = api_get_request(
             relative_path="/getIndirizzi.php", params={"idComune": self.comune_id}
         )
-        _LOGGER.debug("getIndirizzi.php: %d s - %s - %s", indirizzi.elapsed.total_seconds(), indirizzi.status_code, indirizzi.reason)
+        _LOGGER.debug(
+            "getIndirizzi.php: %d s - %s - %s",
+            indirizzi.elapsed.total_seconds(),
+            indirizzi.status_code,
+            indirizzi.reason,
+        )
 
         if indirizzi.status_code != 200 or not isinstance(indirizzi.json(), list):
             raise Exception("Errore durante il recupero degli indirizzi")
@@ -79,9 +89,16 @@ class Source:
             relative_path="/getNumeriCivici.php",
             params={"idComune": self.comune_id, "idIndirizzo": self.inirizzo_id},
         )
-        _LOGGER.debug("getNumeriCivici.php: %d s - %s - %s", numeri_civici.elapsed.total_seconds(), numeri_civici.status_code, numeri_civici.reason)
+        _LOGGER.debug(
+            "getNumeriCivici.php: %d s - %s - %s",
+            numeri_civici.elapsed.total_seconds(),
+            numeri_civici.status_code,
+            numeri_civici.reason,
+        )
 
-        if numeri_civici.status_code != 200 or not isinstance(numeri_civici.json(), list):
+        if numeri_civici.status_code != 200 or not isinstance(
+            numeri_civici.json(), list
+        ):
             raise Exception("Errore durante il recupero dei numeri civici")
 
         for civico in numeri_civici.json():
@@ -92,7 +109,6 @@ class Source:
             raise Exception("Civico non trovato")
 
     def fetch(self):
-
         r = api_get_request(
             relative_path="/getCalendarioPap.php",
             params={
@@ -104,7 +120,12 @@ class Source:
                 "giorniDaMostrare": 31,
             },
         )
-        _LOGGER.debug("getCalendarioPap.php: %d s - %s - %s", r.elapsed.total_seconds(), r.status_code, r.reason)
+        _LOGGER.debug(
+            "getCalendarioPap.php: %d s - %s - %s",
+            r.elapsed.total_seconds(),
+            r.status_code,
+            r.reason,
+        )
 
         if r.status_code != 200 or not isinstance(r.json(), dict):
             raise Exception("Errore durante il recupero del calendario")
@@ -137,7 +158,7 @@ def api_get_request(relative_path, params=None):
         data={
             "url": API_URL_BACKEND + relative_path,
             "type": "GET",
-            "parameters": json.dumps(params) if params else None
+            "parameters": json.dumps(params) if params else None,
         },
-        headers=HEADERS
+        headers=HEADERS,
     )
