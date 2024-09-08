3555f194c5096a3904acdd1a30a871722dd9ab74
Tim Schofield <tim@scoffer.net>
2024-09-08 11:38:22 +0100

    added source Wyre Borough Council, Lancashire UK

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/wyre_gov_uk.py
@@ -0,0 +1,51 @@
+import datetime
+from waste_collection_schedule import Collection
+import requests
+from dateutil import parser
+from bs4 import BeautifulSoup
+
+TITLE = "Wyre Borough Council" # Title will show up in README.md and info.md
+DESCRIPTION = "Source script for wyre.gov.uk"  # Describe your source
+URL = "https://www.wyre.gov.uk"  # Insert url to service homepage. URL will show up in README.md and info.md
+TEST_CASES = {
+    "Test_001": {"uprn": "10094000847"},
+}
+ICON_MAP = {
+    "Grey Bin": "mdi:trash-can",
+    "Blue Bin": "mdi:recycle",
+    "Red Bin": "mdi:recycle",
+    "Green bin": "mdi:leaf",
+}
+
+
+API_URL = "https://www.wyre.gov.uk/bincollections?uprn=10094000847&address=8+Dunlop+Drive%2C+Thornton+Cleveleys%2C+FY5+4GX&submit="
+
+
+class Source:
+    def __init__(self, uprn):
+        self._uprn = str(uprn).zfill(12)
+
+    def fetch(self):
+        response = requests.get(
+            "https://www.wyre.gov.uk/bincollections", params={"uprn": self._uprn}
+        )
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        entries = []
+
+        bins = soup.find_all("h3", class_="bin-collection-tasks__heading")
+        dates = soup.find_all("p", class_="bin-collection-tasks__date")
+
+        for i in range(len(dates)):
+            bint = " ".join(bins[i].text.split()[2:4])
+            date = parser.parse(dates[i].text).date()
+            entries.append(
+                Collection(
+                    date=date,
+                    t=bint,
+                    icon=ICON_MAP.get(bint),
+                )
+            )
+
+        return entries
+
