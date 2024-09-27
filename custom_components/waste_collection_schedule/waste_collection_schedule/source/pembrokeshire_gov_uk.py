af548a6d9ec0202079b2ddfd8ba26dd81d7cec9a
5ila5 <5ila5@users.noreply.github.com>
2024-09-27 23:49:17 +0200

    reformatting + ./update_docu_links.py

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/pembrokeshire_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/pembrokeshire_gov_uk.py
@@ -1,20 +1,20 @@
-import datetime
-from waste_collection_schedule import Collection
-import requests
-from datetime import datetime
 import ast
+from datetime import datetime
 
-TITLE = "Pembrokeshire County Council" # Title will show up in README.md and info.md
+import requests
+from waste_collection_schedule import Collection
+
+TITLE = "Pembrokeshire County Council"  # Title will show up in README.md and info.md
 DESCRIPTION = "Source script for pembrokeshire.gov.uk"  # Describe your source
 URL = "https://www.pembrokeshire.gov.uk/"  # Insert url to service homepage. URL will show up in README.md and info.md
 TEST_CASES = {  # Insert arguments for test cases to be used by test_sources.py script
     "Dew Street": {"uprn": "100100283349"},
-    "Heol Cleddau": {"uprn": "100100281816"}
+    "Heol Cleddau": {"uprn": "100100281816"},
 }
 
 API_URL = "https://www.pembrokeshire.gov.uk/template/waste/api.asp"
 
-TYPE_MAP = {   # Dict of waste formatted bin types
+TYPE_MAP = {  # Dict of waste formatted bin types
     "FOODCAD": "GREEN CADDY",
     "BLUEBOX": "BLUE BOX",
     "GREENBOX": "GREEN BOX",
@@ -22,7 +22,7 @@ TYPE_MAP = {   # Dict of waste formatted bin types
     "REDBAG": "RED BAG",
     "GREYBAG": "BLACK/GREY BAGS",
 }
-ICON_MAP = {   # Optional: Dict of waste types and suitable mdi icons
+ICON_MAP = {  # Optional: Dict of waste types and suitable mdi icons
     "FOODCAD": "mdi:food-apple",
     "BLUEBOX": "mdi:note-multiple",
     "GREENBOX": "mdi:glass-fragile",
@@ -31,41 +31,42 @@ ICON_MAP = {   # Optional: Dict of waste types and suitable mdi icons
     "GREYBAG": "mdi:trash-can",
 }
 
-#### Arguments affecting the configuration GUI ####
+# ### Arguments affecting the configuration GUI ####
 
-HOW_TO_GET_ARGUMENTS_DESCRIPTION = { # Optional dictionary to describe how to get the arguments, will be shown in the GUI configuration form above the input fields, does not need to be translated in all languages
+HOW_TO_GET_ARGUMENTS_DESCRIPTION = {  # Optional dictionary to describe how to get the arguments, will be shown in the GUI configuration form above the input fields, does not need to be translated in all languages
     "en": "To get your UPRN go to https://www.findmyaddress.co.uk and search for your address.",
 }
 
-PARAM_DESCRIPTIONS = { # Optional dict to describe the arguments, will be shown in the GUI configuration below the respective input field
+PARAM_DESCRIPTIONS = {  # Optional dict to describe the arguments, will be shown in the GUI configuration below the respective input field
     "en": {
         "uprn": "Unique Property Reference Number (UPRN)",
     },
 }
 
-#### End of arguments affecting the configuration GUI ####
+# ### End of arguments affecting the configuration GUI ####
+
 
 class Source:
-    def __init__(self, uprn: str):  # argX correspond to the args dict in the source configuration
+    def __init__(
+        self, uprn: str
+    ):  # argX correspond to the args dict in the source configuration
         self._uprn = uprn
 
     def fetch(self) -> list[Collection]:
-
         session = requests.Session()
 
         form_data = {
-            "action":"dates",
-            "public":"true",
-            "uprn":self._uprn,
-            "language":"eng",
-
+            "action": "dates",
+            "public": "true",
+            "uprn": self._uprn,
+            "language": "eng",
         }
 
         collection_response = session.post(API_URL, params=form_data)
-        
+
         if ast.literal_eval(collection_response.text)["error"] == "true":
             raise Exception("No collections found for the given UPRN.")
-        
+
         entries = []  # List that holds collection schedule
 
         bin_dates = ast.literal_eval(collection_response.text)["bins"]
@@ -76,11 +77,11 @@ class Source:
             bin_type = bin["type"]
 
             entries.append(
-            Collection(
-                date = date,  # Collection date
-                t = TYPE_MAP.get(bin_type),  # Collection type
-                icon = ICON_MAP.get(bin_type),  # Collection icon
+                Collection(
+                    date=date,  # Collection date
+                    t=TYPE_MAP.get(bin_type),  # Collection type
+                    icon=ICON_MAP.get(bin_type),  # Collection icon
+                )
             )
-        )
 
         return entries
--- doc/source/pembrokeshire_gov_uk.md
+++ doc/source/pembrokeshire_gov_uk.md
@@ -14,12 +14,13 @@ waste_collection_schedule:
 
 ### Configuration Variables
 
-**UPRN**<br>
+**UPRN**  
 *(string)(required)*
 
 The "Unique Property Reference Number" for your address. You can find it by searching for your address at https://www.findmyaddress.co.uk/.
 
 ## Example
+
 ```yaml
 waste_collection_schedule:
     sources:
@@ -31,19 +32,25 @@ waste_collection_schedule:
 ## Returned collection types
 
 ### Food
+
 Green Caddy for food recycling
 
 ### Paper
+
 Blue Box for paper recycling 
 
 ### Glass
+
 Green box for glass recycling
 
 ### Cardboard
+
 Blue Bag for cardboard recycling
 
 ### Plastics
+
 Red Bag for plastics recycling
 
 ### Non-Recyclables
+
 Black/Grey Bags for non-recyclable waste
