6736e09b63b7a0869129b60c6215529e1a58dd0e
DanielV <daniel@vikstrom.name>
2024-10-28 08:55:15 +0000

    Updated fix-script

--- script/fix_icon_use.sh
+++ script/fix_icon_use.sh
@@ -20,6 +20,8 @@ for file in $(git grep --name mdi: -- "$fixpath"); do
 done
 
 echo ""
-echo "Now manually make sure that all files have the correct import:"
+echo "Now manually tidy up the things the script could not fix:"
 echo "1. Run \"git status\" to find which filed have been modified"
 echo "2. If the file does not contian the following import, add it \"from const import Icons\""
+echo "3. Run \"git grep mdi: -- custom_components/waste_collection_schedule/waste_collection_schedule/source/\" to see if there are any remaining icons that can be manually fixed"
+echo "4. Run \"./script/check_icon_use.sh\" to see what files should be removed from whitelist in that script"
