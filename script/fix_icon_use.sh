5f29571001c95e3c719dae304221ff4c406105c7
DanielV <daniel@vikstrom.name>
2024-10-28 08:45:43 +0000

    Add a script the fixes tha majority of icon uses

---
+++ script/fix_icon_use.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+fixpath="custom_components/waste_collection_schedule/waste_collection_schedule/source/"
+
+for file in $(git grep --name mdi: -- "$fixpath"); do
+    echo Trying to fix file $file
+    sed -i -e 's/"mdi:biohazard"/Icons.ICON_BIOHAZARD/g' $file
+    sed -i -e 's/"mdi:bottle-wine-outline"/Icons.ICON_CLEAR_GLASS/g' $file
+    sed -i -e 's/"mdi:bottle-wine"/Icons.ICON_COLORED_GLASS/g' $file
+    sed -i -e 's/"mdi:food"/Icons.ICON_COMPOST/g' $file
+    sed -i -e 's/"mdi:desktop-classic"/Icons.ICON_ELECTRONICS/g' $file
+    sed -i -e 's/"mdi:leaf"/Icons.ICON_GARDEN_WASTE/g' $file
+    sed -i -e 's/"mdi:trash-can"/Icons.ICON_GENERAL_TRASH/g' $file
+    sed -i -e 's/"mdi:delete-empty"/Icons.ICON_LANDFILL/g' $file
+    sed -i -e 's/"mdi:nail"/Icons.ICON_METAL/g' $file
+    sed -i -e 's/"mdi:newspaper"/Icons.ICON_NEWSPAPER/g' $file
+    sed -i -e 's/"mdi:package-variant"/Icons.ICON_GENERAL_TRASH/g' $file
+    sed -i -e 's/"mdi:bottle-soda-classic-outline"/Icons.ICON_PLASTIC/g' $file
+    sed -i -e 's/"mdi:recycle"/Icons.ICON_RECYCLE/g' $file
+done
+
+echo ""
+echo "Now manually make sure that all files have the correct import:"
+echo "1. Run \"git status\" to find which filed have been modified"
+echo "2. If the file does not contian the following import, add it \"from const import Icons\""
