56ad300961e12d063a4c49b78d8f7072c7b234de
ReneNulschDE <ReneNulschDE.github@Nulsch.de>
2024-11-16 00:25:36 +0100

    fix: islington_gov_uk [Bug]: islington council  #3040

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/islington_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/islington_gov_uk.py
@@ -7,8 +7,9 @@ TITLE = "Islington Council"
 DESCRIPTION = "Source for Islington Council, UK."
 URL = "https://www.islington.gov.uk"
 TEST_CASES = {
-    "Test_001": {"uprn": "5300094897"},
-    "Test_002": {"uprn": 10001295652},
+    "Test_001": {"postcode": "n1 1xr", "uprn": "5300094897"},
+    "Test_002": {"postcode": "N1 0DD", "uprn": 10001295652},
+    "Test_003": {"postcode": "N19 4TA", "uprn": "5300078702"},
 }
 ICON_MAP = {
     "Green recycling box": "mdi:recycle",
@@ -19,18 +20,21 @@ ICON_MAP = {
     "Reuseable garden waste sack": "mdi:leaf",
     "Household refuse sack": "mdi:trash-can",
     "Refuse skip": "mdi:trash-can",
+    "Food waste recycling": "mdi:food",
+    "Mixed dry recycling": "mdi:recycle",
+    "Non-recyclable rubbish": "mdi:recycle",
 }
 
 
 class Source:
-    def __init__(self, uprn):
+    def __init__(self, postcode, uprn):
         self._uprn = str(uprn)
+        self._postcode = str(postcode)
         self._session = requests.Session()
 
     def fetch(self):
-        url = (
-            f"https://www.islington.gov.uk/your-area?Postcode=unused&Uprn={self._uprn}"
-        )
+        url = f"https://www.islington.gov.uk/your-area?Postcode={self._postcode}&Uprn={self._uprn}"
+
         response = self._session.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, "html.parser")
@@ -46,8 +50,10 @@ class Source:
         if waste_table:
             rows = waste_table.find_all("tr")
             for row in rows:
-                waste_type = row.find("th").text.strip()
-                collection_day = row.find("td").text.strip()
+                waste_type = row.find("td").text.strip().split(",")[0].split(" - ")[0]
+                collection_day = (
+                    row.find("td").text.strip().split(",")[1].split(" on ")[1]
+                )
 
                 entries.append(
                     Collection(
