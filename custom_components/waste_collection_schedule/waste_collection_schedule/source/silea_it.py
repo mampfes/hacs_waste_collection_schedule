e9bfb6f18ed082f735f0676d0bebde6ca8807353
Marco Vimercati <32430126+mvimercati@users.noreply.github.com>
2024-11-23 16:09:12 +0100

    Silea it (#3061)
    * Added Silea SpA source.
    
    * Run update_docu_links.
    
    * Fix tabs.
    
    * Fix tabs.
    
    * Fix doc.
    
    * Fix tests.
    
    * Fix double spaces in some service name retrieved from source.
    
    * Implemented review's suggestions. Code cleanup.
    
    * Removed unused stuff; add more icons for common services.
    
    * reformatting
    
    ---------
    
    Co-authored-by: 5ila5 <5ila5@users.noreply.github.com>

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/silea_it.py
@@ -0,0 +1,106 @@
+import json
+from datetime import datetime
+
+import requests
+from waste_collection_schedule import Collection
+from waste_collection_schedule.exceptions import SourceArgumentNotFoundWithSuggestions
+
+TITLE = "Silea"
+DESCRIPTION = "Silea"
+URL = "https://www.sileaspa.it"
+
+API = "https://www.sileaspa.it/wp-admin/admin-ajax.php"
+TEST_CASES = {
+    "Test_001": {"municipality": "Lomagna", "address": "via martiri"},
+    "Test_002": {"municipality": "Cesana", "address": "via foscolo"},
+}
+ICON_MAP = {
+    "Raccolta indifferenziato": "mdi:trash-can",
+    "Raccolta sacco viola": "mdi:recycle",
+    "Raccolta vetro": "mdi:bottle-wine",
+    "Raccolta carta": "mdi:newspaper-variant-multiple",
+    "Spazzamento meccanizzato": "mdi:tanker-truck",
+    "Raccolta umido": "mdi:leaf",
+}
+
+
+class Source:
+    def __init__(self, municipality, address):
+        self._municipality = None
+        self._address = None
+
+        s = requests.Session()
+
+        # Retrieve Municipality/town
+        payload = {"action": "get_comuni"}
+        resp = s.post(API, data=payload)
+        data = json.loads(resp.text)
+
+        for item in data:
+            if municipality.lower() in item["name"].lower():
+                self._municipality = item["id"]
+                break
+
+        # Raise error if municipality is not found
+        if self._municipality is None:
+            raise SourceArgumentNotFoundWithSuggestions(
+                "municipality",
+                self._municipality,
+                [item["name"].title() for item in data],
+            )
+
+        # Retrieve Street
+        payload = {"action": "get_vie", "id_cliente": self._municipality}
+        resp = s.post(API, data=payload)
+        data = json.loads(resp.text)
+
+        for item in data:
+            if address.lower() in item["name"].lower():
+                self._address = item["id"]
+                break
+
+        # Raise error if address is not found
+        if self._address is None:
+            raise SourceArgumentNotFoundWithSuggestions(
+                "address", self._address, [item["name"].title() for item in data]
+            )
+
+    def fetch(self):
+        entries = []
+        s = requests.Session()
+
+        payload = {
+            "action": "get_months",
+            "id_cliente": self._municipality,
+            "id_via": self._address,
+        }
+        resp = s.post(API, data=payload)
+        available_months = json.loads(resp.text)
+
+        for month in available_months.values():
+            # Get data for each available month
+            payload = {
+                "action": "get_calendar",
+                "id_cliente": self._municipality,
+                "id_via": self._address,
+                "id_mese": month["id"],
+            }
+            resp = s.post(API, data=payload)
+            month_data = json.loads(resp.text)
+
+            # request and process calendar items for each available month
+            for item in month_data.values():
+                collection_date = datetime.strptime(
+                    item["date"], "%Y-%m-%dT%H:%M:%S"
+                ).date()
+                for service in item["services"]:
+                    service_clean_name = service["service"].replace("  ", " ")
+                    entries.append(
+                        Collection(
+                            date=collection_date,
+                            t=service_clean_name,
+                            icon=ICON_MAP.get(service_clean_name),
+                        )
+                    )
+
+        return entries
---
+++ doc/source/silea_it.md
@@ -0,0 +1,46 @@
+# Silea SpA
+
+Support for schedules provided by [Silea SpA](https://www.sileaspa.it), serving multiple municipalities in Italy/Lombardia.
+
+## Configuration via configuration.yaml
+
+```yaml
+waste_collection_schedule:
+    sources:
+    - name: silea_it
+      args:
+        municipality: MUNICIPALITY (comune)
+        address: ADDRESS (street name)
+```
+
+### Configuration Variables
+
+**municipality**  
+*(String) (required)*
+
+**address**  
+*(String) (required)* Usually starts with 'via' or 'piazza'
+
+## Example
+
+```yaml
+waste_collection_schedule:
+    sources:
+    - name: silea_it
+      args:
+        municipality: Lomagna
+        address: via Martiri
+```
+
+```yaml
+waste_collection_schedule:
+    sources:
+    - name: silea_it
+      args:
+        municipality: Calolzio
+        address: via foscolo
+```
+
+## How to get the source argument
+
+The municipality name and street address are checked against [Silea SpA](https://www.sileaspa.it) database. The extensions tries to match also partial name (i.e "Annone' matches 'Annone Brianza'). Please consider to check your address in [here](https://www.sileaspa.it/calendario-raccolta-rifiuti) in case of problems.
