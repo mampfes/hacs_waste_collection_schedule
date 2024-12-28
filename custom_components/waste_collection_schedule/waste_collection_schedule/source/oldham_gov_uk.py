a1fef94c7654d16d86657d5ff6e4643f18aa80e0
dt215git <86194065+dt215git@users.noreply.github.com>
2024-12-28 15:12:13 +0000

    initial commit

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/oldham_gov_uk.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+
+import requests
+from bs4 import BeautifulSoup
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
+
+TITLE = "Oldham Council"
+DESCRIPTION = "Source for Oldham Council."
+URL = "https://oldham.gov.uk/"
+TEST_CASES = {
+    "Test_001": {"uprn": 422000125973},
+    "Test_002": {"uprn": "422000129104"},
+    "Test_003": {"uprn": 422000042299},
+}
+ICON_MAP = {
+    "Brown": "mdi:recycle",
+    "Green": "mdi:leaf",
+    "Grey": "mdi:trash-can",
+    "Blue": "mdi:newspaper",
+}
+
+
+class Source:
+    def __init__(self, uprn: str | int):
+        self._uprn = str(uprn)
+
+    def fetch(self):
+        s = requests.session()
+        r = s.get(
+            f"https://portal.oldham.gov.uk/bincollectiondates/details?uprn={self._uprn}"
+        )
+        r.raise_for_status
+
+        soup = BeautifulSoup(r.content, "html.parser")
+        pickups: list = soup.find_all("table", {"class": "data-table confirmation"})
+
+        entries: list = []
+        for item in pickups:
+            w: str = item.find("th")
+            d: str = item.find("td", {"class": "coltwo"})
+            entries.append(
+                Collection(
+                    date=datetime.strptime(d.text.strip(), "%d/%m/%Y").date(),
+                    t=w.text.strip(),
+                    icon=ICON_MAP.get(w.text.strip()),
+                )
+            )
+
+        return entries
