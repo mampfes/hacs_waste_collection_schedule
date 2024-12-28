39e9732acb48e51b802a7a10b63ec4935b89a469
dt215git <86194065+dt215git@users.noreply.github.com>
2024-12-28 15:17:44 +0000

    GUI details added

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/oldham_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/oldham_gov_uk.py
@@ -18,6 +18,21 @@ ICON_MAP = {
     "Grey": "mdi:trash-can",
     "Blue": "mdi:newspaper",
 }
+HOW_TO_GET_ARGUMENTS_DESCRIPTION = {
+    "en": "an easy way to discover your Unique Property Reference Number (UPRN) is by going to https://www.findmyaddress.co.uk/ and entering in your address details.",
+}
+
+PARAM_TRANSLATIONS = {
+    "en": {
+        "uprn": "Unique Property Reference Number (UPRN)",
+    }
+}
+
+PARAM_DESCRIPTIONS = {
+    "en": {
+        "uprn": "Unique Property Reference Number (UPRN)",
+    }
+}
 
 
 class Source:
