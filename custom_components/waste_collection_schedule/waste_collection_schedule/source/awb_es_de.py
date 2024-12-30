d508ce85f402c0a87c53998789120da467e60fd3
5ila5 <5ila5@users.noreply.github.com>
2024-12-30 16:37:42 +0100

    imporve awb_es_de error handling

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/awb_es_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/awb_es_de.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
+from waste_collection_schedule.exceptions import SourceArgumentNotFoundWithSuggestions
 from waste_collection_schedule.service.ICS import ICS
 
 TITLE = "Abfallwirtschaftsbetrieb Esslingen"
@@ -8,7 +9,7 @@ DESCRIPTION = "Source for AWB Esslingen, Germany"
 URL = "https://www.awb-es.de"
 
 TEST_CASES = {
-    "Aichwald": {"city": "Aichwald", "street": "Alte Dorfstrasse"},
+    "Aichwald": {"city": "Aichwald", "street": "Alte Dorfstraße Alle Hausnummern"},
     "Kohlberg": {"city": "Kohlberg", "street": "alle Straßen"},
 }
 
@@ -20,6 +21,47 @@ class Source:
         self._city = city
         self._street = street
         self._ics = ICS()
+
+    def _validate_city(self):
+        data = {
+            "search": self._city,
+            "parent": "",
+            "kind": "removaldate.city",
+        }
+        r = requests.post(
+            "https://www.awb-es.de/statics/abfallplus/search.json.php", data=data
+        )
+        r.raise_for_status()
+        suggestsion = r.json()["suggestions"]
+        for suggestion in suggestsion:
+            if suggestion["value"].lower() == self._city.lower():
+                return
+
+        raise SourceArgumentNotFoundWithSuggestions(
+            "city",
+            self._city,
+            suggestions=[suggestion["value"] for suggestion in suggestsion],
+        )
+
+    def _validate_street(self):
+        data = {
+            "search": self._street,
+            "parent": self._city,
+            "kind": "removaldate.street",
+        }
+        r = requests.post(
+            "https://www.awb-es.de/statics/abfallplus/search.json.php", data=data
+        )
+        r.raise_for_status()
+        suggestsion = r.json()["suggestions"]
+        for suggestion in suggestsion:
+            if suggestion["value"].lower() == self._street.lower():
+                return
+        raise SourceArgumentNotFoundWithSuggestions(
+            "street",
+            self._street,
+            suggestions=[suggestion["value"] for suggestion in suggestsion],
+        )
 
     def fetch(self):
         session = requests.Session()
@@ -40,10 +82,14 @@ class Source:
         ics_urls = list()
         for download in downloads:
             href = download.get("href")
-            if "t=ics" in href and href not in ics_urls: #The website lists the same url multiple times, we only want it once
+            if (
+                "t=ics" in href and href not in ics_urls
+            ):  # The website lists the same url multiple times, we only want it once
                 ics_urls.append(href)
 
         if not ics_urls:
+            self._validate_city()
+            self._validate_street()
             raise Exception("ics url not found")
 
         entries = []
@@ -57,5 +103,5 @@ class Source:
 
             for d in dates:
                 entries.append(Collection(d[0], d[1]))
-        
+
         return entries
