fe5b17bddb446e2c49b99768a7c109496d1218a1
dt215git <86194065+dt215git@users.noreply.github.com>
2025-01-05 11:37:38 +0000

    initial commit

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/bridgend_gov_uk.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+
+import requests
+from bs4 import BeautifulSoup
+from waste_collection_schedule import Collection
+
+TITLE = "Bridgend County Borough Council"
+DESCRIPTION = "Source for bridgend.gov.uk"
+URL = "https://www.bridgend.gov.uk/"
+TEST_CASES = {
+    "test_001": {"uprn": "100100479873"},
+}
+ICON_MAP = {
+    "Refuse": "mdi:trash-can",
+    "Recycling": "mdi:recycle",
+}
+HEADERS: dict = {"user-agent": "Mozilla/5.0"}
+
+
+class Source:
+    def __init__(self, uprn: str | int):
+        self._uprn = str(uprn)
+
+    def fetch(self) -> list[Collection]:
+        s = requests.Session()
+        r = s.get(
+            f"https://bridgendportal.azurewebsites.net/property/{self._uprn}",
+            headers=HEADERS,
+        )
+        soup = BeautifulSoup(r.content, "html.parser")
+
+        tds = soup.find_all("td", {"class": ["service-name", "next-service"]})
+        waste_types = tds[0::2]
+        waste_dates = tds[1::2]
+
+        entries = []
+        for i in range(len(waste_types)):
+            waste_type = waste_types[i].text.split(" ")[0].replace("\n", "").strip()
+            waste_date = (
+                waste_dates[i]
+                .text.split(" ")[1]
+                .replace("\t", "")
+                .replace("Service\n", "")
+                .strip()
+            )
+            entries.append(
+                Collection(
+                    date=datetime.strptime(waste_date, "%d/%m/%Y").date(),
+                    t=waste_type,
+                    icon=ICON_MAP.get(waste_type),
+                )
+            )
+
+        return entries
