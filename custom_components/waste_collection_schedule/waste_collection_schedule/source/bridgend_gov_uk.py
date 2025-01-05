3d63982a968b0cf47b63e1005aa2984b403d297f
dt215git <86194065+dt215git@users.noreply.github.com>
2025-01-05 11:46:33 +0000

    gui dicts added, type hinting added

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/bridgend_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/bridgend_gov_uk.py
@@ -7,16 +7,30 @@ from waste_collection_schedule import Collection
 TITLE = "Bridgend County Borough Council"
 DESCRIPTION = "Source for bridgend.gov.uk"
 URL = "https://www.bridgend.gov.uk/"
-TEST_CASES = {
+TEST_CASES: dict = {
     "test_001": {"uprn": "100100479873"},
     "test_002": {"uprn": 10032996088},
     "test_003": {"uprn": "10090813443"},
 }
-ICON_MAP = {
+ICON_MAP: dict = {
     "Refuse": "mdi:trash-can",
     "Recycling": "mdi:recycle",
 }
 HEADERS: dict = {"user-agent": "Mozilla/5.0"}
+
+HOW_TO_GET_ARGUMENTS_DESCRIPTION: dict = {
+    "en": "an easy way to discover your Unique Property Reference Number (UPRN) is by going to https://www.findmyaddress.co.uk/ and entering in your address details.",
+}
+PARAM_TRANSLATIONS: dict = {
+    "en": {
+        "uprn": "Unique Property Reference Number (UPRN)",
+    }
+}
+PARAM_DESCRIPTIONS: dict = {
+    "en": {
+        "uprn": "Unique Property Reference Number (UPRN)",
+    }
+}
 
 
 class Source:
@@ -29,13 +43,13 @@ class Source:
             f"https://bridgendportal.azurewebsites.net/property/{self._uprn}",
             headers=HEADERS,
         )
-        soup = BeautifulSoup(r.content, "html.parser")
+        soup: BeautifulSoup = BeautifulSoup(r.content, "html.parser")
 
-        tds = soup.find_all("td", {"class": ["service-name", "next-service"]})
-        waste_types = tds[0::2]
-        waste_dates = tds[1::2]
+        tds: list = soup.find_all("td", {"class": ["service-name", "next-service"]})
+        waste_types: list = tds[0::2]
+        waste_dates: list = tds[1::2]
 
-        entries = []
+        entries: list = []
         for i in range(len(waste_types)):
             waste_type = waste_types[i].text.split(" ")[0].replace("\n", "").strip()
             waste_date = (
