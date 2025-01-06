e00eb67c0efe4b8f3f02e37607fea596b60ec4a3
Kamil <k.sciana@gmail.com>
2025-01-06 22:02:01 +0100

    add Gmina Środa Śląska

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/gmina_sroda_slaska_pl.py
@@ -0,0 +1,49 @@
+import datetime
+
+import requests
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
+
+TITLE = "Gmina Środa Śląska"
+DESCRIPTION = "Source for Gmina Środa Śląska, Poland"
+URL = "https://waste-collection.sciana.pro"
+TEST_CASES = {
+    "Ciechów": {"location_id": 3},
+}
+
+ICON_MAP = {
+    "Zmieszane komunalne": "mdi:trash-can",  # Mixed
+    "Tworzywa sztuczne": "mdi:recycle",  # Plastic
+    "Odpady kuchenne ulegające biodegradacji": "mdi:leaf",  # Organic
+    "Papier": "mdi:file-outline",  # Paper
+    "Szkło": "mdi:glass-fragile",  # Glass
+    "Odpady wielkogabarytowe": "mdi:sofa-single",
+}
+
+API_URL = "https://waste-collection.sciana.pro/data/"
+
+
+class Source:
+    def __init__(self, location_id):
+        self._location_id = location_id
+
+    def fetch(self):
+        api_response = requests.get(
+            API_URL + str(self._location_id)
+        )
+
+        entries = []
+
+        kinds = api_response.json()["data"]["garbage_kinds"]
+
+        for k in kinds:
+            name = k["name"]
+            for d in k["disposals"]:
+                entries.append(
+                    Collection(
+                        date=datetime.datetime.strptime(d["date"], "%Y-%m-%d").date(),
+                        t=name.capitalize(),
+                        icon=ICON_MAP.get(name),
+                    )
+                )
+
+        return entries
