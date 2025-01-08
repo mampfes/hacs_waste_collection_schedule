43f51c68ea726bb8a67f166983ac03ced2795541
dt215git <86194065+dt215git@users.noreply.github.com>
2025-01-08 20:29:56 +0000

    soup updates

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/swale_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/swale_gov_uk.py
@@ -1,16 +1,19 @@
-import json
-import re
+# import json
+# import re
+
+from time import sleep as sleep
 
 import requests
 from bs4 import BeautifulSoup
-from dateutil.parser import parse
+
+# from dateutil.parser import parse
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = "Swale Borough Council"
 DESCRIPTION = "Source for swale.gov.uk services for Swale, UK."
 URL = "https://swale.gov.uk"
 TEST_CASES = {
-    "Swale House": {"uprn": 100062375927, "postcode": "ME10 3HT"},
+    # "Swale House": {"uprn": 100062375927, "postcode": "ME10 3HT"},
     "1 Harrier Drive": {"uprn": 100061091726, "postcode": "ME10 4UY"},
 }
 
@@ -53,64 +56,81 @@ class Source:
         self._postcode = postcode
 
     def fetch(self) -> list[Collection]:
-        entries = []
+        entries: list = []
 
         session = requests.Session()
         session.headers.update(HEADERS)
 
         first_form_data = {
-            "SQ_FORM_462397_PAGE": "1",
-            "q462406:q1": self._postcode,
-            "form_email_462397_submit": "Next &#10140;",
+            "SQ_FORM_485465_PAGE": "1",
+            "form_email_485465_referral_url": "https://swale.gov.uk/bins-littering-and-the-environment/bins/check-your-bin-day-v2",
+            "q485476:q1": self._postcode,
+            "form_email_485465_submit": "Choose Your Address &#10140;",
         }
 
         resp = session.post(API_URL, data=first_form_data)
         resp.raise_for_status()
+        sleep(10)
 
         second_form_data = {
-            "SQ_FORM_462397_PAGE": "2",
-            "q462407:q3": self._uprn,
-            "form_email_462397_submit": "Get Bin Days &#10140;",
+            "SQ_FORM_485465_PAGE": "2",
+            "form_email_485465_referral_url": "https://swale.gov.uk/bins-littering-and-the-environment/bins/check-your-bin-day-v2",
+            "q485480:q1": self._uprn,
+            "form_email_485465_submit": "Get Bin Days &#10140;",
         }
 
         collection_response = session.post(API_URL, data=second_form_data)
         collection_response.raise_for_status()
+        print(collection_response.content)
 
         collection_soup = BeautifulSoup(collection_response.text, "html.parser")
 
-        section = collection_soup.find("section", id="SBC-YBD_main")
-        if not section:
-            raise ValueError(
-                "Could not find SBC-YBD_main section. Most likely html has changed"
-            )
-        script = section.find("script", recursive=False)
-        if not script:
-            raise ValueError(
-                "Could not find script entry. Most likely html has changed"
-            )
+        # get details of next collection
+        next_date = collection_soup.find("strong", {"id": "SBC-YBD-collectionDate"})
+        next_wastes = collection_soup.find("div", {"id": "SBCFirstBins"})
+        next_items = next_wastes.find_all("li")
+        print(next_date, next_items)
 
-        bin_data = re.search(
-            r"var BIN_DAYS = Object\.entries\(JSON\.parse\('(.+)'\)\);", script.string
-        )
-        if not bin_data:
-            raise ValueError(
-                "Could not find BIN_DAYS in response. Most likely html has changed"
-            )
-        bin_days = json.loads(bin_data.group(1))
-        for bin in bin_days:
-            bin_details = bin_days[bin]
-            if bin_details["Active"] == "Y":
-                for dateKey in DATE_KEYS:
-                    if dateKey in bin_details:
-                        entries.append(
-                            Collection(
-                                date=parse(bin_details[dateKey]).date(),
-                                t=bin,
-                                icon=ICON_MAP.get(bin),
-                            )
-                        )
-        if not entries:
-            raise ValueError(
-                "Could not get collections for the given combination of UPRN and Postcode."
-            )
+        # get details of future collection
+        future_collection = collection_soup.find("div", {"id": "FutureCollections"})
+        future_date = future_collection.text.split(", ")[-1]
+        future_wastes = collection_soup.find("ul", {"id": "FirstFutureBins"})
+        future_items = future_wastes.find_all("li")
+        print(future_date, future_items)
+
+        # section = collection_soup.find("section", id="SBC-YBD-collectionDate")
+        # if not section:
+        #     raise ValueError(
+        #         "Could not find SBC-YBD_main section. Most likely html has changed"
+        #     )
+        # script = section.find("script", recursive=False)
+        # if not script:
+        #     raise ValueError(
+        #         "Could not find script entry. Most likely html has changed"
+        #     )
+
+        # bin_data = re.search(
+        #     r"var BIN_DAYS = Object\.entries\(JSON\.parse\('(.+)'\)\);", script.string
+        # )
+        # if not bin_data:
+        #     raise ValueError(
+        #         "Could not find BIN_DAYS in response. Most likely html has changed"
+        #     )
+        # bin_days = json.loads(bin_data.group(1))
+        # for bin in bin_days:
+        #     bin_details = bin_days[bin]
+        #     if bin_details["Active"] == "Y":
+        #         for dateKey in DATE_KEYS:
+        #             if dateKey in bin_details:
+        #                 entries.append(
+        #                     Collection(
+        #                         date=parse(bin_details[dateKey]).date(),
+        #                         t=bin,
+        #                         icon=ICON_MAP.get(bin),
+        #                     )
+        #                 )
+        # if not entries:
+        #     raise ValueError(
+        #         "Could not get collections for the given combination of UPRN and Postcode."
+        #     )
         return entries
