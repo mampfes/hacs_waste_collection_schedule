d1bb6e95f9b0c387751919fe65265c536d8b7fd9
dt215git <86194065+dt215git@users.noreply.github.com>
2025-01-11 09:58:51 +0000

    remove redundant old code

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/swale_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/swale_gov_uk.py
@@ -1,13 +1,8 @@
-# import json
-# import re
-
 from datetime import date, datetime, timedelta
 from time import sleep as sleep
 
 import requests
 from bs4 import BeautifulSoup
-
-# from dateutil.parser import parse
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = "Swale Borough Council"
@@ -43,13 +38,6 @@ PARAM_DESCRIPTIONS = {
         "postcode": "Postcode of the property",
     },
 }
-
-# DATE_KEYS = {
-#     "NextDateUTC",
-#     "FollowingDateUTC",
-#     "Following2DateUTC",
-#     "Following3DateUTC",
-# }
 
 
 class Source:
@@ -150,54 +138,4 @@ class Source:
                 )
             )
 
-        # collection_soup = BeautifulSoup(collection_response.text, "html.parser")
-
-        # # get details of next collection
-        # next_date = collection_soup.find("strong", {"id": "SBC-YBD-collectionDate"})
-        # next_wastes = collection_soup.find("div", {"id": "SBCFirstBins"})
-        # next_items = next_wastes.find_all("li")
-        # print(next_date, next_items)
-
-        # # get details of future collection
-        # future_collection = collection_soup.find("div", {"id": "FutureCollections"})
-        # future_date = future_collection.text.split(", ")[-1]
-        # future_wastes = collection_soup.find("ul", {"id": "FirstFutureBins"})
-        # future_items = future_wastes.find_all("li")
-        # print(future_date, future_items)
-
-        # section = collection_soup.find("section", id="SBC-YBD-collectionDate")
-        # if not section:
-        #     raise ValueError(
-        #         "Could not find SBC-YBD_main section. Most likely html has changed"
-        #     )
-        # script = section.find("script", recursive=False)
-        # if not script:
-        #     raise ValueError(
-        #         "Could not find script entry. Most likely html has changed"
-        #     )
-
-        # bin_data = re.search(
-        #     r"var BIN_DAYS = Object\.entries\(JSON\.parse\('(.+)'\)\);", script.string
-        # )
-        # if not bin_data:
-        #     raise ValueError(
-        #         "Could not find BIN_DAYS in response. Most likely html has changed"
-        #     )
-        # bin_days = json.loads(bin_data.group(1))
-        # for bin in bin_days:
-        #     bin_details = bin_days[bin]
-        #     if bin_details["Active"] == "Y":
-        #         for dateKey in DATE_KEYS:
-        #             if dateKey in bin_details:
-        #                 entries.append(
-        #                     Collection(
-        #                         date=parse(bin_details[dateKey]).date(),
-        #                         t=bin,
-        #                         icon=ICON_MAP.get(bin),
-        #                     )
-        #                 )
-        # if not entries:
-        #     raise ValueError(
-        #         "Could not get collections for the given combination of UPRN and Postcode."
-        #     )
         return entries
