c03dbf62c964bf0fe44b1f747f9adf085e226b08
5ila5 <5ila5@users.noreply.github.com>
2025-01-15 14:56:35 +0100

    reformatting + fixing typhint + fixing 2 typos

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/montreal_ca.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/montreal_ca.py
@@ -88,7 +88,7 @@ MONTH_PATTERN = r"\b(?:January|February|March|April|May|June|July|August|Septemb
 LOGGER = logging.getLogger(__name__)
 HOW_TO_GET_ARGUMENTS_DESCRIPTION = {
     "en": 'Download on your computer a <a href="https://donnees.montreal.ca/dataset/2df0fa28-7a7b-46c6-912f-93b215bd201e/resource/5f3fb372-64e8-45f2-a406-f1614930305c/download/collecte-des-ordures-menageres.geojson">Montreal GeoJSON file</a><br/>Visit https://geojson.io/<br/>Click on *Open* and select the Montreal GeoJSON file<br/>Find your sector on the map.',
-    # "fr": 'Téléchargez un <a href="https://donnees.montreal.ca/dataset/2df0fa28-7a7b-46c6-912f-93b215bd201e/resource/5f3fb372-64e8-45f2-a406-f1614930305c/download/collecte-des-ordures-menageres.geojson">fichier Montreal GeoJSON</a><br/>Visitez https://geojson.io/<br/>Ouvrez le fichier Montreal GeoJSON<br/>Trouvez votre secteur sur la carte.',
+    "fr": 'Téléchargez un <a href="https://donnees.montreal.ca/dataset/2df0fa28-7a7b-46c6-912f-93b215bd201e/resource/5f3fb372-64e8-45f2-a406-f1614930305c/download/collecte-des-ordures-menageres.geojson">fichier Montreal GeoJSON</a><br/>Visitez https://geojson.io/<br/>Ouvrez le fichier Montreal GeoJSON<br/>Trouvez votre secteur sur la carte.',
 }
 
 PARAM_TRANSLATIONS = {
@@ -99,13 +99,13 @@ PARAM_TRANSLATIONS = {
         "food": "Food waste sector",
         "green": "Greens and leafs sector",
     },
-    # "fr": {
-    #     "sector": "Secteur ordure ménagère",
-    #     "recycling": "Secteur recyclage",
-    #     "bulky": "Secteur item encombrants",
-    #     "food": "Secteur compost",
-    #     "green": "Secteur résiduts verts et feuilles mortes",
-    # },
+    "fr": {
+        "sector": "Secteur ordure ménagère",
+        "recycling": "Secteur recyclage",
+        "bulky": "Secteur item encombrants",
+        "food": "Secteur compost",
+        "green": "Secteur résiduts verts et feuilles mortes",
+    },
 }
 PARAM_DESCRIPTIONS = {
     "en": {
@@ -115,47 +115,15 @@ PARAM_DESCRIPTIONS = {
         "food": "If value is different from waste sector.",
         "green": "If value is different from waste sector.",
     },
-    # "fr": {
-    #     "sector": "Ce secteur est utilisé par défault",
-    #     "recycling": "Si différent du secteur des ordures ménagères.",
-    #     "bulky": "Si différent du secteur des ordures ménagères.",
-    #     "food": "Si différent du secteur des ordures ménagères.",
-    #     "green": "Si différent du secteur des ordures ménagères.",
-    # },
-}
-
-PARAM_TRANSLATIONS = {
-    "en": {
-        "sector": "Waste sector",
-        "recycling": "Recycling sector",
-        "bulky": "Bulky items sector",
-        "food": "Food waste sector",
-        "green": "Greens and leafs sector"
-    },
-    "fr": {
-        "sector": "Secteur ordure ménagère",
-        "recycling": "Secteur recyclage",
-        "bulky": "Secteur item encombrants",
-        "food": "Secteur compost",
-        "green": "Secteur résiduts verts et feuilles mortes"
-    }
-}
-PARAM_DESCRIPTIONS = {
-    "en": {
-        "sector": "This is the default sector.",
-        "recycling": "If value is different from waste sector.",
-        "bulky": "If value is different from waste sector.",
-        "food": "If value is different from waste sector.",
-        "green": "If value is different from waste sector."
-    },
     "fr": {
         "sector": "Ce secteur est utilisé par défault",
         "recycling": "Si différent du secteur des ordures ménagères.",
         "bulky": "Si différent du secteur des ordures ménagères.",
         "food": "Si différent du secteur des ordures ménagères.",
-        "green": "Si différent du secteur des ordures ménagères."
-    }
+        "green": "Si différent du secteur des ordures ménagères.",
+    },
 }
+
 
 class Source:
     def __init__(
