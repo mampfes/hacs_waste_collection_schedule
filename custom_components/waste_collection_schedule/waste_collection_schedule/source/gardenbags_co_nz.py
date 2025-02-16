9594c22fe77dfda19278a62edf083612a18c4ec4
Fabian Gonzalo Artur de la Villarmois <gonzartur@gmail.com>
2025-02-16 17:06:32 +1300

    Add gardenbags.co.nz source

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/gardenbags_co_nz.py
@@ -0,0 +1,55 @@
+import requests
+from bs4 import BeautifulSoup
+from dateutil.parser import parse
+
+from waste_collection_schedule import Collection
+
+TITLE = "GardenBags NZ"
+DESCRIPTION = "Source for GatdenBags NZ."
+URL = "https://gardenbags.co.nz"
+TEST_CASES = {
+    "Test 1": {
+        "account_number": "!secret gardenbags_co_nz_account_number",
+        "account_pin": "!secret gardenbags_co_nz_account_pin",
+        "franchise": "!secret gardenbags_co_nz_franchise",
+    },
+}
+
+COUNTRY = "nz"
+
+class Source:
+    def __init__(self, account_number: str, account_pin: str, franchise=None):
+        self._account_number: str = account_number
+        self._account_pin: str = account_pin
+        self._franchise: str | None = franchise
+
+    def fetch(self):
+        url = f"{URL}/4DACTION/Web_GF_Login"
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        data = {
+            "web_Username": self._account_number,
+            "web_Password": self._account_pin,
+            "web_Franchise": self._franchise,
+            "web_Javascript": "false",
+            "Submit": "Login"
+        }
+
+        try: 
+            response = requests.post(url, headers=headers, data=data, verify=False)
+
+
+            soup = BeautifulSoup(response.content, "html.parser")
+            upcoming_collection = soup.find("div", class_="upcoming-collection next-collection")
+
+            entries = []
+
+            if upcoming_collection:
+                collection_date_str = upcoming_collection.find("span", class_="not-v-cust").text.strip()
+                collection_date = parse(collection_date_str, fuzzy=True)
+                if collection_date:
+                    date = collection_date.date()
+                    entries.append(Collection(date=date, t="Organic waste", icon="mdi:leaf"))
+                
+            return entries
+        except Exception as exception:
+            pass
