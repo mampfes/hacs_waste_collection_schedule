3e51b6093a23b9fdbb911dc4e30f89b7f234ec23
5ila5 <5ila5@users.noreply.github.com>
2025-04-15 12:49:29 +0200

    Exception improvements + reformatting

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/zys_harmonogram_pl.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/zys_harmonogram_pl.py
@@ -1,22 +1,22 @@
-####Mod by SEMATpl - semat.pl - based on sepan_remondis_pl ###
+# ### Mod by SEMATpl - semat.pl - based on sepan_remondis_pl ###
 import datetime
 import json
 import logging
-from bs4 import BeautifulSoup
 
 import requests
+from bs4 import BeautifulSoup
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
-
+from waste_collection_schedule.exceptions import SourceArgumentNotFoundWithSuggestions
 
 TITLE = "Kleszczewo/Kostrzyn"
 DESCRIPTION = "Source for Kleszczewo/Kostrzyn commune garbage collection"
-URL = "https://www.puk-zys.pl/index.php/harmonogram-wywozow-2024-r/" #Only Kleszczewo and Kostrzyn
+URL = "https://www.puk-zys.pl/index.php/harmonogram-wywozow-2024-r/"  # Only Kleszczewo and Kostrzyn
 TEST_CASES = {
     "Street Name": {
         "city": "Komorniki",
         "street_name": "Komorniki",
         "street_number": "93/2",
-        "commune_name": "Kleszczewo"
+        "commune_name": "Kleszczewo",
     },
 }
 
@@ -43,15 +43,16 @@ class Source:
         self._street_number = street_number.upper()
         self._commune_name = commune_name.lower()
 
-
     def fetch(self):
         try:
-            return self.get_data(API_URL.format(self._commune_name,datetime.datetime.now().year))
+            return self.get_data(
+                API_URL.format(self._commune_name, datetime.datetime.now().year)
+            )
         except Exception:
             _LOGGER.debug(
                 f"fetch failed for source {TITLE}: trying different API_URL ..."
             )
-            return self.get_data(API_URL.format("",""))
+            return self.get_data(API_URL.format("", ""))
 
     def get_data(self, api_url):
         r = requests.get(f"{api_url}/addresses/cities")
@@ -59,20 +60,24 @@ class Source:
         city_id = 0
         cities = json.loads(r.text)
         for item in cities:
-            if item["value"] == self._city:
+            if item["value"].upper() == self._city:
                 city_id = item["id"]
         if city_id == 0:
-            raise Exception("city not found")
+            raise SourceArgumentNotFoundWithSuggestions(
+                "city", self._city, [c["value"] for c in cities]
+            )
 
         r = requests.get(f"{api_url}/addresses/streets/{city_id}")
         r.raise_for_status()
         street_id = 0
         streets = json.loads(r.text)
         for item in streets:
-            if item["value"] == self._street_name:
+            if item["value"].upper() == self._street_name:
                 street_id = item["id"]
         if street_id == 0:
-            raise Exception("street not found")
+            raise SourceArgumentNotFoundWithSuggestions(
+                "street_name", self._street_name, [s["value"] for s in streets]
+            )
 
         r = requests.get(f"{api_url}/addresses/numbers/{city_id}/{street_id}")
         r.raise_for_status()
@@ -82,9 +87,11 @@ class Source:
             if item["value"] == self._street_number:
                 number_id = item["id"]
         if number_id == 0:
-            raise Exception("number not found")
+            raise SourceArgumentNotFoundWithSuggestions(
+                "street_number", self._street_number, [n["value"] for n in numbers]
+            )
 
-        r = requests.get(f"{api_url}/reports", params={"type": "html","id": number_id})
+        r = requests.get(f"{api_url}/reports", params={"type": "html", "id": number_id})
         r.raise_for_status()
         report = json.loads(r.text)
         if report["status"] != "success":
@@ -92,9 +99,9 @@ class Source:
 
         r = requests.get(report["filePath"])
         r.raise_for_status()
-        r.encoding = 'utf-8'
+        r.encoding = "utf-8"
         soup = BeautifulSoup(r.text, "html.parser")
-        
+
         table = soup.find("table")
         if not table:
             raise Exception("Table not found in the HTML response")
@@ -108,17 +115,21 @@ class Source:
                 continue
             cells = row.find_all("td")
             for cell_index, cell in enumerate(cells):
-                if cell_index == 0 or not isinstance(cell.text, str) or not cell.text.strip():
+                if (
+                    cell_index == 0
+                    or not isinstance(cell.text, str)
+                    or not cell.text.strip()
+                ):
                     continue
                 for day in cell.text.split(","):
                     entries.append(
                         Collection(
                             datetime.date(year, row_index - 1, int(day)),
                             NAME_MAP[cell_index],
-                            ICON_MAP.get(cell_index, "mdi:trash-can"),  # Default to trash can if no icon is found
+                            ICON_MAP.get(
+                                cell_index, "mdi:trash-can"
+                            ),  # Default to trash can if no icon is found
                         )
                     )
 
-
         return entries
-        
