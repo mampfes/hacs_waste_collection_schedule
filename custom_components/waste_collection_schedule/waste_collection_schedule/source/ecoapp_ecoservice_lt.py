1b104d029d07be16abd660bc703dcbc24b7e3220
5ila5 <5ila5@users.noreply.github.com>
2025-07-05 13:53:38 +0200

    comment out lt translations as it's not a supported language yet

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/ecoapp_ecoservice_lt.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/ecoapp_ecoservice_lt.py
@@ -32,12 +32,12 @@ EXTRA_INFO = [{"url": "https://ecoservice.lt", "title": "Ecoservice atliekos"}]
 
 PARAM_DESCRIPTIONS = {
     "en": {"waste_object_ids": "Waste object ID, or IDs separated by commas."},
-    "lt": {"waste_object_ids": "Konteinerio vienas arba keli ID, atskirti kableliais."},
+    # "lt": {"waste_object_ids": "Konteinerio vienas arba keli ID, atskirti kableliais."},
 }
 
 PARAM_TRANSLATIONS = {
     "en": {"waste_object_ids": "e.g. 13-L-115261, 13-P-505460"},
-    "lt": {"waste_object_ids": "pvz. 13-L-115261, 13-P-505460"},
+    # "lt": {"waste_object_ids": "pvz. 13-L-115261, 13-P-505460"},
 }
 
 
