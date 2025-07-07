c853239d120ed4862ac2b60bf4e91503d6d47689
Owen Holloway <owen.holloway@lanceolata.com.au>
2025-07-08 00:03:24 +1000

    it works

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/ccc_tas_gov_au.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/ccc_tas_gov_au.py
@@ -10,7 +10,7 @@ DESCRIPTION = "The greater Eastern Shore of Hobart"
 
 URL = "https://www.ccc.tas.gov.au/wp-json/waste-collection"
 ICON_MAP = {
-    "DOMESTIC": "mdi:trash-can",
+    "RUBBISH": "mdi:trash-can",
     "RECYCLE": "mdi:recycle",
     "ORGANIC": "mdi:leaf",
 }
@@ -30,35 +30,29 @@ PARAM_TRANSLATIONS = {
 TEST_CASES = {
     "80 Clarence St": {
         "address": "80 Clarence St"
+    },    
+    "50 East Derwent Highway": {
+        "address": "50 East Derwent Highway"
     },
 }
-
-WEEKDAY_MAP = {
-    "Monday": 0,
-    "Tuesday": 1,
-    "Wednesday": 2,
-    "Thursday": 3,
-    "Friday": 4,
-    "Saturday": 5,
-    "Sunday": 6,
-}
-
 
 def _get_next_n_dates(date_obj: date, n: int, delta: timedelta):
     next_dates = []
     for _ in range(n):
-        date_obj += delta
         while date_obj <= date.today():
             date_obj += delta
         next_dates.append(date_obj)
+        date_obj += delta
     return next_dates
 
 
-def _get_previous_date_for_day_of_week(day_of_week: int):
-    today = date.today()
-    return today - timedelta((today.weekday() - day_of_week + 7) % 7)
+def _next_date(value) -> date:
+    splits = value.replace(",","").split(" ")
+    next_date_raw = splits[-2].split("/")
+    return date(int(next_date_raw[2]),int(next_date_raw[1]),int(next_date_raw[0]))
+    
 
-def calculate_frequency(value) -> int:
+def _calculate_frequency(value) -> int:
     splits = value.replace(",","").split(" ")
     
     next_date_raw = splits[-2].split("/")
@@ -68,31 +62,73 @@ def calculate_frequency(value) -> int:
     next_date_n = date(int(next_date_n_raw[2]),int(next_date_n_raw[1]),int(next_date_n_raw[0]))
     frequency = next_date_n - next_date
 
-    print(int(frequency.days / 7))
+    return int(frequency.days)
 
-    return int(frequency.days / 7)
 
-def process_results(results) -> list[Collection]:
+def _process_results(results) -> list[Collection]:
     entries = []
 
-    collection_day = "unknown"
     frequency_rubbish = 0
+    next_rubbish = date(1970,1,1)
     frequency_recycling = 0
+    next_recycling = date(1970,1,1)
     frequency_greenwaste = 0
+    next_greenwaste = date(1970,1,1)
 
     for result in results:
         if result['name'] == "Waste Collection Day":
             collection_day = result['value']
         
         if result['name'] == 'Garbage Pickup':
-            frequency_rubbish = calculate_frequency(result['value'])
+            frequency_rubbish = _calculate_frequency(result['value'])
+            next_rubbish = _next_date(result['value'])
 
         if result['name'] == 'Recycling Pickup':
-            frequency_recycling = calculate_frequency(result['value'])
+            frequency_recycling = _calculate_frequency(result['value'])
+            next_recycling = _next_date(result['value'])
 
         if result['name'] == 'Green Waste Pickup':
-            frequency_greenwaste = calculate_frequency(result['value'])
+            frequency_greenwaste = _calculate_frequency(result['value'])
+            next_greenwaste = _next_date(result['value'])
 
+    print("rubbish, next %s, frequency %i" % (next_rubbish, frequency_rubbish))
+    print("recycling, next %s, frequency %i" % (next_recycling, frequency_recycling))
+    print("greenwaste, next %s, frequency %i" % (next_greenwaste, frequency_greenwaste))
+
+    rubbish_collection_dates = _get_next_n_dates(
+        next_rubbish, 52, timedelta(days=frequency_rubbish)
+    )
+
+    recycling_collection_dates = _get_next_n_dates(
+        next_recycling, 52, timedelta(days=frequency_recycling)
+    )
+
+    greenwaste_collection_dates = _get_next_n_dates(
+        next_greenwaste, 52, timedelta(days=frequency_greenwaste)
+    )
+
+    entries.extend(
+        [
+            Collection(date=collection_date, t="Rubbish", icon=ICON_MAP["RUBBISH"])
+            for collection_date in rubbish_collection_dates
+        ]
+    )
+
+    entries.extend(
+        [
+            Collection(date=collection_date, t="Recycling", icon=ICON_MAP["RECYCLE"])
+            for collection_date in recycling_collection_dates
+        ]
+    )
+
+    entries.extend(
+        [
+            Collection(date=collection_date, t="Greenwaste", icon=ICON_MAP["ORGANIC"])
+            for collection_date in greenwaste_collection_dates
+        ]
+    )
+    
+    print(entries)
     return entries
 
 
@@ -130,4 +166,4 @@ class Source:
         if not collections_result['success']:
             raise Exception("Could not find collections")
 
-        return process_results(collections_result['results'])
+        return _process_results(collections_result['results'])
