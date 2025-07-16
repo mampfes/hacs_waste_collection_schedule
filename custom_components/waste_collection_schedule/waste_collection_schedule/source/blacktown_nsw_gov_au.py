7b772298c7669ad53221745d34a62b9d1c84be2f
dt215git <86194065+dt215git@users.noreply.github.com>
2025-07-16 19:16:23 +0100

    replace requote_uri with params

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/blacktown_nsw_gov_au.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/blacktown_nsw_gov_au.py
@@ -3,7 +3,6 @@ import json
 
 import requests
 from bs4 import BeautifulSoup
-from requests.utils import requote_uri
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = "Blacktown City Council (NSW)"
@@ -34,11 +33,17 @@ TEST_CASES = {
         "street_name": "Hythe St",
         "street_number": "9-11",
     },
+    "Issue#4434": {
+        "post_code": "2762",
+        "suburb": "Tallawong",
+        "street_name": "Coffey St",
+        "street_number": "13",
+    },
 }
 
 API_URLS = {
-    "address_search": "https://www.blacktown.nsw.gov.au/api/v1/myarea/search?keywords={}",
-    "collection": "https://www.blacktown.nsw.gov.au/ocapi/Public/myarea/wasteservices?geolocationid={}&ocsvclang=en-AU",
+    "address_search": "https://www.blacktown.nsw.gov.au/api/v1/myarea/search",
+    "collection": "https://www.blacktown.nsw.gov.au/ocapi/Public/myarea/wasteservices",
 }
 
 HEADERS = {
@@ -66,64 +71,47 @@ class Source:
     def fetch(self):
         session = requests.Session()
         session.headers.update(HEADERS)
-        locationId = 0
-
-        address = "{} {} {} NSW {}".format(
-            self.street_number, self.street_name, self.suburb, self.post_code
-        )
-
-        q = requote_uri(str(API_URLS["address_search"]).format(address))
 
         # Retrieve suburbs
-        r = session.get(q)
-
+        address = f"{self.street_number} {self.street_name}, {self.suburb} NSW {self.post_code}"
+        payload = {"keywords": address}
+        r = session.get(API_URLS["address_search"], params=payload)
         data = json.loads(r.text)
 
         # Find the ID for our suburb
+        locationId = 0
         for item in data["Items"]:
             locationId = item["Id"]
             break
-
         if locationId == 0:
             raise ValueError(
                 f"Unable to find location ID for {address}, maybe you misspelled your address?"
             )
 
         # Retrieve the upcoming collections for our property
-        q = requote_uri(str(API_URLS["collection"]).format(locationId))
-
-        r = session.get(q)
-
+        payload = {"geolocationid": locationId, "ocsvclang": "en-AU"}
+        r = session.get(API_URLS["collection"], params=payload)
         data = json.loads(r.text)
-
         responseContent = data["responseContent"]
-
         soup = BeautifulSoup(responseContent, "html.parser")
         services = soup.find_all("div", attrs={"class": "waste-services-result"})
 
         entries = []
-
         for item in services:
             # test if <div> contains a valid date. If not, is is not a collection item.
             date_text = item.find("div", attrs={"class": "next-service"})
-
             # The date format currently used on https://www.blacktown.nsw.gov.au/Services/Waste-services-and-collection/Waste-collection-days
             date_format = "%a %d/%m/%Y"
-
             try:
                 # Strip carriage returns and newlines out of the HTML content
                 cleaned_date_text = (
                     date_text.text.replace("\r", "").replace("\n", "").strip()
                 )
-
                 # Parse the date
                 date = datetime.datetime.strptime(cleaned_date_text, date_format).date()
-
             except ValueError:
                 continue
-
             waste_type = item.find("h3").text.strip()
-
             entries.append(
                 Collection(
                     date=date,
