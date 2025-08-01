2c198921417cf4963382332b016f5be1b465cfb8
5ila5 <5ila5@users.noreply.github.com>
2025-08-01 23:00:56 +0200

    retrieve more post args from web page

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/tbv_velbert_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/tbv_velbert_de.py
@@ -13,7 +13,8 @@ TEST_CASES = {
     "Rathaus": {"street": "Thomasstraße 1"},
 }
 
-API_URL = "https://www.tbv-velbert.de/abfall/abfalltrennung/abfallkalender"
+API_BASE = "https://www.tbv-velbert.de"
+API_URL = f"{API_BASE}/abfall/abfalltrennung/abfallkalender"
 HEADERS = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 ICON_MAP = {
     "Restmüll-Gefäß": "mdi:trash-can",
@@ -36,13 +37,31 @@ class Source:
         self._street = street
 
     def fetch(self):
+        session = requests.Session()
+        s = session.get(API_URL, headers=HEADERS)
+        s.raise_for_status()
+        soup = BeautifulSoup(s.text, "html.parser")
+        form = soup.select_one("div.tx-tbvabfall form")
+        if not form:
+            raise ValueError("Could not find form in the response.")
+        form_data = {}
+        action = form.get("action")
+        if not isinstance(action, str):
+            raise ValueError("Form action is not a string.")
+        if action.startswith("/"):
+            action = API_BASE + action
+
+        for input in form.select("input"):
+            name = input.get("name")
+            value = input.get("value")
+            if name and value:
+                form_data[name] = value
+
+        form_data["tx_tbvabfall_strassensuche[suchbegriff]"] = self._street
         r = requests.post(
-            API_URL,
+            action,
             headers=HEADERS,
-            data={
-                "tx_tbvabfall_strassensuche[abgeschickt]": 1,
-                "tx_tbvabfall_strassensuche[suchbegriff]": self._street,
-            },
+            data=form_data,
         )
         _LOGGER.debug(
             f"Fetching waste schedule for '{self._street}' returned status: {r.status_code}."
