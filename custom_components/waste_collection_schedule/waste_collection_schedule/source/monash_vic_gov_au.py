463d3bd8eccb22dc1003869ed5410805e23c242c
5ila5 <5ila5@users.noreply.github.com>
2025-10-04 19:22:54 +0200

    refomratting + remove unused code + use request.get params arg

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/monash_vic_gov_au.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/monash_vic_gov_au.py
@@ -1,5 +1,5 @@
 import datetime
-import json
+
 import requests
 from bs4 import BeautifulSoup
 from waste_collection_schedule import Collection
@@ -20,7 +20,7 @@ ICON_MAP = {
     "Food and Garden Waste": "mdi:leaf",
 }
 
-#### Arguments affecting the configuration GUI ####
+# ### Arguments affecting the configuration GUI ####
 
 HOW_TO_GET_ARGUMENTS_DESCRIPTION = {
     "en": f'Visit the [City of Monash]({SEARCH_PAGE_URL}) "When we collect your bins" page and search for your address. For example: 4 Carson Street, Mulgrave 3170. The arguments should exactly match the full street address after selecting the autocomplete result.',
@@ -38,50 +38,50 @@ PARAM_TRANSLATIONS = {
     },
 }
 
-#### End of arguments affecting the configuration GUI ####
+# ### End of arguments affecting the configuration GUI ####
+
 
 class Source:
-    def __init__(self, address:str):  # argX correspond to the args dict in the source configuration
+    def __init__(self, address: str):
         self._address = address
 
     def fetch(self) -> list[Collection]:
         s = requests.Session()
 
         r = s.get(
-            f"{URL}api/v1/myarea/search?keywords={self._address.replace(" ", "+")}"
+            f"{URL}api/v1/myarea/search",
+            params={"keywords": self._address},
         )
         if r.status_code != requests.codes.ok:
-            raise Exception("Error Accessing myarea API") # DO NOT JUST return [] 
+            raise Exception("Error Accessing myarea API")  # DO NOT JUST return []
         data = r.json()
-        geoid = data["Items"][0]["Id"] # assume first one is correct and get geoid
-        
+        geoid = data["Items"][0]["Id"]  # assume first one is correct and get geoid
+
         r = s.get(
-            f"{URL}ocapi/Public/myarea/wasteservices?geolocationid={geoid}&ocsvclang=en-AU"
+            f"{URL}ocapi/Public/myarea/wasteservices",
+            params={"geolocationid": geoid, "ocsvclang": "en-AU"},
         )
 
         if r.status_code != requests.codes.ok:
-            raise Exception("Error Accessing Waste Services API") # DO NOT JUST return []
-        schedule = r.json()
-        html = r.json()['responseContent']
+            raise Exception(
+                "Error Accessing Waste Services API"
+            )  # DO NOT JUST return []
+        html = r.json()["responseContent"]
         soup = BeautifulSoup(html, "html.parser")
 
         entries = []  # List that holds collection schedule
         for article in soup.find_all("article"):
             waste_type = article.find("h3").get_text(strip=True)
 
-            note = (
-                article.find("div", class_="note")
-                .get_text(" ", strip=True)
-                .replace("Collected ", "")
-                .replace(". Your next collection day is:", "")
+            next_date_str = article.find("div", class_="next-service").get_text(
+                strip=True
             )
-            next_date_str = article.find("div", class_="next-service").get_text(strip=True)
             next_date = datetime.datetime.strptime(next_date_str, "%a %d/%m/%Y").date()
             entries.append(
                 Collection(
-                    date = next_date,  # Collection date
-                    t = waste_type,  # Collection type
-                    icon = ICON_MAP.get(waste_type),  # Collection icon
+                    date=next_date,  # Collection date
+                    t=waste_type,  # Collection type
+                    icon=ICON_MAP.get(waste_type),  # Collection icon
                 )
             )
 
