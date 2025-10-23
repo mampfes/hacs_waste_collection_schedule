b85820913812494ccfb6766f74542226c6f4d9ae
dansollok <sollok@gmail.com>
2025-10-23 10:28:39 +0200

    sutton - added headers to request

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/sutton_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/sutton_gov_uk.py
@@ -29,6 +29,13 @@ class Source:
 
     def fetch(self):
         s = requests.Session()
+        s.headers.update({
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en-GB,en;q=0.5",
+            "Referer": "https://sutton.gov.uk",
+        })
+        
         api_url = API_URL.format(id=self._id)
         ical_url = ICAL_URL.format(id=self._id)
 
