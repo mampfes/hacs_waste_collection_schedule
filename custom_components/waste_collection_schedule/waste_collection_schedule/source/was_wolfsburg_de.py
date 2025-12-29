c9bf959f311e534c0c15071d937918e20a5c2045
Kenodai <marc@grfzqzq.de>
2025-12-29 18:55:03 +0100

    Fix WAS Wolfsburg DE (#5171)
    * fix WAS Wolfsburg DE
    
    * reformatting + ./update_docu_links.py

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/was_wolfsburg_de.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/was_wolfsburg_de.py
@@ -1,3 +1,4 @@
+import re
 from datetime import datetime
 
 import requests
@@ -7,12 +8,12 @@ TITLE = "Wolfsburger Abfallwirtschaft und Straßenreinigung"
 DESCRIPTION = "Source for waste collections for WAS-Wolfsburg, Germany."
 URL = "https://was-wolfsburg.de"
 TEST_CASES = {
-    "Barnstorf": {"street": "Bahnhofspassage"},
-    "Sülfeld": {"street": "Bärheide"},
+    "Barnstorf": {"street": "Bahnhofspassage", "number": 1},
+    "Sülfeld": {"street": "Bärheide", "number": 1},
 }
 
 ICON_MAP = {
-    "gelber Sack": "mdi:recycle",
+    "Wertstofftonne": "mdi:recycle",
     "Bioabfall": "mdi:leaf",
     "Restabfall": "mdi:trash-can",
     "Altpapier": "mdi:file-document-outline",
@@ -21,31 +22,47 @@ ICON_MAP = {
 PARAM_TRANSLATIONS = {
     "de": {
         "street": "Straße",
+        "number": "Hausnummer",
     }
 }
 
 
 class Source:
-    def __init__(self, street: str | None):
+    def __init__(self, street: str | None, number: int | None):
         self._street = street
+        self._number = str(number)
+
         if street is None:
             raise ValueError("Street must be set")
+        if number is None:
+            raise ValueError("Number must be set")
 
     def fetch(self) -> list[Collection]:
+        # get token
+        js = requests.get("https://abfuhrtermine.waswob.de/js/main.js")
+        token = re.search(r"token=([^\"\n\`]*)", js.text)
+        if token is None:
+            raise ValueError("Token not found")
+
         entries = []
         r = requests.get(
-            "https://abfuhrtermine.waswob.de/php/abfuhrtermineeingabe.php",
-            {"k": self._street},
+            "https://apiabfuhrtermine.waswob.de/api/download-json",
+            {
+                "strasse": self._street,
+                "hausnummer": self._number,
+                "token": token.group(1),
+            },
         )
         answer = r.json()
 
-        for a in answer:
-            entries.append(
-                Collection(
-                    date=datetime.strptime(a["date"], "%d.%m.%Y").date(),
-                    t=a["waste"],
-                    icon=ICON_MAP.get(a["waste"]),
+        for name, icon in ICON_MAP.items():
+            for a in answer[0][name]:
+                entries.append(
+                    Collection(
+                        date=datetime.strptime(a, "%Y-%m-%d").date(),
+                        t=name,
+                        icon=icon,
+                    )
                 )
-            )
 
         return entries
