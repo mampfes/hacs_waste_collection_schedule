5d5203aa0206b4e0995287f988421574936c324e
Jonas Arnold <jonas.arnold@live.de>
2025-12-30 01:07:15 +0100

    fixed missing import

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/sammelkalender_ch.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/sammelkalender_ch.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from typing import Literal
 
 import requests
+import logging
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 from waste_collection_schedule.exceptions import (
     SourceArgumentNotFoundWithSuggestions,
