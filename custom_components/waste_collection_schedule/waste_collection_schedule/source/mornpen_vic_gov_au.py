969d20ae4b76324be1139982589695355e09cdcc
tbham <3260638+tbham@users.noreply.github.com>
2026-01-17 00:21:47 +1100

    adds source mornington peninsula shire council vic au

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/mornpen_vic_gov_au.py
@@ -0,0 +1,103 @@
+import logging
+import re
+from datetime import datetime
+
+import requests
+from bs4 import BeautifulSoup
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
+
+TITLE = "Mornington Peninsula Shire Council"
+DESCRIPTION = "Source for Mornington Peninsula Shire Council rubbish collection."
+URL = "https://www.mornpen.vic.gov.au"
+TEST_CASES = {
+    "Main Ridge Pony Club": {
+        "street_address": "305 Baldrys Rd Main Ridge VIC 3928"
+    },
+    "Laneway Espresso Dromana": {
+        "street_address": "167 Point Nepean Rd Dromana VIC 3936"
+    },
+    "Pt. Leo Estate Merricks": {
+        "street_address": "3649 Frankston-Flinders Rd Merricks VIC 3916"
+    },
+}
+
+
+
+_LOGGER = logging.getLogger(__name__)
+
+ICON_MAP = {
+    "Green waste bin": "mdi:leaf",
+    "Household rubbish bin": "mdi:trash-can",
+    "Recycling bin": "mdi:recycle",
+}
+
+
+class Source:
+    def __init__(self, street_address):
+        self._street_address = street_address
+
+    def fetch(self):
+        session = requests.Session()
+
+        response = session.get(
+            "https://www.mornpen.vic.gov.au/Your-Property/Rubbish-Recycling/Bins/Find-your-bin-day"
+        )
+        response.raise_for_status()
+
+        response = session.get(
+            "https://www.mornpen.vic.gov.au/api/v1/myarea/search",
+            params={"keywords": self._street_address},
+        )
+        response.raise_for_status()
+        addressSearchApiResults = response.json()
+
+        if (
+            addressSearchApiResults["Items"] is None
+            or len(addressSearchApiResults["Items"]) < 1
+        ):
+            raise Exception(
+                f"Address search for '{self._street_address}' returned no results. Check your address on https://www.mornpen.vic.gov.au/Your-Property/Rubbish-Recycling/Bins/Find-your-bin-day"
+            )
+
+        addressSearchTopHit = addressSearchApiResults["Items"][0]
+        _LOGGER.debug("Address search top hit: %s", addressSearchTopHit)
+
+        geolocationid = addressSearchTopHit["Id"]
+        _LOGGER.debug("Geolocationid: %s", geolocationid)
+
+        response = session.get(
+            "https://www.mornpen.vic.gov.au/ocapi/Public/myarea/wasteservices",
+            params={"geolocationid": geolocationid, "ocsvclang": "en-AU"},
+        )
+        response.raise_for_status()
+
+        wasteApiResult = response.json()
+        _LOGGER.debug("Waste API result: %s", wasteApiResult)
+
+        soup = BeautifulSoup(wasteApiResult["responseContent"], "html.parser")
+
+        entries = []
+        for article in soup.find_all("article"):
+            waste_type = article.h3.string
+            icon = ICON_MAP.get(waste_type)
+
+            if waste_type == "Burning off":
+                continue
+
+            next_pickup = (
+                article.find("div", {"class": "next-service"}).getText().strip()
+            )
+
+            if not re.match(r"[^\s]* \d{1,2}\/\d{1,2}\/\d{4}", next_pickup):
+                continue
+
+            next_pickup_date = datetime.strptime(
+                next_pickup.split(sep=" ")[1], "%d/%m/%Y"
+            ).date()
+
+            if next_pickup_date is None or waste_type is None:
+                continue
+
+            entries.append(Collection(date=next_pickup_date, t=waste_type, icon=icon))
+
+        return entries
