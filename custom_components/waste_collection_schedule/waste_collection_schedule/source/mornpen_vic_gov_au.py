570158c0f660701de3491671f0a634608bc33101
tbham <3260638+tbham@users.noreply.github.com>
2026-01-27 21:20:56 +1100

    file clean up for PR

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/mornpen_vic_gov_au.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/mornpen_vic_gov_au.py
@@ -61,7 +61,7 @@ class Source:
             raise SourceArgumentException(
                 "street_address",
                 f"Address search for '{self._street_address}' returned no results. Check your address on https://www.mornpen.vic.gov.au/Your-Property/Rubbish-Recycling/Bins/Find-your-bin-day"
-)
+            )
 
         addressSearchTopHit = addressSearchApiResults["Items"][0]
         _LOGGER.debug("Address search top hit: %s", addressSearchTopHit)
