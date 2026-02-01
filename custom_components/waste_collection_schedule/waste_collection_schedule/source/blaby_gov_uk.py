88cc367f01e306de51861fe079972376e1066cd0
textbookcal <callumaholt94@outlook.com>
2026-02-01 13:08:49 +0000

    Add files via upload
    Fixed url

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/blaby_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/blaby_gov_uk.py
@@ -1,88 +1,67 @@
+TITLE = "Blaby District Council"
+DESCRIPTION = "Recycling and refuse collection dates for Blaby District Council, UK."
+URL = "https://my.blaby.gov.uk/collections"
+
 import re
 from datetime import datetime
-
 import requests
-import urllib3
 from bs4 import BeautifulSoup
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
-# With verify=True the POST fails due to a SSLCertVerificationError.
-# Using verify=False works, but is not ideal. The following links may provide a better way of dealing with this:
-# https://urllib3.readthedocs.io/en/1.26.x/advanced-usage.html#ssl-warnings
-# https://urllib3.readthedocs.io/en/1.26.x/user-guide.html#ssl
-# This line suppresses the InsecureRequestWarning when using verify=False
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+REGEX = r"\d{2}/\d{2}/\d{4}"
 
-TITLE = "Blaby District Council"
-DESCRIPTION = "Source for blaby.gov.uk services for Blaby district, UK."
-URL = "https://blaby.gov.uk"
-TEST_CASES = {
-    "Test_001": {"uprn": 100030407500},
-    "Test_002": {"uprn": "100030395499"},
-    "Test_003": {"uprn": "010001238216"},
-}
-HEADERS = {
-    "user-agent": "Mozilla/5.0",
-}
 ICON_MAP = {
-    "Recycling": "mdi:recycle",
     "Refuse": "mdi:trash-can",
+    "Recycling": "mdi:recycle",
     "Garden": "mdi:leaf",
 }
-HOW_TO_GET_ARGUMENTS_DESCRIPTION = {
-    "en": "An easy way to discover your Unique Property Reference Number (UPRN) is by going to https://www.findmyaddress.co.uk/ and entering in your address details",
-}
-PARAM_TRANSLATIONS = {
-    "en": {
-        "uprn": "Unique Property Reference Number (UPRN)",
-    }
-}
-PARAM_DESCRIPTIONS = {
-    "en": {
-        "uprn": "An easy way to discover your Unique Property Reference Number (UPRN) is by going to https://www.findmyaddress.co.uk/ and entering in your address details",
-    }
-}
-REGEX = r"\d+\/\d+\/\d+"
-
 
 class Source:
     def __init__(self, uprn: str | int):
         self._uprn = str(uprn).zfill(12)
 
     def fetch(self):
-        s = requests.Session()
-        payload: dict = {"ref": self._uprn, "redirect": "collections"}
-        r = s.get(
+        session = requests.Session()
+        session.headers.update({"User-Agent": "Mozilla/5.0"})
+
+        r = session.get(
             "https://my.blaby.gov.uk/set-location.php",
+            params={"ref": self._uprn, "redirect": "collections"},
             verify=False,
-            params=payload,
-            headers=HEADERS,
+            timeout=30,
         )
         r.raise_for_status()
 
-        soup = BeautifulSoup(r.content, "html.parser")
-        waste_collections = soup.find_all("span", {"class": "box-item"})
+        r = session.get(
+            "https://my.blaby.gov.uk/collections",
+            verify=False,
+            timeout=30,
+        )
+        r.raise_for_status()
 
+        soup = BeautifulSoup(r.text, "html.parser")
         entries = []
 
-        for waste_collection in waste_collections:
-            # Garden waste is a subscription service, catch
-            # error and skip processing when no dates shown
-            try:
-                waste_dates = waste_collection.find_all("strong")
-                waste_days = re.findall(REGEX, waste_dates[0].text)
-            except IndexError:
-                break
+        for h2 in soup.find_all("h2"):
+            bin_type = h2.get_text(strip=True)
+            if bin_type not in ICON_MAP:
+                continue
 
-            waste_type = waste_collection.find_all("h2")
-            if waste_days:
-                for item in waste_days:
-                    entries.append(
-                        Collection(
-                            t=waste_type[0].text,
-                            date=datetime.strptime(item, "%d/%m/%Y").date(),
-                            icon=ICON_MAP.get(waste_type[0].text),
-                        )
+            content = []
+            for sib in h2.next_siblings:
+                if getattr(sib, "name", None) == "h2":
+                    break
+                if hasattr(sib, "get_text"):
+                    content.append(sib.get_text(" ", strip=True))
+
+            text = " ".join(content)
+            for d in re.findall(REGEX, text):
+                entries.append(
+                    Collection(
+                        t=bin_type,
+                        date=datetime.strptime(d, "%d/%m/%Y").date(),
+                        icon=ICON_MAP[bin_type],
                     )
+                )
 
         return entries
