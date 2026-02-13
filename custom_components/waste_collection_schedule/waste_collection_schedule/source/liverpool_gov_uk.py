e09206cd64bb808f4567f2505aa240cc9af9e3a5
Benjamin <46243805+bbr111@users.noreply.github.com>
2026-02-13 18:10:32 +0100

    Revert "reformating"
    This reverts commit abbbe768ebf40dbf01c0efbe8046b0e65aa02557.

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/liverpool_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/liverpool_gov_uk.py
@@ -1,8 +1,7 @@
 import re
-from datetime import datetime, timedelta
-
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 from waste_collection_schedule import Collection
 
 TITLE = "Liverpool City Council"
@@ -31,36 +30,35 @@ class Source:
         today = datetime.today().date()
         entries = []
 
-        def trimsuffix(s):
-            return re.sub(r"(\d)(st|nd|rd|th)", r"\1", s)
-
+        def trimsuffix(s):                                             
+            return re.sub(r'(\d)(st|nd|rd|th)', r'\1', s)
+        
         q = str(API_URL).format(uprn=self._uprn)
-
+        
         r = requests.get(q)
         r.raise_for_status()
 
         soup = BeautifulSoup(r.text, "html.parser")
-        table = soup.findAll("table")
+        table = soup.findAll('table')
 
-        rows = table[0].findAll(lambda tag: tag.name == "tr")
+        rows = table[0].findAll(lambda tag: tag.name=='tr')
 
         for row in rows[1:]:
-            type = " ".join(row.find("th").text.split())
-            fields = row.find_all("td")
+            type=' '.join(row.find('th').text.split())
+            fields = row.find_all('td')
             for field in fields:
-                collectiondate = trimsuffix(
-                    " ".join(field.text.split()) + " " + datetime.today().strftime("%Y")
-                )
-                if re.match("Today", collectiondate):
+                collectiondate = trimsuffix(' '.join(field.text.split()) + " " + datetime.today().strftime("%Y"))
+                if re.match('Today',collectiondate):
                     date = today
-                elif re.match("Tomorrow", collectiondate):
+                elif re.match('Tomorrow',collectiondate):
                     date = today + timedelta(days=1)
                 else:
-                    date = datetime.strptime(collectiondate, "%A, %d %B %Y").date()
+                    date = datetime.strptime(collectiondate, '%A, %d %B %Y').date()
                     # As no year is specified we might need to add one year if it crosses Dec 31st
                     if date.month == 1 and today.month == 12:
-                        date = date.replace(year=date.year + 1)
-
+                        date = date.replace(year=date.year+1)
+                
+        
                 entries.append(
                     Collection(
                         date=date,
@@ -68,5 +66,5 @@ class Source:
                         icon=ICON_MAP.get(type),
                     )
                 )
-
+        
         return entries
