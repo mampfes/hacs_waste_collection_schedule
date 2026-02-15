91310b6e2b8a43d635f281651b51823b00631ce4
Benjamin <46243805+bbr111@users.noreply.github.com>
2026-02-15 16:31:58 +0100

    Refactor code for improved readability and add timeout to requests

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/mestorudna_cz.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/mestorudna_cz.py
@@ -15,14 +15,15 @@ TEST_CASES = {
     "ALL": {"city_part": "Vše"},
 }
 
-WASTE_TYPES_URL = "https://mesto-rudna.cz/odpadovy-kalendar/permanent/data_description.csv"
+WASTE_TYPES_URL = (
+    "https://mesto-rudna.cz/odpadovy-kalendar/permanent/data_description.csv"
+)
 WASTE_DATES_URL = "https://mesto-rudna.cz/odpadovy-kalendar/permanent/data_calendar.csv"
 
 ICON_MAP = {
     "B": "mdi:compost",
     "TKOS": "mdi:trash-can",
     "TKOJ": "mdi:trash-can",
-    "PET": "mdi:recycle",
     "BS": "mdi:pine-tree",
     "SHK": "mdi:pot",
     "SHPA": "mdi:note-text-outline",
@@ -46,7 +47,7 @@ HOW_TO_GET_ARGUMENTS_DESCRIPTION = {
     "en": "Choose your service area from the published map https://www.rudnamesto.cz/odpadovy-kalendar/d-193572",
 }
 
-PARAM_DESCRIPTIONS = { 
+PARAM_DESCRIPTIONS = {
     "en": {
         "city_part": "Select whether waste is collected on even or odd weeks.",
     },
@@ -61,7 +62,7 @@ PARAM_TRANSLATIONS = {
 CONFIG_FLOW_TYPES = {
     "city_part": {
         "type": "SELECT",
-        "values": ["Sudé týdny","Liché týdny","Vše"],
+        "values": ["Sudé týdny", "Liché týdny", "Vše"],
         "multiple": False,
     }
 }
@@ -74,17 +75,17 @@ class Source:
     def fetch(self):
         session = requests.Session()
 
-        csv_content = session.get(WASTE_TYPES_URL).content.decode("utf-8")
+        csv_content = session.get(WASTE_TYPES_URL, timeout=30).content.decode("utf-8")
 
         csv_lines = list(csv.reader(csv_content.splitlines(), delimiter=";"))
 
         waste_types = {}
 
         for row in csv_lines:
-            mydict = {"desc": row[1], "icon": ICON_MAP.get(row[0])}
+            mydict = {"desc": row[1], "icon": ICON_MAP.get(row[0], "mdi:trash-can")}
             waste_types[row[0]] = mydict
 
-        csv_content = session.get(WASTE_DATES_URL).content.decode("utf-8")
+        csv_content = session.get(WASTE_DATES_URL, timeout=30).content.decode("utf-8")
 
         csv_lines = list(csv.reader(csv_content.splitlines(), delimiter=";"))
 
@@ -97,14 +98,22 @@ class Source:
                 continue
             if self._city_part.lower() == "j" and row[2] == "TKOS":
                 continue
-            if self._city_part.lower() == "liché týdny" and row[2] in["SKOS","D2DPLASTS","BIOSU"]:
+            if self._city_part.lower() == "liché týdny" and row[2] in [
+                "SKOS",
+                "D2DPLASTS",
+                "BIOSU",
+            ]:
                 continue
-            if self._city_part.lower() == "sudé týdny" and row[2] in["SKOL","D2DPLASTL","BIOL"]:
+            if self._city_part.lower() == "sudé týdny" and row[2] in [
+                "SKOL",
+                "D2DPLASTL",
+                "BIOL",
+            ]:
                 continue
             pickup_date = datetime.strptime(row[1], format)
             waste_type = waste_types[row[2]]["desc"]
             icon = waste_types[row[2]]["icon"]
 
-            entries.append(Collection(date = pickup_date.date(), t = waste_type, icon = icon))
+            entries.append(Collection(date=pickup_date.date(), t=waste_type, icon=icon))
 
         return entries
