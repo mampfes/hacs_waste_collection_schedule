df0820b8f7f1dfed3ce7cba395c9ba738ac616ef
cunners <cunners@gmail.com>
2026-02-18 20:14:46 +0000

    Update User-Agent header for requests
    Fixes 503 error

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/sutton_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/sutton_gov_uk.py
@@ -31,7 +31,7 @@ class Source:
         s = requests.Session()
         s.headers.update(
             {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
                 "Accept-Language": "en-GB,en;q=0.5",
                 "Referer": "https://sutton.gov.uk",
