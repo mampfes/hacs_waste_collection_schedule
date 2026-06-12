bf5bd757b2c5b82d77db09e30cc43463f9b82776
Tim Wilkes <phpsystems@users.noreply.github.com>
2026-06-12 14:19:22 +0100

    Update stockport_gov_uk.py
    Fix for the 403 from whatever stockport council have used to protect the info.

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/stockport_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/stockport_gov_uk.py
@@ -31,10 +31,27 @@ class Source:
         self._uprn = uprn
 
     def fetch(self):
+
+        headers = {
+            "User-Agent": (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/137.0 Safari/537.36"
+            )
+        }
+
         r = requests.get(
-            f"https://myaccount.stockport.gov.uk/bin-collections/show/{self._uprn}"
+            f"https://myaccount.stockport.gov.uk/bin-collections/show/{self._uprn}", headers=headers, timeout=30
         )
 
+#        _LOGGER.warning("Status: %s", r.status_code)
+#        _LOGGER.warning("URL after redirects: %s", r.url)
+#        _LOGGER.warning("Length: %s", len(r.text))
+
+#        with open("/tmp/stockport.html", "w") as f:
+#            f.write(r.text)
+
+        
         soup = BeautifulSoup(r.text, features="html.parser")
 
         # Find all bin service items
