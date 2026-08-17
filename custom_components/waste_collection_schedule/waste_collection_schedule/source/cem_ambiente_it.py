0f6230367114a6170db17938750dc4db81046d89
Riccardo Rossi <48052263+riccardorossi92@users.noreply.github.com>
2026-08-17 18:59:01 +0200

    Fix: removed extra headers

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/cem_ambiente_it.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/cem_ambiente_it.py
@@ -20,9 +20,6 @@ HEADERS = {
         "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) "
         "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1"
     ),
-    "Accept": "*/*",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Connection": "keep-alive",
     "Content-Type": "application/json",
 }
 
