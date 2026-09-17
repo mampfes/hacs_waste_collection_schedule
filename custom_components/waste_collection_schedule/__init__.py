071cfa7f85dd2f9a9a8cf6fea05cba3acfbd1d8b
Kevin <kevinbayer8@gmail.com>
2026-09-17 16:19:44 +0200

    fix ruff

--- custom_components/waste_collection_schedule/__init__.py
+++ custom_components/waste_collection_schedule/__init__.py
@@ -2,9 +2,9 @@
 
 from .init_ui import (  # noqa: F401
     async_migrate_entry,
+    async_remove_config_entry_device,
     async_setup_entry,
     async_unload_entry,
     async_update_listener,
-    async_remove_config_entry_device,
 )
 from .init_yaml import CONFIG_SCHEMA, async_setup  # noqa: F401
