ac9207068492392606c82f5628135dd32af14ad7
Phil Richardson <ppslim+guthub@gmail.com>
2026-09-19 09:48:39 +0100

    Correct variable reuse causing type issues. Resolves #7456

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/adur_worthing_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/adur_worthing_gov_uk.py
@@ -108,9 +108,9 @@ class Source:
                     waste_date = text.text.split(": ")[1]
             # Append year and deal with year-end dates
             waste_date += f" {datetime.now().year}"
-            waste_date = parser.parse(waste_date).date()
-            if waste_date.month < datetime.now().month:
-                waste_date = waste_date + timedelta(days=365)
+            waste_date_dt = parser.parse(waste_date).date()
+            if waste_date_dt.month < datetime.now().month:
+                waste_date_dt = waste_date_dt + timedelta(days=365)
             # waste descriptions changed, so make consistent with old configs
             if waste_type == "General rubbish":
                 waste_type = "Refuse"
@@ -118,7 +118,7 @@ class Source:
                 waste_type = "Garden"
 
             entries.append(
-                Collection(t=waste_type, date=waste_date, icon=ICON_MAP.get(waste_type))
+                Collection(t=waste_type, date=waste_date_dt, icon=ICON_MAP.get(waste_type))
             )
 
         return entries
