90c24234a7409650acf4035b0a868d9961aaf9f7
Robin Madsen <robin@robserob.dk>
2026-09-21 23:22:04 +0200

    affaldonline - Create servicefile

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/service/AffaldOnlineDk.py
@@ -0,0 +1,162 @@
+from typing import TYPE_CHECKING, Any
+
+import requests
+from bs4 import BeautifulSoup
+from curl_cffi import requests as cffi_requests
+
+from waste_collection_schedule.exceptions import SourceArgumentException
+from waste_collection_schedule.parsers import Parser
+from waste_collection_schedule.retrievers import RetrieverFunc
+
+if TYPE_CHECKING:
+    from waste_collection_schedule.base_source import BaseSource
+
+CLIENT_ID_LOOKUP = {
+    "aeroe": "db765a2f-3f50-4abd-a738-3825813fedcb",
+    "assens": "afd912a2-44b3-402f-9e13-5aeb701ce143",
+    "favrskov": "dffcc5b6-b9ee-478d-82e2-030123485f7e",
+    "fanoe": "af7badab-508b-43fa-87dc-162347b288f3",
+    "fredericia": "dea6ff86-2ee9-4e7a-8fce-76dcb5625714",
+    "ffv": "ceca5978-6380-4ff8-ac28-9b6505457da8",
+    "holbaek": "017efd06-ac42-4b36-8a70-ab309162e988",
+    "langeland": "be8a9420-a9ae-42e6-83f2-eda5ec3fa29f",
+    "middelfart": "17F02B8B-7743-4FA6-8646-74F59436AED1",
+    "morsoe": "0199b7d2-bbae-46a5-a726-293c9236f4e5",
+    "nyborg": "4571D02F-602C-485A-8961-466EAA2B7B04",
+    "silkeborg": "eea63ff1-96fd-4288-b96e-83100ebbc378",
+    "rebild": "cb3ddc8c-900a-43ce-ac88-ec7587db4db3",
+    "vejle": "209cb669-e2e8-4c9b-8048-8287db51a61e",
+    "viborg": "4EBB900C-088E-475F-83ED-B087F4AD07BA",
+}
+
+"""
+Provides the cascading_select choices for affaldonline_dk
+The ``street`` field is populated with every street in a given ```municipality```
+
+When a ``street`` is selected the house numbers for that ``street`` is gathered.
+This also gives us the ``values`` associated with that address on the affaldonline platform.
+
+The house number field is called ``values`` for backwards compatability reasons, as the retired screenscraping approach used this naming convention.
+By keeping that naming scheme old installations can continue working.
+
+The ``values`` field is labeled with the house numbers, while the value is the internal affaldonline identifier string for that address.
+"""
+
+
+def discover_choices(field: str, selections: dict) -> list[tuple[str, str]]:
+    municipality = selections.get("municipality")
+    if not municipality:
+        return []
+    session = cffi_requests.Session(impersonate="chrome")
+    if field == "street":
+        response = session.get(
+            url=f"https://www.affaldonline.dk/kalender/{municipality}/acCal.php?term="
+        )
+        if response.status_code != 200:
+            return []
+        json_entries = response.json()
+        street_list = [
+            (
+                entry["value"],
+                f"{entry['vejnavn']}|{entry['postnr']}|{entry['Bynavn']}",
+            )
+            for entry in json_entries
+        ]
+        return street_list
+    if field == "values":
+        street = selections.get("street")
+        if not street:
+            return []
+        # Split the street string into the three values the api expects
+        street_components = street.split("|")
+        response = session.get(
+            url=f"https://www.affaldonline.dk/kalender/{municipality}/husnrCal.php",
+            params={
+                "vejnavn": street_components[0],
+                "postnr": street_components[1],
+                "postdist": street_components[2],  # This is the municipality name
+            },
+        )
+        # The response is quite strange, it is a html select element...
+        soup = BeautifulSoup(response.text, "html.parser")
+        options = soup.find_all("option")
+        return [(str(option.string), str(option["value"])) for option in options]
+    return []
+
+
+class AffaldOnlineDkRetriver(RetrieverFunc):
+    """
+    Calls the undocumented affaldonline api.
+    The api differenciates municipalities by the "X-Client-Provider" header.
+    Suitable provider_id's are available in the CLIENT_ID_LOOKUP map.
+
+    Reads the fields ``municipality`` and ``values`` from the source params.
+    The ``values`` param is an internal identifying string used by affaldonline.
+    It is found using the cascading select implemented in ``get_choices`` above
+    """
+
+    def __call__(self, source: "BaseSource") -> dict[str, Any]:
+        municipality = source.params.get("municipality")
+        if not municipality:
+            raise SourceArgumentException(
+                "municipality", "Provided munipality is not valid"
+            )
+        client_id = CLIENT_ID_LOOKUP.get(municipality)
+        if not client_id:
+            raise SourceArgumentException(
+                "municipality", "Provided munipality is not valid"
+            )
+        values = source.params.get("values")
+        if not values:
+            raise SourceArgumentException("values", "Internal values are missing")
+        address_id_split = values.split("|")
+        if len(address_id_split) < 2:
+            raise SourceArgumentException("values", "Provided address is not valid")
+        address_id = "|".join(address_id_split[-2:])
+        return source.session.get(
+            url="https://www.affaldonline.dk/api/address/collections",
+            params={"groupBy": "date", "addressId": address_id},
+            headers={
+                "X-Client-Type": "Kunde app",
+                "X-Client-OS": "android",
+                "X-Client-Version": "9999",
+                "X-Client-Provider": client_id,
+            },
+        )
+
+
+class AffaldOnlineDkParser(Parser["list[dict]"]):
+    """
+    Parses the affaldonline data into [{date, fraction_name, fraction_types:[fraction_id]}] records
+    If source params provides the "split_bins" boolean, the records is split into multiple records, one for each fraction_id
+    """
+
+    def __call__(
+        self, response: requests.Response, source: "BaseSource | None" = None
+    ) -> "list[dict]":
+        json_content = response.json()
+        if "message" in json_content:
+            raise ValueError("Error from API: " + json_content["message"])
+        entries = []
+        for entry in json_content:
+            for collection in entry["collections"]:
+                fraction_name = str(collection["fraction"]["name"]).strip()
+                if source and source.params.get("split_bins", False):
+                    # Split the fractions into individual collections
+                    for fraction_id in collection["daIcon"]:
+                        entries.append(
+                            {
+                                "date": entry["date"],
+                                "fraction_name": fraction_name,
+                                "fraction_types": [fraction_id],
+                            }
+                        )
+                else:
+                    entries.append(
+                        {
+                            "date": entry["date"],
+                            "fraction_name": fraction_name,
+                            "fraction_types": collection["daIcon"],
+                        }
+                    )
+        return entries
--- custom_components/waste_collection_schedule/waste_collection_schedule/source/affaldonline_dk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/affaldonline_dk.py
@@ -1,7 +1,6 @@
 import logging
 from typing import ClassVar
 
-from bs4 import BeautifulSoup
 from waste_collection_schedule import (  # type: ignore[attr-defined]
     Collection,
     Icons,
@@ -11,17 +10,19 @@ from waste_collection_schedule import (  # type: ignore[attr-defined]
 )
 from waste_collection_schedule.base_source import BaseSource
 from waste_collection_schedule.config_params import (
+    boolean,
     cascading_select,
     municipality,
-    boolean
 )
-from waste_collection_schedule.exceptions import (
-    SourceArgumentException,
+from waste_collection_schedule.service.AffaldOnlineDk import (
+    AffaldOnlineDkParser,
+    AffaldOnlineDkRetriver,
+    discover_choices,
 )
 
 _LOGGER = logging.getLogger("waste_collection_schedule.affaldonline_dk")
 
-""" 
+"""
 Waste seperation in Denmark is mandetory to be atleast seperated into these 10 fractions:
 Food Waste
 Paper
@@ -37,7 +38,7 @@ Residual waste
 Only 6 out of the 10 types currently have a WasteType, the following are missing:
 Cardboard, Plastic, Food and drink cartons, Metal
 
-These 10 fractions are usually combined into bins for collection, with one or two compartments. 
+These 10 fractions are usually combined into bins for collection, with one or two compartments.
 Some fractions are additionally allowed to be combined into the same compartment. As such some bins have 4 different fractions combined.
 
 This makes the WasteType approach highly impractical, as some bins would end up with the same WasteType, even though it is a completely different bin and content.
@@ -77,35 +78,15 @@ FRACTION_MAP = {
     89: ("Madaffald", Icons.BIO_KITCHEN),
 }
 
-CLIENT_ID_LOOKUP = {
-    "aeroe": "db765a2f-3f50-4abd-a738-3825813fedcb",
-    "assens": "afd912a2-44b3-402f-9e13-5aeb701ce143",
-    "favrskov": "dffcc5b6-b9ee-478d-82e2-030123485f7e",
-    "fanoe": "af7badab-508b-43fa-87dc-162347b288f3",
-    "fredericia": "dea6ff86-2ee9-4e7a-8fce-76dcb5625714",
-    "ffv": "ceca5978-6380-4ff8-ac28-9b6505457da8",
-    "holbaek": "017efd06-ac42-4b36-8a70-ab309162e988",
-    "langeland": "be8a9420-a9ae-42e6-83f2-eda5ec3fa29f",
-    "middelfart": "17F02B8B-7743-4FA6-8646-74F59436AED1",
-    "morsoe": "0199b7d2-bbae-46a5-a726-293c9236f4e5",
-    "nyborg": "4571D02F-602C-485A-8961-466EAA2B7B04",
-    "silkeborg": "eea63ff1-96fd-4288-b96e-83100ebbc378",
-    "rebild": "cb3ddc8c-900a-43ce-ac88-ec7587db4db3",
-    "vejle": "209cb669-e2e8-4c9b-8048-8287db51a61e",
-    "viborg": "4EBB900C-088E-475F-83ED-B087F4AD07BA",
-}
-
 
 class Source(BaseSource):
     TITLE = "Affaldonline"
     DESCRIPTION = "Gather waste collection schedules from Affaldonline"
     URL = "https://affaldonline.dk"
     API_URL = "https://www.affaldonline.dk/api/address/collections"
-    SOURCE_CODEOWNERS = ["@superrob"]
+    SOURCE_CODEOWNERS: ClassVar[list] = ["@superrob"]
 
-    REGIONS = regions.from_yaml(
-        "affaldonline_dk", title_suffix="affaldonline"
-    )
+    REGIONS = regions.from_yaml("affaldonline_dk", title_suffix="affaldonline")
 
     TEST_CASES: ClassVar[dict] = {
         "aeroe": {
@@ -203,97 +184,17 @@ class Source(BaseSource):
     @classmethod
     def get_choices(cls, field: str, selections: dict) -> list[tuple[str, str]]:
         """Options for one cascade level given the levels chosen so far."""
-        municipality = selections.get("municipality")
-        if not municipality:
-            _LOGGER.warning("No municipality selected")
-            return []
-        from curl_cffi import requests as cffi_requests
+        return discover_choices(field, selections)
 
-        session = cffi_requests.Session(impersonate="chrome")
-        if field == "street":
-            response = session.get(
-                url=f"https://www.affaldonline.dk/kalender/{municipality}/acCal.php?term="
-            )
-            if response.status_code != 200:
-                return []
-            json_entries = response.json()
-            street_list = [
-                (
-                    entry["value"],
-                    f"{entry['vejnavn']}|{entry['postnr']}|{entry['Bynavn']}",
-                )
-                for entry in json_entries
-            ]
-            return street_list
-        if field == "values":
-            street = selections.get("street")
-            if not street:
-                return []
-            # Split the street string into the three values the api expects
-            street_components = street.split("|")
-            response = session.get(
-                url=f"https://www.affaldonline.dk/kalender/{municipality}/husnrCal.php",
-                params={
-                    "vejnavn": street_components[0],
-                    "postnr": street_components[1],
-                    "postdist": street_components[2],  # This is the municipality name
-                },
-            )
-            # The response is quite strange, it is a html select element...
-            soup = BeautifulSoup(response.text, "html.parser")
-            options = soup.find_all("option")
-            return [(str(option.string), str(option["value"])) for option in options]
+    retrieve = AffaldOnlineDkRetriver()
+    parse = AffaldOnlineDkParser()
 
-        return []
+    """
+    Under the new WasteTypes paradime, this is "wrong".
+    However the alternative would be both breaking and in many cases group multiple different containers into the same category.
+    See the notes on the top of this file.
+    """
 
-    def retrieve(self, source):
-        municipality = source.params.get("municipality")
-        if not municipality:
-            raise SourceArgumentException(
-                "municipality", "Provided munipality is not valid"
-            )
-        client_id = CLIENT_ID_LOOKUP.get(municipality)
-        if not client_id:
-            raise SourceArgumentException(
-                "municipality", "Provided munipality is not valid"
-            )
-        values = source.params.get("values")
-        if not values:
-            raise SourceArgumentException("values", "Internal values are missing")
-        address_id_split = values.split("|")
-        if len(address_id_split) < 2:
-            raise SourceArgumentException("values", "Provided address is not valid")
-        address_id = "|".join(address_id_split[-2:])
-        return source.session.get(
-            url="https://www.affaldonline.dk/api/address/collections",
-            params={"groupBy": "date", "addressId": address_id},
-            headers={
-                "X-Client-Type": "Kunde app",
-                "X-Client-OS": "android",
-                "X-Client-Version": "9999",
-                "X-Client-Provider": client_id,
-            },
-        )
-
-    def parse(self, response, source):
-        json_content = response.json()
-        if "message" in json_content:
-            raise ValueError("Error from API: " + json_content["message"])
-
-        entries = []
-        for entry in json_content:
-            for collection in entry["collections"]:
-                fraction_name = str(collection["fraction"]["name"]).strip()
-                if self.params.get("split_bins", False):
-                    # Split the fractions into individual collections
-                    for fraction_id in collection["daIcon"]:
-                        entries.append({"date": entry["date"], "fraction_name": fraction_name, "fraction_types": [fraction_id]})
-                else:
-                    entries.append({"date": entry["date"], "fraction_name": fraction_name, "fraction_types": collection["daIcon"]})
-        return entries
-
-    # Yes, under the new WasteTypes paradime, this is "wrong".
-    # However the alternative would be both breaking and in many cases group multiple different containers into the same category.
     def classify(self, record):
         parse_date = date_parsers.for_format("%Y-%m-%d")
         date = parse_date(record["date"])
@@ -304,19 +205,23 @@ class Source(BaseSource):
         fraction_names = []
         fraction_icon = Icons.GENERAL_WASTE
         for fraction_id in record["fraction_types"]:
-            fraction_info = FRACTION_MAP.get(fraction_id, (f"{fraction_id} Mangler navn", Icons.GENERAL_WASTE))    
+            fraction_info = FRACTION_MAP.get(
+                fraction_id, (f"{fraction_id} Mangler navn", Icons.GENERAL_WASTE)
+            )
             fraction_names.append(fraction_info[0])
             fraction_icon = fraction_info[1]
         return Collection(
             date=date,
-            t=fraction_names[0] if len(fraction_names) == 1 else " og ".join([", ".join(fraction_names[:-1]),fraction_names[-1]]),
+            t=fraction_names[0]
+            if len(fraction_names) == 1
+            else " og ".join([", ".join(fraction_names[:-1]), fraction_names[-1]]),
             icon=fraction_icon,
         )
 
-    """     
+    """
     This is the unused transformer aproach. Due to waste_type overlap.
     transform = JsonTransformer(
-        date_key="date", 
+        date_key="date",
         parse_date=date_parsers.for_format("%Y-%m-%d"),
         type_key="type",
         description_key="type",
